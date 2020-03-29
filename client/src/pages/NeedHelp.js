f17999f40313ff4bba5c2841e6f59573624bb864
Robin Venneman <robin.venneman@gmail.com>
2020-03-29 22:11:16 +0200

    update need help flow, separate checkbox component and go to medical after submit

--- client/src/pages/NeedHelp.js
+++ client/src/pages/NeedHelp.js
@@ -1,137 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { withRouter } from 'react-router-dom';
 import StepWizard from 'react-step-wizard';
+
+import { CheckBoxItem } from '../components/CheckBoxItem';
+import { asyncGetGeoLocation } from '../utils/geolocation';
 
 const CONTAINER_STYLES = {
     marginTop: "160px",
     width: "600px",
 };
 
-const CHECKBOX_STYLES = {
-    display: 'flex',
-    border: '1px solid #bbb',
-    borderRadius: '3px',
-    padding: '15px',
-    margin: '15px 50px 15px 0',
-};
-
-const CHECKBOX_INPUT_STYLES = {
-    cursor: 'pointer',
-    margin: '15px',
-    position: 'relative',
-};
-
-const CHECKBOX_LABEL_STYLES = {
-    cursor: 'pointer',
-    display: 'block',
-    margin: '15px',
-};
-
 // todo: should find or update step wizard to pass data from event instead of mutating this state
-const WIZARD_STATE = {
-    answers: [],
-    currentStep: 0,
-};
+const needHelpAnswers = [];
 
 const Step1 = (props) => {
+    const onSelectMedical = () => {
+        needHelpAnswers.push({ type: 'need medical help' });
+        props.nextStep()
+    };
+    const onSelectOther = () => {
+        needHelpAnswers.push({ type: 'other, non medical' });
+        props.nextStep()
+    };
+
     return (
         <div>
             <h5 className="text-primary">Question {props.currentStep} / {props.totalSteps}</h5>
             <h2 className="mb-5">What type of help do you need?</h2>
-            <Form.Check style={CHECKBOX_STYLES} type="radio" id="type-medical">
-                <Form.Check.Input style={CHECKBOX_INPUT_STYLES} type="radio" onChange={() => {
-                    WIZARD_STATE.answers.push({ type: 'need medical help' });
-                    props.nextStep()
-                }} />
-                <Form.Check.Label style={CHECKBOX_LABEL_STYLES}>
-                    Medical: I believe I might have symptoms of COVID-19.
-                </Form.Check.Label>
-            </Form.Check>
-            <Form.Check style={CHECKBOX_STYLES} type="radio" id="type-other">
-                <Form.Check.Input style={CHECKBOX_INPUT_STYLES} type="radio" onChange={() => {
-                    WIZARD_STATE.answers.push({ type: 'other, non medical' });
-                    props.nextStep()
-                }}/>
-                <Form.Check.Label style={CHECKBOX_LABEL_STYLES}>
-                    Other Help: I need assistance getting groceries/medicine/etc.
-                </Form.Check.Label>
-            </Form.Check>
+            <CheckBoxItem id="type-medical" label="Medical: I believe I might have symptoms of COVID-19." onSelect={onSelectMedical} />
+            <CheckBoxItem id="type-other" label="Other Help: I need assistance getting groceries/medicine/etc." onSelect={onSelectOther} />
         </div>
     );
 };
 
-const getGeoLocation = () => {
-    return new Promise((resolve, reject) => {
-        const onGeoSuccess = (pos) => {
-            const { coords } = pos;
-            console.log('got pos:', { pos });
-            resolve(coords);
-        };
-        const onGeoError = () => {
-            reject(new Error('failed getting location'));
-        };
-        navigator.geolocation.getCurrentPosition(onGeoSuccess,onGeoError);
-    })
-};
-
-
 const Step2 = (props) => {
+    const selectLocationDetection = async () => {
+        try {
+            const location = await asyncGetGeoLocation();
+            needHelpAnswers.push({ location });
+        } catch {
+            needHelpAnswers.push({ location:  'unknown' });
+        } finally {
+            props.nextStep();
+        }
+    };
+    const rejectLocationDetection = () => {
+        needHelpAnswers.push({ location: 'unknown' });
+        props.nextStep()
+    };
     return (
         <div>
             <h5 className="text-primary">Question {props.currentStep} / {props.totalSteps}</h5>
             <h2 className="mb-5">Where are you located?</h2>
-            <Form.Check style={CHECKBOX_STYLES} type="radio" id="detect">
-                <Form.Check.Input style={CHECKBOX_INPUT_STYLES} type="radio" onChange={async () => {
-                    try {
-                        const location = await getGeoLocation();
-                        WIZARD_STATE.answers.push({ location });
-                    } catch {
-                        WIZARD_STATE.answers.push({ location:  'unknown' });
-                    } finally {
-                        props.nextStep()
-                    }
-                }} />
-                <Form.Check.Label style={CHECKBOX_LABEL_STYLES}>
-                    Detect my location
-                </Form.Check.Label>
-            </Form.Check>
-            <Form.Check style={CHECKBOX_STYLES} type="radio" id="location-unkonwn">
-                <Form.Check.Input style={CHECKBOX_INPUT_STYLES} type="radio" onChange={() => {
-                    WIZARD_STATE.answers.push({ location: 'unknown' });
-                    props.nextStep()
-                }}/>
-                <Form.Check.Label style={CHECKBOX_LABEL_STYLES}>
-                    Doesn't matter
-                </Form.Check.Label>
-            </Form.Check>
+            <CheckBoxItem id="detect" label="Detect my location" onSelect={selectLocationDetection} />
+            <CheckBoxItem id="reject" label="Doesn't matter" onSelect={rejectLocationDetection} />
         </div>
     );
 };
 
-const Step3 = (props) => {
+const Step3 = withRouter((props) => {
+    const [email, setEmail] = useState('');
+    const onChange = (evt) => setEmail(evt.target.value);
+    const onSubmit = () => {
+        needHelpAnswers.push({ email });
+        console.log("submit", { email, needHelpAnswers });
+        localStorage.setItem('needHelpAnswers', needHelpAnswers);
+        props.history.push({
+            pathname: '/medical',
+            data: { needHelpAnswers: needHelpAnswers }
+        });
+    };
     return (
         <div>
             <h5 className="text-primary">Question {props.currentStep} / {props.totalSteps}</h5>
             <h2 className="mb-5">What is your email address?</h2>
             <div style={{ marginRight: '50px' }}>
-                <Form.Control className="mb-3" placeholder="Type your email" />
-                <Button block variant="primary" onClick={() => {}}>Submit</Button>
+                <Form.Control className="mb-3" placeholder="Type your email" onChange={onChange} />
+                <Button block variant="primary" onClick={onSubmit}>Submit</Button>
             </div>
         </div>
     );
-};
+});
 
 export const NeedHelp = () => {
-    // const [state, setState] = useState(INITIAL_STATE);
     return (
         <div className="mx-auto" style={CONTAINER_STYLES}>
-            <StepWizard onStepChange={(step, answer) => {
-                console.log({ step, answer })
-                /*setState({
-                    answers: [...state.answers, answer],
-                    currentStep: state.currentStep +1,
-                });*/
-            }}>
+            <StepWizard>
                 <Step1 />
                 <Step2 />
                 <Step3 />
