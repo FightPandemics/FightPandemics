c9e4e51f24afcad363ff9c2c7d61a4f1678d3f18
Godstime <lewisagho47@gmail.com>
2020-03-31 14:24:42 +0100

    Adding setting up page 6

---
+++ client/src/pages/symptoms-check/pages/page6/Page6.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+//     return (
+//         <div className="text-center mx-auto" style={ CONTAINER_STYLES }>
+//             <h5>Local Emergency Number</h5>
+//             <h1 className="text-primary display-4 font-weight-bolder">911</h1>
+
+//             <div style={ { display: "flex", margin: "30px 10px 10px 50px" } }>
+//                 <Nav variant="med-info">
+//                     <Nav.Item>
+//                         <Nav.Link>Nearest Hospitals</Nav.Link>
+//                     </Nav.Item>
+//                     <Nav.Item>
+//                         <Nav.Link>Symptoms Check</Nav.Link>
+//                     </Nav.Item>
+//                     <Nav.Item>
+//                         <Nav.Link>Find Help</Nav.Link>
+//                     </Nav.Item>
+//                 </Nav>
+//             </div>
+//         </div>
+//     );
+// >>>>>>> master
+};
