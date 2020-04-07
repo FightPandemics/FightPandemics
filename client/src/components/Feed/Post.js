06ff77da13001137713292834a85b9e2fa9d6afb
Annie Giang <annieg4123@gmail.com>
2020-04-07 00:05:15 -0700

    Style share icons and comment input

--- client/src/components/Feed/Post.js
+++ client/src/components/Feed/Post.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { Card, WingBlank, WhiteSpace } from "antd-mobile";
+import { Card, WhiteSpace } from "antd-mobile";
+import FilterTag from "../../components/Tag/FilterTag";
+import TextInput from "../../components/Input/TextInput";
+import { ROYAL_BLUE, DARK_GRAY, SELAGO } from "../../constants/colors";
 
 const CustomCard = styled(Card)`
+  margin-bottom: 40px;
   &.am-card,
   .am-card-body {
     &::before {
@@ -18,7 +22,7 @@ const CustomCard = styled(Card)`
 
         img {
           margin-top: -5px;
-          margin-right: 10px;
+          margin-right: 7px;
         }
       }
       .am-card-header-extra {
@@ -29,42 +33,128 @@ const CustomCard = styled(Card)`
       }
     }
     .am-card-body {
-      padding: 15px 0;
+      padding: 0;
       color: black;
 
       h1 {
         font-size: 21px;
+        margin: 0;
       }
 
       p {
         font-size: 13.5px;
         line-height: 20px;
       }
+
+      .view-more {
+        color: ${ROYAL_BLUE};
+        font-weight: bold;
+      }
+
+      .social-icons {
+      }
+      .social-icon {
+        display: inline-block;
+        color: ${DARK_GRAY};
+        margin-right: 50px;
+        img {
+          margin-right: 10px;
+        }
+      }
     }
   }
 `;
 
 export default ({ post }) => {
-  const { title, description, author, location } = post;
+  const [expanded, setExpanded] = useState(false);
+  const {
+    title,
+    description,
+    author,
+    photoUrl,
+    location,
+    tags,
+    numLikes,
+    numComments,
+    numShares,
+  } = post;
 
   const thumbStyle = {
     borderRadius: "40px",
-    width: "45px",
-    height: "45px",
+    width: "40px",
+    height: "40px",
     maxWidth: "100%",
+  };
+
+  const commentStyles = {
+    backgroundColor: SELAGO,
+    width: "86%",
+    borderBottom: "unset",
+    borderRadius: "40px",
+    padding: "14px",
+  };
+
+  const renderTags = () => {
+    return tags.map((tag, idx) => (
+      <FilterTag label={tag} selected={false} key={idx} />
+    ));
   };
 
   return (
     <CustomCard>
       <Card.Header
         title={author}
-        thumb="https://www.pinclipart.com/picdir/middle/148-1486972_mystery-man-avatar-circle-clipart.png"
         thumbStyle={thumbStyle}
         extra={<span># {location}</span>}
+        thumb={photoUrl}
       />
+      <WhiteSpace size="lg" />
+      <Card.Body>{renderTags()}</Card.Body>
+      <WhiteSpace />
       <Card.Body>
         <h1>{title}</h1>
         <p>{description}</p>
+      </Card.Body>
+      <WhiteSpace size="sm" />
+      <Card.Body>
+        <a class="view-more" href="">
+          View More
+        </a>
+      </Card.Body>
+      <Card.Body>
+        <div class="social-icons">
+          <div class="social-icon">
+            <img
+              style={{ width: "15px", height: "15px" }}
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQsRidJC_XT8z0t0r4nqJF2YTxJp7xP80vCky7UPRmKxAeTXvde&usqp=CAU"
+              alt="social"
+            />
+            <span>{numLikes}</span>
+          </div>
+          <div class="social-icon">
+            <img
+              style={{ width: "20px", height: "20px" }}
+              src="https://lh3.googleusercontent.com/-W4_-RfoF-Gj1Heb6ZePq4SbNThOn8cPb9NCOiV0HuBrzSU-V2YHkTO4rz6wUOO1tY0"
+              alt="social"
+            />
+            <span>{numComments}</span>
+          </div>
+          <div class="social-icon">
+            <img
+              style={{ width: "15px", height: "15px" }}
+              src="https://library.kissclipart.com/20180830/iuw/kissclipart-arrow-bullet-icon-clipart-computer-icons-arrow-bul-da4eb4d84fc18db6.png"
+              alt="social"
+            />
+            <span>{numShares}</span>
+          </div>
+        </div>
+      </Card.Body>
+      <Card.Body>
+        <TextInput
+          type={"text"}
+          style={commentStyles}
+          placeholder={"Write a comment ..."}
+        />
       </Card.Body>
     </CustomCard>
   );
--- client/src/components/Tag/FilterTag.js
+++ client/src/components/Tag/FilterTag.js
@@ -1,16 +1,20 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { Tag } from "antd-mobile";
 import { ROYAL_BLUE } from "../../constants/colors";
 
 const FilterTag = styled(Tag)`
+  &.am-tag-normal {
+    padding: 0 8px;
+  }
+
   &.am-tag-normal,
   &.am-tag-active {
     color: ${ROYAL_BLUE};
     font-size: 12px;
     margin: 5px 3px;
     &:before {
-      border: 1px solid ${ROYAL_BLUE} !important;
+      border: 2px solid ${ROYAL_BLUE} !important;
       border-radius: 40px !important;
     }
   }
