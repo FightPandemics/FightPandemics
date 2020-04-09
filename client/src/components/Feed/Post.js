0e30bda2058be26af2a5ff72ee1959fb18e20ad0
Annie Giang <annieg4123@gmail.com>
2020-04-09 13:01:45 -0700

    Create seperate file for PostCard

--- client/src/components/Feed/Post.js
+++ client/src/components/Feed/Post.js
@@ -1,91 +1,14 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { Card, WhiteSpace } from "antd-mobile";
+import { SELAGO } from "../../constants/colors";
+import PostCard from "./PostCard";
 import FilterTag from "../../components/Tag/FilterTag";
 import TextInput from "../../components/Input/TextInput";
-import { ROYAL_BLUE, DARK_GRAY, SELAGO } from "../../constants/colors";
 import HeartIcon from "../Icon/heart";
 import CommentIcon from "../Icon/comment";
 import ShareIcon from "../Icon/share";
 import StatusIcon from "../Icon/status-indicator";
-
-const CustomCard = styled(Card)`
-  margin-bottom: 65px;
-  border: unset !important;
-  &.am-card,
-  .am-card-body {
-    border-top: unset;
-    &::before {
-      content: normal !important;
-    }
-    .am-card-header {
-      display: block;
-      padding: 0;
-
-      .am-card-header-content {
-        align-items: unset;
-        font-size: 15px;
-
-        img {
-          margin-top: -5px;
-          margin-right: 7px;
-        }
-      }
-      .am-card-header-extra {
-        text-align: unset;
-        font-size: 13px;
-        margin-left: 47px;
-        margin-top: -13px;
-        .status-icon {
-          margin-right: 8px;
-        }
-      }
-    }
-    .am-card-body {
-      padding: 0;
-      color: black;
-
-      h1 {
-        font-size: 21px;
-        margin: 0;
-      }
-
-      p {
-        font-weight: 400;
-        font-size: 13.5px;
-        line-height: 20px;
-      }
-
-      .view-more {
-        color: ${ROYAL_BLUE};
-        font-weight: 500;
-      }
-
-      .test {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-      }
-
-      .social-icons {
-        display: flex;
-      }
-      .social-icon {
-        display: flex;
-        align-items: center;
-        color: ${DARK_GRAY};
-        margin-right: 46px;
-        cursor: pointer;
-        .social-icon-svg {
-          margin-right: 10px;
-        }
-        span {
-          font-size: 15px;
-        }
-      }
-    }
-  }
-`;
 
 export default ({ post }) => {
   const [expanded, setExpanded] = useState(false);
@@ -123,7 +46,7 @@ export default ({ post }) => {
   };
 
   return (
-    <CustomCard>
+    <PostCard>
       <Card.Header
         title={author}
         thumbStyle={thumbStyle}
@@ -172,6 +95,6 @@ export default ({ post }) => {
           placeholder={"Write a comment ..."}
         />
       </Card.Body>
-    </CustomCard>
+    </PostCard>
   );
 };
