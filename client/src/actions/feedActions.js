37e15668db288cf5389753fda2edb0acbd2c3b94
Godstime <lewisagho47@gmail.com>
2020-04-10 19:51:45 +0100

    Local update with remote branch

---
+++ client/src/actions/feedActions.js
@@ -0,0 +1,6 @@
+
+export const ADD_OPTION = "ADD_OPTION";
+export const REMOVE_OPTION = "REMOVE_OPTION";
+export const REMOVE_ALL_OPTIONS = "REMOVE_ALL_OPTIONS";
+export const TOGGLE_STATE = "TOGGLE_STATE";
+export const SET_VALUE = "SET_VALUE";
