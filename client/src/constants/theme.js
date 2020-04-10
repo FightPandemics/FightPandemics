4f13f30b926bcb41678b4f362dd0e80535ee0327
luxumbra <hello@crafted.im>
2020-04-10 02:54:09 +0100

    #88 Adding more to the theme file. * More typography * Media Queries (WIP) * Form elements

--- client/src/constants/theme.js
+++ client/src/constants/theme.js
@@ -1,6 +1,6 @@
 import { PRIMARY, ROYAL_BLUE } from "./colors";
 
-export const theme = {
+const theme = {
   global: {
     font: {
       family: "Work Sans",
@@ -9,6 +9,11 @@ export const theme = {
       border: {
         color: PRIMARY,
       },
+    },
+    colors: {
+      primary: PRIMARY,
+      royalBlue: ROYAL_BLUE,
+      darkGray: "#282828",
     },
   },
   typography: {
@@ -19,22 +24,35 @@ export const theme = {
       },
     },
     size: {
-      xsmall: "1rem",
-      small: "1.1rem",
-      medium: "1.2rem",
-      large: "1.4rem",
-      xlarge: "1.6rem",
+      // xxsmall: "1rem",
+      xsmall: "1.1rem",
+      small: "1.2rem",
+      medium: "1.4rem",
+      large: "1.6rem",
+      xlarge: "1.8rem",
+      xxlarge: "2.2rem",
+      xxlarge: "2.6rem",
     },
     heading: {
-      one: "3rem",
-      two: "2.4rem",
-      three: "2.2rem",
-      four: "2rem",
+      one: "4.2rem",
+      two: "3.2rem",
+      three: "2.6rem",
+      four: "2.2rem",
+    },
+    paragraph: {
+      center: {
+        textAlign: "center",
+      },
+      skip: {
+        textAlign: "center",
+        color: PRIMARY,
+      },
     },
   },
   colors: {
     primary: PRIMARY,
     royalBlue: ROYAL_BLUE,
+    darkGray: "#282828",
   },
   button: {
     border: {
@@ -44,7 +62,7 @@ export const theme = {
     },
     color: "#fff",
     padding: {
-      vertical: "10px",
+      vertical: "1rem",
     },
     primary: {
       color: PRIMARY,
@@ -59,4 +77,62 @@ export const theme = {
       stroke: PRIMARY,
     },
   },
+  form: {
+    label: {
+      color: PRIMARY,
+      fontSize: "2rem",
+      marginBottom: "0.5rem",
+    },
+    input: {
+      fontSize: "2rem",
+    },
+    button: {
+      color: "#fff",
+      backgroundColor: ROYAL_BLUE,
+      borderColor: ROYAL_BLUE,
+    },
+  },
 };
+const mq = {
+  phone: {
+    narrow: {
+      minWidth: 0,
+      maxWidth: "449px",
+      min: "(min-width: 0)",
+      max: "(max-width: 449px)",
+    },
+    wide: {
+      minWidth: "450px",
+      maxWidth: "767px",
+      min: "(min-width: 450px)",
+      max: "(max-width: 767px)",
+    },
+  },
+  tablet: {
+    narrow: {
+      minWidth: "768px",
+      maxWidth: "1023px",
+      min: "(min-width: 768px)",
+      max: "(max-width:1023px)",
+    },
+    wide: {
+      minWidth: "1024px",
+      maxWidth: "1279px",
+    },
+  },
+  desktop: {
+    small: {
+      minWidth: "1280px",
+      maxWidth: "1439px",
+    },
+    medium: {
+      minWidth: "1440px",
+      maxWidth: "1919px",
+    },
+    large: {
+      minWidth: "1920px",
+    },
+  },
+};
+
+export { theme, mq };
