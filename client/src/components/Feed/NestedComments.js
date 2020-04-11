0284ca51b51f666ccdbda7703eb19f6a50f083c9
Annie Giang <annieg4123@gmail.com>
2020-04-11 15:34:54 -0700

    Style comment likes and add comment like/unlike functionality

--- client/src/components/Feed/NestedComments.js
+++ client/src/components/Feed/NestedComments.js
@@ -1,73 +1,72 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
 import { Comment, Avatar } from "antd";
-import { LIGHTER_GRAY } from "../../constants/colors";
-
-const StyledComment = styled(Comment)`
-  &.ant-comment {
-    .ant-comment-inner {
-      padding: 1rem 0;
-      .ant-comment-content {
-        position: relative;
-        max-width: 30rem;
-        .ant-comment-content-author {
-          border-top-left-radius: 1.5rem;
-          border-top-right-radius: 1.5rem;
-          margin-bottom: 0;
-          padding: 1rem 0 0.5rem 1.5rem;
-          .ant-comment-content-author-name > * {
-            color: black;
-            font-weight: 500;
-            font-size: 1.4rem;
-          }
-        }
-        .ant-comment-content-detail {
-          border-bottom-left-radius: 1.5rem;
-          border-bottom-right-radius: 1.5rem;
-          padding: 0 1.5rem 1.5rem;
-          font-size: 1.4rem;
-          line-height: 140%;
-        }
-        .ant-comment-actions {
-          margin-top: 0;
-          padding-left: 1.5rem;
-          li span {
-            padding-right: 2.3rem;
-            font-size: 1.1rem;
-            color: black;
-          }
-        }
-        .ant-comment-content-author,
-        .ant-comment-content-detail {
-          background-color: ${LIGHTER_GRAY};
-        }
-      }
-    }
-  }
-`;
+import { ROYAL_BLUE } from "../../constants/colors";
+import HeartSmallIcon from "../Icon/heart-small";
+import StyledComment from "./StyledComment";
 
 const NestedComments = ({ comment }) => {
+  const [likedComment, setLikedComment] = useState(false);
+  const [fakeNumLikes, setFakeNumLikes] = useState(comment.numLikes);
+
+  const handleLikeComment = () => {
+    likedComment
+      ? setFakeNumLikes(fakeNumLikes - 1)
+      : setFakeNumLikes(fakeNumLikes + 1);
+    setLikedComment(!likedComment);
+  };
+
+  const renderLikeButton = () => {
+    const style = likedComment ? { color: ROYAL_BLUE, fontWeight: "bold" } : {};
+    return (
+      <span style={style} onClick={handleLikeComment} key="comment-basic-like">
+        Like
+      </span>
+    );
+  };
+
+  const renderNumLikes =
+    comment.numLikes > 0 ? (
+      <span className="comment-likes">
+        <HeartSmallIcon />
+        {fakeNumLikes}
+      </span>
+    ) : (
+      ""
+    );
+
+  const renderReplyTo = <span key="comment-nested-reply-to">Reply</span>;
+  const renderTimeStamp = <span>1w</span>;
+
+  const commentActions = [
+    // order matters
+    renderTimeStamp,
+    renderLikeButton(),
+    renderReplyTo,
+    renderNumLikes,
+  ];
+
+  const renderAvatar = (
+    <Avatar
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTGhWTUkY0xGbbdHyReD6227iz53ADtRmcn1PTN4GUS3clC6MCT&usqp=CAU"
+      alt={`${comment.name}`}
+    />
+  );
+
   const nestedComments = (comment.children || []).map((comment) => {
     return <NestedComments comment={comment} key={comment._id} />;
   });
+
   return (
-    <StyledComment
-      actions={[
-        <span>1w</span>,
-        <span key="comment-basic-like">Like</span>,
-        <span key="comment-nested-reply-to">Reply</span>,
-      ]}
-      author={<span>{comment.name}</span>}
-      avatar={
-        <Avatar
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTGhWTUkY0xGbbdHyReD6227iz53ADtRmcn1PTN4GUS3clC6MCT&usqp=CAU"
-          alt={`${comment.name}`}
-        />
-      }
-      content={<p>{comment.comment}</p>}
-    >
-      {nestedComments}
-    </StyledComment>
+    <div>
+      <StyledComment
+        actions={commentActions}
+        author={<span>{comment.name}</span>}
+        avatar={renderAvatar}
+        content={<p>{comment.comment}</p>}
+      >
+        {nestedComments}
+      </StyledComment>
+    </div>
   );
 };
 
---
+++ client/src/components/Feed/StyledComment.js
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+import { LIGHTER_GRAY, DARK_GRAY } from "../../constants/colors";
+
+export default styled(Comment)`
+  &.ant-comment {
+    .ant-comment-inner {
+      padding: 1rem 0;
+      .ant-comment-content {
+        max-width: 30rem;
+        .ant-comment-content-author {
+          border-top-left-radius: 1.5rem;
+          border-top-right-radius: 1.5rem;
+          margin-bottom: 0;
+          padding: 1rem 0 0.5rem 1.5rem;
+          .ant-comment-content-author-name > * {
+            color: black;
+            font-weight: 500;
+            font-size: 1.4rem;
+          }
+        }
+        .ant-comment-content-detail {
+          border-bottom-left-radius: 1.5rem;
+          border-bottom-right-radius: 1.5rem;
+          padding: 0 1.5rem 1.5rem;
+          font-size: 1.4rem;
+          line-height: 140%;
+        }
+        .ant-comment-actions {
+          position: relative;
+          margin-top: 0;
+          padding-left: 1.5rem;
+          li span {
+            padding-right: 2.3rem;
+            font-size: 1.1rem;
+            color: black;
+          }
+          .comment-likes {
+            background: #fff;
+            box-shadow: 0px 0.1rem 0.4rem rgba(0, 0, 0, 0.06);
+            border-radius: 0.85rem;
+            position: absolute;
+            min-width: 3.6rem;
+            min-height: 1.7rem;
+            right: -5px;
+            top: -40%;
+            padding: 0.2rem 0.3rem;
+            font-size: 11px;
+            color: ${DARK_GRAY};
+            img {
+              padding-left: 0.2rem;
+              padding-right: 0.7rem;
+            }
+          }
+        }
+        .ant-comment-content-author,
+        .ant-comment-content-detail {
+          background-color: ${LIGHTER_GRAY};
+        }
+      }
+    }
+  }
+`;
