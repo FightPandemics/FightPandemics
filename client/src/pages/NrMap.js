e496a644a4077fa3c91cd5b7701a4c8df7b34968
PriyalPatney <34459102+PriyalPatney@users.noreply.github.com>
2020-04-12 22:22:35 +0530

    removed css & refactoring

--- client/src/pages/NrMap.js
+++ client/src/pages/NrMap.js
@@ -1,8 +1,26 @@
 import React, { Component, createRef } from "react";
 import Script from "react-load-script";
-import "./styles/NrMap.css";
 
 const url = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_KEY}&libraries=places`;
+
+const Wrapper = {
+  display: "grid",
+  gridTemplateColumns: "repeat(auto-fill, minmax(400px, 4fr))",
+  gridColumnGap: "0.5em",
+};
+
+const Nested = {
+  height: "400px",
+  overflowY: "auto",
+};
+
+const MapStyle = {
+  margin: "2% auto",
+  width: "100%",
+  maxWidth: "800px",
+  height: "50vh",
+  minHeight: "300px",
+};
 
 class NrMap extends Component {
   constructor(props) {
@@ -10,8 +28,8 @@ class NrMap extends Component {
   }
 
   handleScriptLoad() {
-    var map;
-    var pyrmont = new window.google.maps.LatLng(13.0827, 80.2707);
+    let map;
+    let pyrmont = new window.google.maps.LatLng(13.0827, 80.2707);
 
     map = new window.google.maps.Map(document.getElementById("map"), {
       center: pyrmont,
@@ -19,49 +37,34 @@ class NrMap extends Component {
       disableDefaultUI: true,
     });
 
-    var marker = new window.google.maps.Marker({
-      position: pyrmont,
-      map: map,
-      draggable: true,
-    });
-
-    marker.setMap(map);
-
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         function (position) {
-          var pos = new window.google.maps.LatLng(
+          let pos = new window.google.maps.LatLng(
             position.coords.latitude,
             position.coords.longitude,
           );
 
-          var infowindow = new window.google.maps.InfoWindow({
+          new window.google.maps.InfoWindow({
             map: map,
-            // position: pos,
-            // content: 'Great! your location is found.'
           });
 
           map.setCenter(pos);
-          var service = new window.google.maps.places.PlacesService(map);
+          let service = new window.google.maps.places.PlacesService(map);
           service.nearbySearch(
             {
               location: pos,
               radius: 3000,
               type: ["hospital"],
             },
-            function (results, status, pagination) {
+            function (results, status) {
               if (status !== "OK") return;
-
               createMarkers(results);
-              pagination.hasNextPage &&
-                function () {
-                  pagination.nextPage();
-                };
             },
           );
 
           function createMarkers(places) {
-            var bounds = new window.google.maps.LatLngBounds();
+            let bounds = new window.google.maps.LatLngBounds();
             for (var i = 0, place; (place = places[i]); i++) {
               var request = {
                 placeId: place.place_id,
@@ -80,26 +83,25 @@ class NrMap extends Component {
                 if (
                   status == window.google.maps.places.PlacesServiceStatus.OK
                 ) {
-                  var board = document.createElement("div");
-                  board.className = "card";
-                  var str = "";
-                  console.log(place);
-                  str += '<div class="card-body">';
-                  str += '<h3 class="card-title">' + place.name + "</h3>";
+                  let board = document.createElement("div");
+                  let str = "";
+                  str +=
+                    '<div style="border: 1px solid #eee;box-shadow: 0 2px 6px #bababa;border-radius: 10px;padding: 20px;margin: 10px;">';
+                  str += "<h3>" + place.name + "</h3>";
                   str +=
                     '<div><p class="card-text">' +
                     place.formatted_address +
                     '</p><p style="color: #425af2;" }>' +
                     place.formatted_phone_number +
                     "</p></div>";
-                  var image = {
+                  let image = {
                     url: place.icon,
                     size: new window.google.maps.Size(71, 71),
                     origin: new window.google.maps.Point(0, 0),
                     anchor: new window.google.maps.Point(17, 34),
                     scaledSize: new window.google.maps.Size(25, 25),
                   };
-                  var marker = new window.google.maps.Marker({
+                  new window.google.maps.Marker({
                     map: map,
                     icon: image,
                     title: place.name,
@@ -140,7 +142,7 @@ class NrMap extends Component {
         content: content,
       };
 
-      var infowindow = new window.google.maps.InfoWindow(options);
+      new window.google.maps.InfoWindow(options);
       map.setCenter(options.position);
     }
   }
@@ -149,12 +151,12 @@ class NrMap extends Component {
     return (
       <div>
         <Script url={url} onLoad={this.handleScriptLoad} />
-        <div className="wrapper">
-          <div className="division">
-            <div className="nested" id="board"></div>
+        <div style={Wrapper}>
+          <div>
+            <div style={Nested} id="board"></div>
           </div>
-          <div className="division">
-            <div id="map"></div>
+          <div>
+            <div style={MapStyle} id="map"></div>
           </div>
         </div>
       </div>
