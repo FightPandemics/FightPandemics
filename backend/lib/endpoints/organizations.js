d1729dd06a234ce08fde5c7f24f47dd8433e3c7e
Manny Karyampudi <mannykary@users.noreply.github.com>
2020-04-11 17:42:04 -0700

    Add organizations endpoint

---
+++ backend/lib/endpoints/organizations.js
@@ -0,0 +1,95 @@
+const httpErrors = require("http-errors");
+
+const {
+  createOrganizationSchema,
+  deleteOrganizationSchema,
+  getOrganizationSchema,
+  getOrganizationsSchema,
+  updateOrganizationSchema,
+} = require("./schema/organizations");
+
+/*
+ * /api/organizations
+ */
+async function routes(app) {
+  const Organization = app.mongo.model("Organization");
+
+  app.delete(
+    "/:organizationId",
+    {
+      preValidation: [app.authenticate],
+      schema: deleteOrganizationSchema,
+    },
+    async (req) => {
+      // TODO: make sure user can only delete organizations they own
+      return Organization.findByIdAndRemove(req.params.organizationId);
+    },
+  );
+
+  app.get(
+    "/",
+    {
+      preValidation: [app.authenticate],
+      schema: getOrganizationsSchema,
+    },
+    async (req, reply) => {
+      const { ownerId } = req.params;
+      const filter = ownerId ? { ownerId } : {};
+      const sortedOrganizations = await Organization.find(filter).sort({
+        name: 1,
+      });
+      return reply.send(sortedOrganizations);
+    },
+  );
+
+  app.get(
+    "/:organizationId",
+    {
+      preValidation: [app.authenticate],
+      schema: getOrganizationSchema,
+    },
+    async (req) => {
+      const result = await Organization.findById(req.params.organizationId);
+      if (result === null) {
+        return new httpErrors.NotFound();
+      }
+      return result;
+    },
+  );
+
+  app.patch(
+    "/:organizationId",
+    {
+      preValidation: [app.authenticate],
+      schema: updateOrganizationSchema,
+    },
+    async (req, reply) => {
+      // TODO: make sure user can only update organizations they own
+      const organization = await Organization.findById(
+        req.params.organizationId,
+      );
+      if (organization === null) {
+        return reply.send(new httpErrors.NotFound());
+      }
+      Organization.schema.eachPath((key) => {
+        if (req.body[key] && organization[key] !== req.body[key]) {
+          organization[key] = req.body[key];
+        }
+      });
+      return organization.save();
+    },
+  );
+
+  app.post(
+    "/",
+    {
+      preValidation: [app.authenticate],
+      schema: createOrganizationSchema,
+    },
+    async (req) => {
+      return new Organization(req.body).save();
+    },
+  );
+}
+
+module.exports = routes;
