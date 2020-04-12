9ac3c20fdbfb076d2b0bb952ba71e99e86690b1b
luxumbra <hello@crafted.im>
2020-04-12 14:02:54 +0100

    Switched to using `antd-mobile` for the input control so we have the onChange prop.

--- client/src/components/StepWizard/StyledTextInput.js
+++ client/src/components/StepWizard/StyledTextInput.js
@@ -1,13 +1,19 @@
 import styled from "styled-components";
-import TextInput from "../Input/TextInput";
+import { InputItem } from "antd-mobile";
 import { theme } from "../../constants/theme";
 
-export default styled(TextInput)`
-  ${theme.form.input}
-  border-color: ${theme.colors.primary};
-  border-width: 0 0 0.1rem 0;
-  color: ${theme.colors.darkGray};
-  padding: 0.5rem 0;
-  margin: 0.5rem 0;
+export default styled(InputItem)`
   width: 100%;
+
+  .am-input-control {
+
+    input {
+      ${theme.form.input}
+      color: ${theme.colors.darkGray};
+      border: 0.1rem solid ${theme.colors.primary};
+      border-width: 0 0 0.1rem 0;
+      padding: 0.5rem 0;
+      margin: 0.5rem 0 1rem;
+    }
+  }
 `;
--- client/src/components/StepWizard/WizardButtonGroup.js
+++ client/src/components/StepWizard/WizardButtonGroup.js
@@ -2,4 +2,5 @@ import styled from "styled-components";
 
 export default styled.div`
   flex: 0;
+  margin-top: 5rem;
 `;
