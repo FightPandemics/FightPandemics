884433e5ff3e24f347a33312f781a5e60aa75d80
Robin Venneman <robin.venneman@gmail.com>
2020-04-13 13:22:19 +0200

    update post & comment schema, initial logic to add comments

--- backend/lib/models/schemas/Comment.js
+++ backend/lib/models/schemas/Comment.js
@@ -1,20 +1,38 @@
 const { Schema } = require("mongoose");
 
-const CommentSchema = new Schema({
-  authorId: {
-    ref: "User",
-    required: true,
-    type: Schema.Types.ObjectId,
+const CommentSchema = new Schema(
+  {
+    authorId: {
+      ref: "User",
+      required: true,
+      type: Schema.Types.ObjectId,
+    },
+    comment: {
+      required: true,
+      type: String,
+    },
+    createdAt: {
+      required: true,
+      type: Date,
+    },
+    parentId: {
+      ref: "Comment",
+      type: Schema.Types.ObjectId,
+    },
+    postId: {
+      ref: "Post",
+      required: true,
+      type: Schema.Types.ObjectId,
+    },
   },
-  comment: {
-    required: true,
-    type: String,
+  {
+    timestamps: true,
   },
-  postId: {
-    ref: "Post",
-    required: true,
-    type: Schema.Types.ObjectId,
-  },
+);
+
+CommentSchema.index({
+  createdAt: 1,
+  postId: 1,
 });
 
 module.exports = CommentSchema;
