8ed2bc7d23ee231ed5bb3e00f7a6c06fa6a10c79
PriyalPatney <34459102+PriyalPatney@users.noreply.github.com>
2020-04-15 01:33:34 +0530

    code for review

--- client/src/pages/NearestHospital.js
+++ client/src/pages/NearestHospital.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NrMap from "./NrMap.js";
+import Exp from "./mapexample.js";
 
 class NearestHospital extends Component {
   render() {
@@ -8,6 +9,7 @@ class NearestHospital extends Component {
         <br />
         <h2>Your Nearest Hospital</h2>
         <NrMap />
+        {/* <Exp /> */}
       </div>
     );
   }
