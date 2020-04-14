ac9a8ff87abce60548a8c75914fa6a693b9aa1b4
pishili <pishili@users.noreply.github.com>
2020-04-13 21:19:56 -0700

    Adds intial endpoint for createProfile

--- backend/lib/endpoints/users.js
+++ backend/lib/endpoints/users.js
@@ -35,6 +35,45 @@ async function routes(app) {
       };
     },
   );
+
+  // app.post("/signup/createProfile", async (req, reply) => {
+  //   try {
+  //     const { email, name, country, neighborhood } = req.body;
+  //     console.log(email)
+
+  //     pool.query(
+  //       `INSERT INTO users (email, name, country, neighborhood)
+  //        VALUES ($1::VARCHAR, $2:: VARCHART, $3::VARCHAR)
+  //        `,
+  //        [email, name, country, neighborhood]
+  //     )
+  //     .then((res) => {
+  //       reply.code(204);
+  //     })
+  //   } catch (err) {
+  //     return reply.code(err.statusCode).send(err);s
+  //   }
+  // });
+
+  app.post(
+    "/signup/createProfile", async (req, reply) => {
+      try {
+        const { email, name, country, neighborhood } = req.body;
+        console.log(email)
+        console.log(name)
+        console.log(country)
+        console.log(neighborhood)
+        // const newComment = await new Comment(req.body).save();
+        const updatedPost = await Post.findOneAndUpdate(
+          { _id: postId },
+          { $push: { comments: newComment } },
+          { new: true },
+        );
+        return updatedPost;
+      } catch (err) {
+        return reply.code(err.statusCode).send(err);
+      }
+    });
 }
 
 module.exports = routes;
