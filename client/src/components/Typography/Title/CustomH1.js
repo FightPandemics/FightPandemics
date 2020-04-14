11a8bb6377189b55940870ddebb1ed25f7546d56
Annie Giang <annieg4123@gmail.com>
2020-04-14 16:38:12 -0700

    Add create post title

--- client/src/components/Typography/Title/CustomH1.js
+++ client/src/components/Typography/Title/CustomH1.js
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 import { DARK_GRAY } from "../../../constants/colors";
 
-export default styled.h5`
+export default styled.h1`
+  font-family: "Poppins";
   color: ${(props) => props.color};
   font-size: ${(props) => props.fontsize};
+  font-weight: ${(props) => props.fontweight};
   margin: 0.5rem;
 `;
