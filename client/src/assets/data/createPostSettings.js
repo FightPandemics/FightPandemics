2c85b2ee80cbc87bf31935e637c45fc245d4a1d5
Annie Giang <annieg4123@gmail.com>
2020-04-15 00:06:34 -0700

    Add default value in settings

--- client/src/assets/data/createPostSettings.js
+++ client/src/assets/data/createPostSettings.js
@@ -2,21 +2,23 @@ export default {
   shareWith: {
     type: "shareWith",
     label: "Share with ...",
+    default: "Worldwide",
     options: [
-      { label: "In my Zip Code", value: "zipCode" },
-      { label: "In my State", value: "state" },
-      { label: "In my Country", value: "country" },
-      { label: "Worldwide", value: "worldwide" },
+      { label: "In my Zip Code", value: "Zip Code" },
+      { label: "In my State", value: "State" },
+      { label: "In my Country", value: "Country" },
+      { label: "Worldwide", value: "Worldwide" },
     ],
   },
   expires: {
     type: "expires",
     label: "For how long do you want to keep your post?",
+    default: "Forever",
     options: [
       { label: "Forever", value: "Forever" },
-      { label: "For a month", value: "month" },
-      { label: "For a week", value: "week" },
-      { label: "For a day", value: "day" },
+      { label: "For a month", value: "Month" },
+      { label: "For a week", value: "Week" },
+      { label: "For a day", value: "Day" },
     ],
   },
   helpTypes: {
