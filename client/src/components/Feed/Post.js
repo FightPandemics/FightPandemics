efe7e80de35acd4dc6e04d238a5fa1240d211d6a
Godstime <lewisagho47@gmail.com>
2020-04-15 16:41:14 +0100

    Did some update

--- client/src/components/Feed/Post.js
+++ client/src/components/Feed/Post.js
@@ -1,120 +1,131 @@
 import React, { useState } from "react";
-import styled from "styled-components";
 import { Modal, Card, WhiteSpace } from "antd-mobile";
 import PostCard from "./PostCard";
 import PostSocial from "./PostSocial";
 import Comments from "./Comments";
 import FilterTag from "../../components/Tag/FilterTag";
 import StatusIcon from "../Icon/status-indicator";
-import TextInput from "../../components/Input/TextInput";
-import { SELAGO } from "../../constants/colors";
+import AutoSize from "../../components/Input/AutoSize";
 
 export default ({ post }) => {
-  const {
-    title,
-    description,
-    author,
-    photoUrl,
-    location,
-    tags,
-    numLikes,
-    numComments,
-    numShares,
-    url,
-    comments,
-  } = post;
-
   const [showComments, setShowComments] = useState(false);
   const [copied, setCopied] = useState(false);
-
   // mock API to test functionality
   const [liked, setLiked] = useState(false);
   const [shared, setShared] = useState(false);
-  const [fakeLikes, setFakeLikes] = useState(numLikes);
-  const [fakeComments, setFakeComments] = useState(numComments);
-  const [fakeShares, setFakeShares] = useState(numShares);
+  const [comment, setComment] = useState("");
+  const [fakeLikes, setFakeLikes] = useState(post.numLikes);
+  const [fakeComments, setFakeComments] = useState(post.numComments);
+  const [fakeShares, setFakeShares] = useState(post.numShares);
 
-  const thumbStyle = {
-    borderRadius: "40px",
-    width: "40px",
-    height: "40px",
-    maxWidth: "100%",
+  const handleComment = (e) => {
+    e.preventDefault();
+    const testNewComment = {
+      _id: 10,
+      name: "Guest User",
+      numLikes: 0,
+      children: [],
+      comment,
+    };
+    post.comments.push(testNewComment); // not good but mocking API and testing UI
+    setFakeComments(fakeComments + 1);
+    setShowComments(true);
+    setComment("");
   };
 
-  const commentStyles = {
-    backgroundColor: SELAGO,
-    width: "96%",
-    borderBottom: "unset",
-    borderRadius: "40px",
-    padding: "14px",
-  };
+  const renderHeader = (
+    <Card.Header
+      title={post.author}
+      thumb={post.photoUrl}
+      extra={
+        <span>
+          <StatusIcon className="status-icon" />
+          {post.location}
+        </span>
+      }
+    />
+  );
 
-  const renderTags = () => {
-    return tags.map((tag, idx) => (
-      <FilterTag label={tag} selected={false} disabled={true} key={idx} />
-    ));
-  };
+  const renderContent = (
+    <Card.Body>
+      <h1>{post.title}</h1>
+      <p className="post-description">{post.description}</p>
+    </Card.Body>
+  );
+
+  const renderTags = (
+    <Card.Body>
+      {post.tags.map((tag, idx) => (
+        <FilterTag label={tag} selected={false} disabled={true} key={idx} />
+      ))}
+    </Card.Body>
+  );
+
+  const renderViewMore = (
+    <Card.Body>
+      <span className="view-more">View More</span>
+    </Card.Body>
+  );
+
+  const renderComments = (
+    <Card.Body>
+      <AutoSize
+        placeholder={"Write a comment..."}
+        onPressEnter={handleComment}
+        onChange={(e) => setComment(e.target.value)}
+        value={comment}
+      />
+      {showComments ? <Comments comments={post.comments} /> : ""}
+    </Card.Body>
+  );
+
+  const renderSocialIcons = (
+    <Card.Body>
+      <PostSocial
+        url={post.url}
+        liked={liked}
+        shared={shared}
+        showComments={showComments}
+        numLikes={fakeLikes}
+        numComments={fakeComments}
+        numShares={fakeShares}
+        setShowComments={() => setShowComments(!showComments)}
+        onCopyLink={() => {
+          if (!shared) setFakeShares(fakeShares + 1);
+          setShared(true);
+          return setCopied(!copied);
+        }}
+        likePost={() => {
+          liked ? setFakeLikes(fakeLikes - 1) : setFakeLikes(fakeLikes + 1);
+          return setLiked(!liked);
+        }}
+      />
+    </Card.Body>
+  );
+
+  const renderShareModal = (
+    <Modal
+      onClose={() => setCopied(!copied)}
+      maskClosable={true}
+      closable={true}
+      visible={copied}
+      transparent
+    >
+      <h1 style={{ color: "black" }}>Link Copied!</h1>
+    </Modal>
+  );
 
   return (
     <PostCard>
-      <Card.Header
-        title={author}
-        thumbStyle={thumbStyle}
-        extra={
-          <span>
-            <StatusIcon className="status-icon" />
-            {location}
-          </span>
-        }
-        thumb={photoUrl}
-      />
-      <WhiteSpace size="lg" />
-      <Card.Body>{renderTags()}</Card.Body>
+      {renderHeader}
+      <WhiteSpace size="md" />
+      {renderTags}
       <WhiteSpace />
-      <Card.Body>
-        <h1>{title}</h1>
-        <p>{description}</p>
-      </Card.Body>
-      <Card.Body>
-        <a className="view-more" href="">
-          View More
-        </a>
-      </Card.Body>
-      <Card.Body>
-        <PostSocial
-          url={url}
-          numLikes={fakeLikes}
-          numComments={fakeComments}
-          numShares={fakeShares}
-          setShowComments={() => setShowComments(!showComments)}
-          onCopyLink={() => {
-            if (!shared) setFakeShares(fakeShares + 1);
-            setShared(true);
-            return setCopied(!copied);
-          }}
-          likePost={() => {
-            liked ? setFakeLikes(fakeLikes - 1) : setFakeLikes(fakeLikes + 1);
-            return setLiked(!liked);
-          }}
-        />
-      </Card.Body>
-      <Card.Body>
-        <TextInput
-          type={"text"}
-          style={commentStyles}
-          placeholder={"Write a comment ..."}
-        />
-        {showComments ? <Comments comments={comments} /> : ""}
-      </Card.Body>
-      <Modal
-        onClose={() => setCopied(!copied)}
-        maskClosable={true}
-        closable={true}
-        visible={copied}
-        transparent
-      >
-        <h1 style={{ color: "black" }}>Link Copied!</h1>
-      </Modal>
+      {renderContent}
+      {renderViewMore}
+      {renderSocialIcons}
+      {renderComments}
+      {renderShareModal}
     </PostCard>
   );
 };
