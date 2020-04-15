d38f4dd72db7b02fd57c3ce418158d0c999237ad
pishili <pishili@users.noreply.github.com>
2020-04-15 09:57:50 -0700

    Removes req body parameters

--- backend/lib/endpoints/schema/users.js
+++ backend/lib/endpoints/schema/users.js
@@ -5,6 +5,21 @@ const getUserByIdSchema = {
   params: strictSchema().prop("userId", S.string().required()),
 };
 
+        // return new User({
+        //   "email": email,
+        //   "name": name,
+        //   "country": country,
+        //   "neighborhood": neighborhood
+        // }).save();
+const createProfileSchema = {
+  body: S.object()
+    .prop("email", S.string().required())
+    .prop("name", S.string().required())
+    .prop("country", S.string().required())
+    .prop("neighborhood", S.string().required()),
+};
+
 module.exports = {
   getUserByIdSchema,
+  createProfileSchema,
 };
