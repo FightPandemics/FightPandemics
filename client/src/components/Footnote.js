1306d953f972252f4f85478a83af244882ab9e75
Nadine Ju <nadine.jura@online.de>
2020-04-23 07:06:55 -0700

    using stlyed-components

--- client/src/components/Footnote.js
+++ client/src/components/Footnote.js
@@ -1,70 +1,68 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import styled from "styled-components";
 
-const footerStyle = {
+const StyledFooter = styled.div`
   position: "absolute",
   width: "376px",
   height: "71px",
   left: "0",
+  bottom: "0",
   top: "759px",
 
   background: "#F3F4FE",
   textAlign: "center",
   lineHeight: "15px",
-  fontSize: "11px",
-};
+  fontFamily: "Poppins",
+  fontSize: "11px"
+`;
 
-const copyrightStyle = {
+const Copyright = styled.div`
   position: "absolute",
   width: "312px",
   height: "21px",
   left: "28px",
   top: "18px",
 
-  fontFamily: "Poppins",
   fontWeight: "500",
   color: "#282828",
-};
+`;
 
-const policies = {
+const Policies = styled.div`
   position: "absolute",
   width: "326px",
   height: "15px",
   left: "21px",
   top: "39px",
 
-  fontFamily: "Poppins",
   textDecorationLine: "underline",
-};
+`;
 
-const footerLink = {
-  color: "#939393",
-};
+const FooterLink = styled(Link)`
+  color: "#939393";
+`;
 
 export default () => {
   const currentYear = new Date().getFullYear();
   return (
-    <footer style={footerStyle}>
-      <div style={copyrightStyle}>
+    <StyledFooter>
+      <Copyright>
         Copyright {currentYear} FightPandemics. All rights reserved.
-      </div>
-      <div style={policies}>
-        <a
-          style={footerLink}
-          href="https://fightpandemics.com/terms-conditions"
-        >
+      </Copyright>
+      <Policies>
+        <FooterLink href="https://fightpandemics.com/terms-conditions">
           Terms & Conditions
-        </a>{" "}
+        </FooterLink>{" "}
         |{" "}
-        <a style={footerLink} href="https://fightpandemics.com/privacy-policy">
+        <FooterLink href="https://fightpandemics.com/privacy-policy">
           Privacy Policy
-        </a>{" "}
+        </FooterLink>{" "}
         |{" "}
-        <a style={footerLink} href="https://fightpandemics.com/cookies-policy">
+        <FooterLink href="https://fightpandemics.com/cookies-policy">
           Cookies Policy
-        </a>
-      </div>
-    </footer>
+        </FooterLink>
+      </Policies>
+    </StyledFooter>
   );
 };
