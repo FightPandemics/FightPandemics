c0771f1047ac4f205982ea3eefb8ee2a14c01539
João Fernandes <joaofnds@joaofnds.com>
2020-04-25 20:38:31 -0300

    Avoid trying to load directories when loading models

--- backend/lib/models/index.js
+++ backend/lib/models/index.js
@@ -1,11 +1,14 @@
-const { readdirSync } = require("fs");
+const { readdirSync, statSync } = require("fs");
 const { join } = require("path");
 
-const PATH_TO_SCHEMAS = join(__dirname, "./schemas");
+const PATH_TO_SCHEMAS = join(__dirname, "schemas");
 
 module.exports = function registerModels(mongoConnection) {
   readdirSync(PATH_TO_SCHEMAS).forEach((fileName) => {
     const filePath = join(PATH_TO_SCHEMAS, fileName);
+
+    if (statSync(filePath).isDirectory()) return;
+
     const schema = require(filePath); // eslint-disable-line import/no-dynamic-require
     const modelName = fileName.replace(".js", "");
     mongoConnection.model(modelName, schema);
