e594b4c227655646ce4aa487d61571d9ef2fee95
tennga <tenngawang6080@gmail.com>
2020-04-28 18:57:52 -0400

    styling for home page, desktop version

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -18,6 +18,20 @@ const FlexChild = styled.div`
 
 const StyledIntro = styled.div`
   margin-top: 4rem;
+
+  @media only screen and (min-width: 600px) {  
+    background-color: ${theme.colors.royalBlue};
+    border-radius: 1rem;
+    padding-left: 8rem;
+    padding-top: 16rem; 
+    width: 100%;
+  }
+`;
+
+const MainContainer = styled.div`
+  @media only screen and (min-width: 600px){
+    display:flex
+  }
 `;
 
 const StyledWelcome = styled(Heading)`
@@ -28,12 +42,29 @@ const StyledWelcome = styled(Heading)`
   line-height: 3rem;
   margin: 2.5rem auto 0;
   text-align: center;
+
+  @media only screen and (min-width: 600px) {  
+    font-size: ${theme.typography.heading.one};
+    text-align: left;
+  }
 `;
 
 const StyledStrapline = styled(StyledWelcome)`
   font-weight: bold;
   margin: 0 auto;
   margin-bottom: 1.5rem;
+
+  @media only screen and (min-width: 600px){
+    color: white;
+    line-height: 5rem;
+  }
+
+`;
+
+const IntroText = styled.div`
+@media only screen and (min-width: 600px){
+  width: 70%;
+}
 `;
 
 const StyledP = styled.p`
@@ -43,24 +74,42 @@ const StyledP = styled.p`
   line-height: 2.1rem;
   letter-spacing: 0.1px;
   margin: 0;
+  
+  @media only screen and (min-width: 600px){
+    font-size: ${theme.typography.size.large};
+    text-align: left;
+    color: white;
+  }
 `;
 
 const OnboardingContainer = styled.div`
   margin-top: 4rem;
+
+  @media only screen and (min-width: 600px){
+    display: "flex";
+    flexWrap: "wrap";
+    width: 100%;
+    justify-content: center;
+  }
 `;
 
 export const Home = (props) => {
   console.log("render home", { props });
   return (
-    <div className="text-center">
+    <MainContainer className="text-center">
+
       <StyledIntro>
-        <StyledStrapline level={2} margin="none">
-          A place to give and get help
-        </StyledStrapline>
-        <StyledP>Pandemics will continue to happen.</StyledP>
-        <StyledP>We help communities prepare and respond.</StyledP>
+        <IntroText>
+          <StyledStrapline level={2} margin="none">
+            A place to give and get help
+          </StyledStrapline>
+          <StyledP>Pandemics will continue to happen.</StyledP>
+          <StyledP>We help communities prepare and respond.</StyledP>
+        </IntroText>
       </StyledIntro>
-      <OnboardingContainer style={{ display: "flex", flexWrap: "wrap" }}>
+
+      <>
+      <OnboardingContainer>
         <FlexChild>
           <ImageButton
             type="ghost"
@@ -81,20 +130,20 @@ export const Home = (props) => {
             Give Help
           </ImageButton>
         </FlexChild>
+      
+          <Link
+            style={{
+              color: theme.colors.royalBlue,
+              fontSize: theme.typography.size.large,
+              fontFamily: theme.typography.font.family.display,
+              fontWeight: "500",
+            }}
+            to="/feed"
+          >
+            View Community Postings
+          </Link>
       </OnboardingContainer>
-      <p style={{ marginTop: "2rem" }}>
-        <Link
-          style={{
-            color: theme.colors.royalBlue,
-            fontSize: theme.typography.size.large,
-            fontFamily: theme.typography.font.family.display,
-            fontWeight: "500",
-          }}
-          to="/feed"
-        >
-          View Community Postings
-        </Link>
-      </p>
-    </div>
+      </>
+    </MainContainer>
   );
 };
