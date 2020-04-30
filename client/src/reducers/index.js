41f6cc49daf4d54ad94ac4f875e47cea3a85123b
Annie <annieg4123@gmail.com>
2020-04-30 14:32:55 -0700

    Create user slice of state in redux state

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import session from "./sessionReducer";
+import user from "./userReducer";
 
 const rootReducer = combineReducers({
   session,
+  user,
 });
 
 export default rootReducer;
