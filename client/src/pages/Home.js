b5d241807bd1443b447bc4e36ee8ba65d73ed457
tennga <tenngawang6080@gmail.com>
2020-04-30 14:56:56 -0400

    updated media using theme mq

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import { Heading } from "grommet";
 
 import ImageButton from "../components/Button/ImageButton";
-import { theme } from "../constants/theme";
+import { theme, mq } from "../constants/theme";
 
 const needHelpInactive = require("../assets/thermometer-unselected.svg");
 const needHelpActive = require("../assets/thermometer-selected.svg");
@@ -17,9 +17,13 @@ const FlexChild = styled.div`
 `;
 
 const StyledIntro = styled.div`
-  margin-top: 4rem;
+  
+    
+  @media screen and ${mq.phone.narrow.max} {  
+    margin-top: 4rem;
+  }
 
-  @media only screen and (min-width: 600px) {  
+  @media screen and (min-width: ${mq.tablet.wide.minWidth}){  
     background-color: ${theme.colors.royalBlue};
     border-radius: 1rem;
     display:flex;
@@ -30,102 +34,108 @@ const StyledIntro = styled.div`
     padding: 16rem 7rem 16rem 5rem;
   }
 
-  @media only screen and (min-width: 1440px) {  
+  @media only screen and ${mq.desktop.medium.minWidth} {  
     padding: 20rem 7rem 20rem 5rem;
   }
 `;
 
 const MainContainer = styled.div`
-  @media only screen and (min-width: 600px){
-    display:flex
-    
+  display:grid;
+  grid-template-columns: 1fr 1fr;
+
+  @media only screen and ${mq.phone.narrow.max}{
+    display: block;
   }
 `;
 
 const StyledWelcome = styled(Heading)`
-  font-family: ${theme.typography.font.family.display}, sans-serif;
-  font-size: ${theme.typography.size.large};
-  font-style: normal;
-  font-weight: 300;
-  margin: 2.5rem auto 0;
-  text-align: center;
-
-  @media only screen and (min-width: 600px) {  
     font-size: ${theme.typography.heading.three};
     text-align: left;
     margin: 0 0 7rem 0;
+    font-family: ${theme.typography.font.family.display}, sans-serif;
+    font-style: normal;
+   
+  @media only screen and ${mq.phone.narrow.max}{  
+    font-size: ${theme.typography.size.large};
+    font-weight: 300;
+    margin: 2.5rem auto 0;
+    text-align: center;
   }
 
-  @media only screen and (min-width: 1024px) {  
+  @media only screen and (min-width: ${mq.tablet.wide.minWidth}) {  
     font-size: ${theme.typography.heading.one};
   }
 
-  @media only screen and (min-width: 1280px) {  
+  @media only screen and (min-width: ${mq.desktop.small.minWidth}){  
     font-size: 5rem;
   }
 
-  @media only screen and (min-width: 1440px) {  
+  @media only screen and (min-width: ${mq.desktop.medium.minWidth}) {  
     font-size: 6rem;
   }
 `;
 
 
 const StyledStrapline = styled(StyledWelcome)`
-  font-weight: bold;
-  margin: 0 auto;
-  margin-bottom: 1.5rem;
-
-  @media only screen and (min-width: 600px){
     color: white;
     line-height: 7rem;
     width: 90%;
     text-align: left;
+
+  @media only screen and ${mq.phone.narrow.max}{
+    text-align: center;
+    color: black;
+    font-weight: bold;
+    margin: 0 auto;
+    margin-bottom: 1.5rem;
   }
 
 `;
 
 const IntroText = styled.div`
-@media only screen and (min-width: 600px){
+@media only screen and (min-width: ${mq.desktop.small.minWidth}){
   width: 90%;
 }
 `;
 
 const StyledP = styled.p`
-  font-family: ${theme.typography.font.family.display}, sans-serif;
-  font-size: ${theme.typography.size.small};
-  color: #000;
-  line-height: 4rem;
-  letter-spacing: 0rem;
-  margin: 0;
-  
-  @media only screen and (min-width: 600px){
-    font-size: ${theme.typography.size.xsmall};
+  font-size: ${theme.typography.size.xsmall};
     text-align: left;
     color: white;
+
+  @media only screen and ${mq.phone.narrow.max}{
+    font-family: ${theme.typography.font.family.display}, sans-serif;
+    font-size: ${theme.typography.size.small};
+    text-align: center;
+    color: #000;
+    line-height: 4rem;
+    letter-spacing: 0rem;
+    margin: 0;
   }
 
-  @media only screen and (min-width: 1024px){
-    font-size: ${theme.typography.size.medium};
+  @media only screen and (min-width: ${mq.tablet.wide.minWidth}){
+    font-size: ${theme.typography.size.large};
   }
 
   
-  @media only screen and (min-width: 1280px) {  
+  @media only screen and (min-width: ${mq.desktop.small.minWidth}) {  
     font-size: ${theme.typography.size.xlarge};
   }
 
-  @media only screen and (min-width: 1440px){
+  @media only screen and (min-width: ${mq.desktop.medium.minWidth}){
     font-size: ${theme.typography.size.xxlarge};
   }
 `;
 
 const OnboardingContainer = styled.div`
-  margin-top: 4rem;
-
-  @media only screen and (min-width: 600px){
+  
     // display: flex;
     // flexWrap: wrapgit ;
     width: 100%;
     margin: auto 0;
+
+  @media only screen and ${mq.phone.narrow.max}{
+   margin-top: 4rem;
   }
 `;
 
