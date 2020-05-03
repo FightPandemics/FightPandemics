f9ea41779aeacee7b1996ea17379f9fb41f6b53d
Annie <annieg4123@gmail.com>
2020-05-03 09:52:48 -0700

    Add ENVIRONEMNT to config data

--- backend/config.js
+++ backend/config.js
@@ -11,6 +11,7 @@ const configData = envSchema({
     .prop("AUTH_DOMAIN", S.string().required())
     .prop("AUTH_SECRET_KEY", S.string().required())
     .prop("AUTH_STATE", S.string().required())
+    .prop("ENVIRONMENT", S.string().default("dev").required())
     .prop("GEO_SERVICE_URL", S.string().required())
     .prop("MONGO_URI", S.string().required())
     .prop("PORT", S.number().default(8000).required()),
@@ -24,6 +25,7 @@ const config = {
     secretKey: configData.AUTH_SECRET_KEY,
     state: configData.AUTH_STATE,
   },
+  env: configData.ENVIRONMENT,
   geoService: {
     host: `http://${configData.GEO_SERVICE_URL}`,
   },
