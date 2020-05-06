6c8e5b2611e106058596492255febaeca3151e56
Chimaobi.Johnson <woguchimaobi@gmail.com>
2020-05-06 11:58:20 +0100

    updated styling for login/signup page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -9,7 +9,7 @@ import {
   loginWithEmail,
   signup,
 } from "../actions/authActions";
-import SubmitButton from "~/components/Button/SubmitButton";
+import CustomButton from "~/components/Button/SubmitButton";
 import Label from "~/components/Input/Label";
 import Input from "~/components/Input/BaseInput";
 import { validateEmail } from "../utils/common.js";
@@ -21,7 +21,9 @@ import twitter from "~/assets/icons/social-twitter.svg";
 import facebook from "~/assets/icons/social-facebook.svg";
 import gmail from "~/assets/icons/social-google.svg";
 import linkedin from "~/assets/icons/social-linkedin.svg";
-
+import socialmedia from "~/assets/social-media.svg";
+import socialmedia2 from "~/assets/social-media2.svg";
+import eye from "~/assets/icons/eye.svg";
 import { theme } from "../constants/theme";
 const { colors } = theme;
 const { typography } = theme;
@@ -35,9 +37,22 @@ const Title = styled.h1`
   justify-content: center;
 `;
 
+const SubmitButton = styled(CustomButton)`
+  color: #fff;
+  background-color: #425AF2;
+  font-weight: 300;
+`;
+
 const InputWrapper = styled.div`
   margin: 2.2rem auto;
   width: 100%;
+  position: relative;
+`;
+
+const PasswordVisibility = styled.img`
+   position: absolute;
+   bottom: 6px;
+   right: 5px;
 `;
 
 const StyleInput = {
@@ -47,6 +62,7 @@ const StyleInput = {
   paddingBottom: "0.8rem",
   width: "100%",
   borderBottom: "2px solid #5970EC",
+  backgroundColor: "transparent"
 };
 
 const StyleLabel = {
@@ -71,7 +87,7 @@ const SectionDiv = styled.div`
     display: inline-block;
     content: "";
     border-top: 0.1rem solid ${colors.lightGray};
-    width: 3rem;
+    width: 7rem;
     margin: 0 0.5rem;
     transform: translateY(-0.3rem);
   }
@@ -101,11 +117,43 @@ const ButtonText = styled.span`
 const AuthLink = styled.a`
   font-family: ${typography.font.family.display};
   font-style: normal;
-  font-weight: 500;
+  font-weight: 300;
   font-size: 1.6rem;
   line-height: 2.1rem;
   text-align: center;
   color: ${colors.royalBlue};
+`;
+
+
+const LoginContainer = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: #FBFBFD;
+`;
+
+const LoginLeftContainer = styled.div`
+  flex-basis: 45%;
+  background-color: #F3F4FE;
+  height: 100vh;
+  position: relative;
+`;
+
+const LoginRightContainer = styled.div`
+  flex: 1;
+`;
+
+const SocialImageContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 55%;
+  transform: translate(-50%, -50%);
+  width: 80%;
+  margin: 0 auto;
+`;
+
+const FormContainer = styled.div`
+  width: 50%;
+  margin: 0 auto;
 `;
 
 const Login = ({ isLoginForm }) => {
@@ -150,14 +198,20 @@ const Login = ({ isLoginForm }) => {
   const handleSignup = (evt) => {
     evt.preventDefault();
     // todo: add inline validation (disable button / indicate error on form)
-    /*if (!validateEmail(email)) {
-      "Invalid email address!"
-    }*/
+    if (!validateEmail(email)) {
+      Toast.fail("Invalid email address!", 3);
+      return
+    }
     // todo: add inline validation (disable button / indicate error on form)
-    /*if (password.length < PASSWORD_MIN_LENGTH) {
-      "Password must be at least 6 characters"
-    }*/
+    if (password.length < PASSWORD_MIN_LENGTH) {
+      Toast.fail("Password must be at least 6 characters", 3);
+      return
+    }
     // todo: check if passwords are the same (dissable button / indicate error on form)
+    if(password !== confirmPassword) {
+      Toast.fail("Password and confirm password do not match!", 3);
+      return
+    }
     dispatch(signup({ email, password, confirmPassword }));
   };
 
@@ -165,112 +219,144 @@ const Login = ({ isLoginForm }) => {
     window.location.href = `/api/auth/oauth/${provider}`;
   };
 
-  return (
-    <div className="text-center">
-      <Title>{isLoginForm ? "Sign In" : "Sign Up"}</Title>
-      <form id="login-password" method="POST">
-        <InputWrapper>
-          <Label for="email" style={StyleLabel} label="E-mail" />
-          <Input
-            type="email"
-            name="email"
-            required
-            placeholder="Enter email address"
-            value={email}
-            onChange={handleInputChangeEmail}
-            style={StyleInput}
-          />
-        </InputWrapper>
-        <InputWrapper>
-          <Label for="password" style={StyleLabel} label="Password" />
-          <Input
-            type="password"
-            name="password"
-            required
-            placeholder="Enter password"
-            value={password}
-            onChange={handleInputChangePassword}
-            style={StyleInput}
-          />
-        </InputWrapper>
-        {!isLoginForm && (
-        <InputWrapper>
+  const togglePasswordVisibility = (e) => {
+    let x = document.getElementById("password");
+     if (x.type === "password") {
+       x.type = "text";
+     } else {
+       x.type = "password";
+     }
+  }
 
-        <Label style={StyleLabel} label="Confirm password" />
-          <Input
-          type="password"
-          required
-          placeholder="Confirm password"
-          value={confirmPassword}
-          onChange={handleInputChangeConfirmPassword}
-          style={StyleInput}
-          />
-          </InputWrapper>
-        )}
-        
-        <SubmitButton
-          primary="true"
-          onClick={isLoginForm ? handleLoginWithEmail : handleSignup}
-        >
-          {isLoginForm ? "Sign In" : "Sign Up"}
-        </SubmitButton>
-      </form>
-      <WhiteSpace />
-      <WhiteSpace />
-      {isLoginForm ? (
-        <>
-          <p>
-            <AuthLink href="/auth/forgot-password">Forgot password?</AuthLink>
-          </p>
-          <p>
-            <AuthLink href="/auth/signup">
-              Don't have an account? <u>Sign Up</u>
-            </AuthLink>
-          </p>
-        </>
-      ) : (
-        <p>
-          <AuthLink href="/auth/login">
-            Already have an account? <u>Sign In</u>
-          </AuthLink>
-        </p>
-      )}
-      <WhiteSpace />
-      <SectionDiv>
-        {isLoginForm ? "Or Log in with" : "Or Sign up with"}
-      </SectionDiv>
-      <WhiteSpace />
-      <FlexBox>
-        <SocialButton
-          style={StyleSocialIcon}
-          icon={<SvgIcon src={facebook} />}
-          onClick={() => handleSocialLogin("facebook")}
-        >
-          <ButtonText>Facebook</ButtonText>
-        </SocialButton>
-        <SocialButton
-          style={StyleSocialIcon}
-          icon={<SvgIcon src={gmail} />}
-          onClick={() => handleSocialLogin("google")}
-        >
-          <ButtonText>Gmail</ButtonText>
-        </SocialButton>
-        <SocialButton
-          style={StyleSocialIcon}
-          icon={<SvgIcon src={twitter} />}
-          onClick={() => handleSocialLogin("twitter")}
-        >
-          <ButtonText>Twitter</ButtonText>
-        </SocialButton>
-        <SocialButton
-          style={StyleSocialIcon}
-          icon={<SvgIcon src={linkedin} />}
-          onClick={() => handleSocialLogin("linkedin")}
-        >
-          <ButtonText>Linkedin</ButtonText>
-        </SocialButton>
-      </FlexBox>
-    </div>
+  const toggleConfirmPasswordVisibility = (e) => {
+    let x = document.getElementById("confirmPassword");
+     if (x.type === "password") {
+       x.type = "text";
+     } else {
+       x.type = "password";
+     }
+  }
+
+
+  return (
+    <LoginContainer>
+       <LoginLeftContainer>
+           <SocialImageContainer>
+               <img
+                  src={isLoginForm ? socialmedia : socialmedia2}
+                  alt=""
+                  />
+           </SocialImageContainer>
+       </LoginLeftContainer>
+       <LoginRightContainer>
+          <div className="text-center">
+            <FormContainer>
+                <Title>{isLoginForm ? "Sign In" : "Sign Up"}</Title>
+                <form id="login-password" method="POST">
+                  <InputWrapper>
+                    <Label for="email" style={StyleLabel} label="E-mail" />
+                    <Input
+                      type="email"
+                      name="email"
+                      required
+                      placeholder="Enter email address"
+                      value={email}
+                      onChange={handleInputChangeEmail}
+                      style={StyleInput}
+                    />
+                  </InputWrapper>
+                  <InputWrapper>
+                    <Label for="password" style={StyleLabel} label="Password" />
+                    <Input
+                      type="password"
+                      name="password"
+                      id="password"
+                      required
+                      placeholder="Enter password"
+                      value={password}
+                      onChange={handleInputChangePassword}
+                      style={StyleInput}
+                    />
+                    <PasswordVisibility onClick={togglePasswordVisibility} src={eye} alt="" />
+                  </InputWrapper>
+                  {isLoginForm ? ''
+                   : ( <InputWrapper>
+                    <Label for="confirmPassword" style={StyleLabel} label="Confirm Password" />
+                    <Input
+                      type="password"
+                      name="confirmPassword"
+                      id="confirmPassword"
+                      required
+                      placeholder="Confirm password"
+                      onChange={handleInputChangeConfirmPassword}
+                      value={confirmPassword}
+                      style={StyleInput}
+                    />
+                    <PasswordVisibility onClick={toggleConfirmPasswordVisibility} src={eye} alt="" />
+                  </InputWrapper> ) }
+                  <SubmitButton
+                    onClick={isLoginForm ? handleLoginWithEmail : handleSignup}
+                  >{isLoginForm ? "Sign In" : "Sign Up"}</SubmitButton>
+                </form>
+                <WhiteSpace />
+                <WhiteSpace />
+                {isLoginForm ? (
+                  <>
+                    <p>
+                      <AuthLink href="/auth/forgot-password">Forgot password?</AuthLink>
+                    </p>
+                    <p>
+                      <AuthLink href="/auth/signup">
+                        Don't have an account? <u>Sign Up</u>
+                      </AuthLink>
+                    </p>
+                  </>
+                ) : (
+                  <p>
+                    <AuthLink href="/auth/login">
+                      Already have an account? <u>Sign In</u>
+                    </AuthLink>
+                  </p>
+                )}
+                <WhiteSpace />
+                <SectionDiv>
+                  {isLoginForm ? "Or Log in with" : "Or Sign up with"}
+                </SectionDiv>
+                <WhiteSpace />
+              </FormContainer>
+            <FlexBox>
+              <SocialButton
+                style={StyleSocialIcon}
+                icon={<SvgIcon src={facebook} />}
+                onClick={() => handleSocialLogin("facebook")}
+              >
+                <ButtonText>Facebook</ButtonText>
+              </SocialButton>
+              <SocialButton
+                style={StyleSocialIcon}
+                icon={<SvgIcon src={gmail} />}
+                onClick={() => handleSocialLogin("google")}
+              >
+                <ButtonText>Gmail</ButtonText>
+              </SocialButton>
+              <SocialButton
+                style={StyleSocialIcon}
+                icon={<SvgIcon src={twitter} />}
+                onClick={() => handleSocialLogin("twitter")}
+              >
+                <ButtonText>Twitter</ButtonText>
+              </SocialButton>
+              <SocialButton
+                style={StyleSocialIcon}
+                icon={<SvgIcon src={linkedin} />}
+                onClick={() => handleSocialLogin("linkedin")}
+              >
+                <ButtonText>Linkedin</ButtonText>
+              </SocialButton>
+            </FlexBox>
+          </div>
+        </LoginRightContainer>
+    </LoginContainer>
   );
 };
 
--- client/src/templates/layouts/LogoLayout.js
+++ client/src/templates/layouts/LogoLayout.js
@@ -6,19 +6,27 @@ import logo from "~/assets/logo.svg";
 import Logo from "~/components/Logo";
 import Main from "./Main";
 
+const NavbarStyles = {
+  position: 'absolute',
+  top: "1rem",
+  left: "2rem",
+  zIndex: "10",
+  backgroundColor: "transparent"
+}
+
 const LogoLayout = (props) => {
   return (
     <>
-      <div className="header" style={{ marginTop: 8 }}>
-        <NavBar mode="light">
+      <div className="header">
+        <NavBar mode="light" style={NavbarStyles}>
           <Link to="/">
             <Logo src={logo} alt="Fight Pandemics logo" />
           </Link>
         </NavBar>
       </div>
-      <Main>
+      <div>
         <props.component {...props} />
-      </Main>
+      </div>
     </>
   );
 };
