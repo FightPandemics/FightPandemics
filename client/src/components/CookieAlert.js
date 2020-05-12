580cc1e61910ba385cb50ab2e3be4dc62fd22147
harithzamri <harithtoikee96@gmail.com>
2020-05-12 09:04:12 +0800

    solve pointer and delete the comment

--- client/src/components/CookieAlert.js
+++ client/src/components/CookieAlert.js
@@ -30,9 +30,9 @@ const CookieLink = styled.a`
   text-decoration: underline;
 `;
 
-// const CloseButton = styled.button`
-//   align-self: start;
-// `;
+const ClosePointer = styled.span`
+  cursor: pointer;
+`;
 
 export default () => {
   const [active, setActive] = useState(true);
@@ -62,7 +62,9 @@ export default () => {
         Your use of FightPandemics' Products is subject to these policies and
         terms.
       </div>
-      <SvgIcon src={closeX} onClick={hideMessage} />
+      <ClosePointer>
+        <SvgIcon src={closeX} onClick={hideMessage} />
+      </ClosePointer>
     </CookieContainer>
   );
 };
