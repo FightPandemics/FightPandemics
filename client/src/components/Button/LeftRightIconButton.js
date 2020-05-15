60dd44ad6e1a9d594ea93c004cbc3038052625bf
Trev <trevor.smith.holbourn@gmail.com>
2020-05-15 15:56:26 -0400

    Added new button component to meet the requirements of the github issue

---
+++ client/src/components/Button/LeftRightIconButton.js
@@ -0,0 +1,61 @@
+import styled, { css } from "styled-components";
+import SubmitButton from "./SubmitButton";
+import { theme } from "constants/theme";
+const { button } = theme;
+
+// props: inline, primary, primarylight, secondary, tertiary, icon, righticon
+
+const LeftRightIconButton = styled(SubmitButton).attrs(({ icon, inline }) => {
+  return { icon, inline };
+})`
+  font-weight: 400;
+  width: 192px;
+  &.am-button > span {
+    margin-right: 3rem;
+  }
+  &.am-button > .am-button-icon {
+    margin-right: 1.5rem;
+  }
+
+  ${(props) =>
+    props.righticon &&
+    css`
+      &.am-button {
+        flex-direction: row-reverse;
+      }
+      &.am-button > span {
+        margin-right: unset;
+        margin-left: 3rem;
+      }
+      &.am-button > .am-button-icon {
+        margin-right: unset;
+        margin-left: 1.5rem;
+      }
+    `}
+
+  ${(props) =>
+    props.disabled &&
+    css`
+      &.am-button > .am-button-icon {
+        opacity: 0.6;
+      }
+      &.am-button.am-button-disabled {
+        color: ${() => {
+          if (props.primary) {
+            return button.primary.color;
+          } else if (props.primarylight) {
+            return button.primarylight.color;
+          } else if (props.secondary) {
+            return button.secondary.color;
+          } else if (props.tertiary) {
+            return button.tertiary.color;
+          } else {
+            return "unset";
+          }
+        }};
+      }
+      pointer-events: none;
+    `}
+`;
+
+export default LeftRightIconButton;
