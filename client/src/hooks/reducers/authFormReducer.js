249698cf34d3826d398d837fa3352aa78dfb4636
Robin Venneman <robin.venneman@gmail.com>
2020-05-20 21:25:39 +0200

    feat: finish refactoring to react-hook-form and update error alert, social auth integration

--- client/src/hooks/reducers/authFormReducer.js
+++ client/src/hooks/reducers/authFormReducer.js
@@ -3,6 +3,8 @@ import {
   AUTH_FORM_LOGIN_ERROR,
   AUTH_FORM_SIGNUP,
   AUTH_FORM_SIGNUP_ERROR,
+  AUTH_FORM_SOCIAL,
+  AUTH_FORM_SOCIAL_ERROR,
 } from "../actions/authFormActions";
 
 export const initialState = {
@@ -15,9 +17,11 @@ export const authFormReducer = (state, action) => {
   switch (type) {
     case AUTH_FORM_LOGIN:
     case AUTH_FORM_SIGNUP:
+    case AUTH_FORM_SOCIAL:
       return { ...state, loading: true, error: null };
     case AUTH_FORM_LOGIN_ERROR:
     case AUTH_FORM_SIGNUP_ERROR:
+    case AUTH_FORM_SOCIAL_ERROR:
       return { ...state, loading: false, error: payload.error };
     default:
       return state;
