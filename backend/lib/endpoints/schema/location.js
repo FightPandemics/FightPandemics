acb037826c6ed45fb6ca495e8c89ccadbc3a888b
João Fernandes <joaofnds@joaofnds.com>
2020-05-21 16:14:49 -0300

    Add state and zip to location schema

--- backend/lib/endpoints/schema/location.js
+++ backend/lib/endpoints/schema/location.js
@@ -6,10 +6,12 @@ const { LOCATION_TYPES } = require("../../models/Location");
 const locationSchema = strictSchema()
   .prop("address", S.string())
   .prop("city", S.string())
+  .prop("coordinates", S.array().items(S.number()).minItems(2).maxItems(2))
   .prop("country", S.string())
   .prop("neighborhood", S.string())
+  .prop("state", S.string())
   .prop("type", S.string().enum(LOCATION_TYPES))
-  .prop("coordinates", S.array().items(S.number()).minItems(2).maxItems(2));
+  .prop("zip", S.string());
 
 module.exports = {
   locationSchema,
