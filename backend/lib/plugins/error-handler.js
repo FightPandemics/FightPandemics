5e331f646e6ca373c3b76073e2b745d02532ae38
Manny Karyampudi <mannykary@users.noreply.github.com>
2020-05-24 13:09:11 -0700

    Add error handler plugin for Sentry

---
+++ backend/lib/plugins/error-handler.js
@@ -0,0 +1,42 @@
+const fp = require("fastify-plugin");
+const Sentry = require("@sentry/node");
+
+// Default Fastify error handler
+// https://github.com/fastify/fastify/blob/5bc406d4c6677131237f85b6ae69fe9e34d4b8dd/lib/context.js#L29
+function defaultErrorHandler(error, request, reply) {
+  if (reply.statusCode >= 500) {
+    reply.log.error(
+      { req: request, res: reply, err: error },
+      error && error.message,
+    );
+  } else if (reply.statusCode >= 400) {
+    reply.log.info({ res: reply, err: error }, error && error.message);
+  }
+  reply.send(error);
+}
+
+function errorNotifierHandler(error, request, reply) {
+  Sentry.withScope((scope) => {
+    scope.setUser({
+      ip_address: request.raw.ip,
+    });
+    scope.setTag("path", request.raw.url);
+    Sentry.captureException(err);
+    defaultErrorHandler(error, request, reply);
+  });
+}
+
+function errorHandler(app, config, done) {
+  let errorHandler = defaultErrorHandler;
+  if (config.url) {
+    Sentry.init({
+      dsn: config.url,
+      environment: config.environment,
+    });
+    errorHandler = errorNotifierHandler;
+  }
+  app.setErrorHandler(errorHandler);
+  done();
+}
+
+module.exports = fp(errorHandler);
