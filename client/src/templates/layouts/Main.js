a7c876e10e08c14d5aa54da20f4ddf667607e193
Nadine Ju <nadine.jura@online.de>
2020-05-25 23:11:56 -0700

    first screen fixed, buttons refactored

--- client/src/templates/layouts/Main.js
+++ client/src/templates/layouts/Main.js
@@ -1,22 +1,27 @@
 import styled from "styled-components";
-import { mq } from "constants/theme";
+import { theme, mq } from "constants/theme";
+const { offWhite } = theme.colors;
 
 const Main = styled.main`
   margin-left: 2rem;
   margin-right: 2rem;
 
-  min-height: calc(100vh-124px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  min-height: calc(100% - 119px);
+  min-height: -o-calc(100% - 119px); /* opera */
+  min-height: -webkit-calc(100% - 119px); /* google, safari */
+  min-height: -moz-calc(100% - 119px); /* firefox */
 
   @media screen and (min-width: ${mq.tablet.wide.minWidth}) {
-    min-height: calc(100vh-166px);
-  }
+    background-color: ${offWhite};
 
-  @media screen and (min-width: ${mq.desktop.small.minWidth}) {
-    min-height: calc(100vh-166px);
-  }
-
-  @media screen and (min-width: ${mq.desktop.medium.minWidth}) {
-    min-height: calc(100vh-166px);
+    min-height: calc(100%-160px);
+    min-height: -o-calc(100% - 160px); /* opera */
+    min-height: -webkit-calc(100% - 160px); /* google, safari */
+    min-height: -moz-calc(100% - 160px); /* firefox */
   }
 `;
 
