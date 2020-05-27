fa6837ec6c553175b30672475f138799a8a0cd46
Nadine Ju <nadine.jura@online.de>
2020-05-26 20:59:11 -0700

    StyledTextInput fixed

--- client/src/components/StepWizard/StyledTextInput.js
+++ client/src/components/StepWizard/StyledTextInput.js
@@ -1,22 +1,51 @@
 import styled from "styled-components";
+import { InputItem } from "antd-mobile";
 import { theme, mq } from "constants/theme";
-import SearchInput from "components/Input/SearchInput";
 
 const { darkGray, royalBlue } = theme.colors;
 
-const StyledTextInput = styled(SearchInput)`
-  border: none;
-  border-bottom: 0.1rem solid ${royalBlue} !important;
-  color: ${darkGray};
-  font-family: "Poppins";
-  font-size: 1.6rem;
-  height: 5rem;
-  line-height: 2.2rem;
+const StyledTextInput = styled(InputItem)`
   width: 100%;
 
+  &.am-list-item {
+    &.am-input-item {
+      padding-left: 0;
+      .am-list-line {
+        padding-right: 0;
+      }
+    }
+  }
+  .am-input-control {
+    input {
+      ${theme.form.input}
+      color: ${darkGray};
+      border: none;
+      border-bottom: 0.1rem solid ${royalBlue} !important;
+      font-size: 1.6rem;
+      padding: 0.8rem;
+      margin: 0.5rem 0;
+    }
+  }
+
   @media screen and (min-width: ${mq.tablet.wide.minWidth}) {
-    border: 0.2rem solid ${royalBlue} !important;
     width: 40.8rem;
+
+    &.am-list-item {
+      &.am-input-item {
+        height: 5rem;
+        .am-list-line {
+        }
+      }
+    }
+
+    .am-input-control {
+      input {
+        border: 0.2rem solid ${royalBlue};
+        border-radius: 4.6rem;
+        height: 5rem;
+        padding-left: 4rem;
+      }
+    }
   }
 `;
 
