1c1012e4a2404421d2473ec85d52070c1d485096
Robin Venneman <robin.venneman@gmail.com>
2020-05-29 18:34:18 +0200

    feat: update error handling reusable error alert component, disable submit while loading

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -4,10 +4,10 @@ import { getAuthToken } from "utils/auth-token";
 // Note: for production apps, both localstorage & cookies contain risks to store user & auth data
 export const initAuth = () => {
   return (dispatch) => {
-    const { token, emailVerified } = getAuthToken();
+    const token = getAuthToken();
 
     if (token) {
-      dispatch({ type: AUTH_SUCCESS, payload: { token, emailVerified } });
+      dispatch({ type: AUTH_SUCCESS, payload: { token } });
     }
   };
 };
