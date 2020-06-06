1109fd11b703714253aac1cd1099211ff70590d2
Robin Venneman <robin.venneman@gmail.com>
2020-06-07 00:42:30 +0200

    feat: adding actions and reducers for profile

--- client/src/hooks/reducers/userReducers.js
+++ client/src/hooks/reducers/userReducers.js
@@ -2,6 +2,9 @@ import {
   CREATE_USER,
   CREATE_USER_ERROR,
   CREATE_USER_SUCCESS,
+  FETCH_USER,
+  FETCH_USER_ERROR,
+  FETCH_USER_SUCCESS,
 } from "../actions/userActions";
 
 export const initialState = {
@@ -16,6 +19,26 @@ export const createUserFormReducer = (state, action) => {
       return { ...state, loading: true, error: null };
     case CREATE_USER_ERROR:
       return { ...state, loading: false, error: payload.error };
+    default:
+      return state;
+  }
+};
+
+export const initialProfileState = {
+  error: null,
+  loading: false,
+  user: null,
+};
+
+export const userProfileReducer = (state, action) => {
+  const { type, ...payload } = action;
+  switch (type) {
+    case FETCH_USER:
+      return { ...state, loading: true, error: null };
+    case FETCH_USER_ERROR:
+      return { ...state, loading: false, error: payload.error };
+    case FETCH_USER_SUCCESS:
+      return { ...state, loading: false, user: payload.user };
     default:
       return state;
   }
