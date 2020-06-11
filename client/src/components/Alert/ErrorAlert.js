166f4cf2c550e23d6469b05e830813145b20cef9
Robin Venneman <robin.venneman@gmail.com>
2020-06-11 02:49:43 +0200

    feat: add client-side validation for the url's on profile

--- client/src/components/Alert/ErrorAlert.js
+++ client/src/components/Alert/ErrorAlert.js
@@ -4,6 +4,7 @@ import { ORANGE_RED, WHITE } from "../../constants/colors";
 
 const ErrorAlert = styled(Alert)`
   background-color: ${ORANGE_RED};
+  margin: 1rem 0;
   .ant-alert-message {
     color: ${WHITE};
   }
