381914941168cc82d6337bcf613fb5fe2cb66c0d
Israel Adetunji <adisen230@gmail.com>
2020-06-12 22:24:48 +0100

    Added format(email)

--- backend/lib/endpoints/schema/auth.js
+++ backend/lib/endpoints/schema/auth.js
@@ -10,14 +10,16 @@ const loginSchema = {
 // todo: add password confirmation as required field
 const signupSchema = {
   body: strictSchema()
-    .prop("email", S.string().required())
+    .prop("email", S.string().format(S.FORMATS.EMAIL).required())
     .prop("password", S.string().required())
     .prop("confirmPassword", S.string().required()),
 };
 
 const changePasswordSchema = {
-  body: strictSchema()
-    .prop("email", S.string().required())
+  body: strictSchema().prop(
+    "email",
+    S.string().format(S.FORMATS.EMAIL).required(),
+  ),
 };
 
 const oAuthSchema = {
