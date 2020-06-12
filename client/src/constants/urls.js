10bed3bbbd6b183f3c902000e2a64af48c536a1c
Robin Venneman <robin.venneman@gmail.com>
2020-06-12 20:25:39 +0200

    feat: fix rendering of social url's

---
+++ client/src/constants/urls.js
@@ -0,0 +1,4 @@
+export const FACEBOOK_URL = "https://www.facebook.com/";
+export const LINKEDIN_URL = "https://www.linkedin.com/in/";
+export const TWITTER_URL = "https://twitter.com/";
+export const GITHUB_URL = "https://github.com/";
