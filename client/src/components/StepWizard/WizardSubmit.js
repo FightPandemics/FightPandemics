31981a95c84305f2110c8148f0567f3c3d6a397d
Joeumar Souza <joeumar@gmail.com>
2020-06-12 15:56:08 -0300

    Fixing issue #563

--- client/src/components/StepWizard/WizardSubmit.js
+++ client/src/components/StepWizard/WizardSubmit.js
@@ -1,24 +1,29 @@
-import styled from 'styled-components';
-import { theme } from "constants/theme";
+import styled from "styled-components";
+import { theme, mq } from "constants/theme";
 import BaseButton from "components/Button/BaseButton";
 
 const { white, royalBlue } = theme.colors;
 const WizardSubmit = styled(BaseButton)`
-border-radius: 4rem;
-margin-right: 7rem;
-margin-left: 7rem;
+  border-radius: 4rem;
+  align-self: center;
   cursor: pointer;
   color: ${white};
   padding: 0 0.1rem;
   background-color: ${royalBlue};
+  width: inherit;
+  
   &:hover,
   &.am-button-active {
     background-color: ${royalBlue};
     color: ${white};
   } 
+  
   html body &.am-button {
     border: 0.2rem solid ${royalBlue};
   }
+
+  @media screen and (min-width: ${mq.tablet.wide.minWidth}) {
+    width: 40.8rem;
   `;
 
 export default WizardSubmit;
