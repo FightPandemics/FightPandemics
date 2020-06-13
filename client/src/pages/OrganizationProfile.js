4903a1bcbce6108d494389fe9ebd99fe6f101913
Chimaobi.Johnson <woguchimaobi@gmail.com>
2020-06-13 01:14:29 +0100

    added route for org profile

---
+++ client/src/pages/OrganizationProfile.js
@@ -0,0 +1,43 @@
+import { WhiteSpace } from "antd-mobile";
+import axios from "axios";
+import React, { useContext, useEffect, useReducer, useState } from "react";
+import { Link } from "react-router-dom";
+
+import Activity from "components/Profile/Activity";
+import CreatePost from "components/CreatePost/CreatePost";
+import ErrorAlert from "../components/Alert/ErrorAlert";
+import FeedWrapper from "components/Feed/FeedWrapper";
+import ProfilePic from "components/Picture/ProfilePic";
+import {
+  ProfileLayout,
+  BackgroundHeader,
+  MenuIcon,
+  UserInfoContainer,
+  EditIcon,
+  UserInfoDesktop,
+  NameDiv,
+  PlaceholderIcon,
+  EditEmptyIcon,
+  DescriptionDesktop,
+  LocationDesktopDiv,
+  LocationMobileDiv,
+  IconsContainer,
+  HelpContainer,
+  LocationIcon,
+  SocialIcon,
+  DescriptionMobile,
+  SectionHeader,
+  CreatePostDiv,
+  CreatePostIcon,
+  DrawerHeader,
+  CustomDrawer,
+} from "../components/Profile/ProfileComponents";
+
+
+
+const OrganizationProfile = () => {
+
+
+}
+
+export default OrganizationProfile;
