437261cd75ae772b05ad00ffeb8393d7cde2918a
joshmorel <morel.josh@hotmail.com>
2020-06-18 11:01:49 -0400

    make address/coordinates required on location

--- backend/lib/endpoints/schema/location.js
+++ backend/lib/endpoints/schema/location.js
@@ -3,10 +3,14 @@ const { strictSchema } = require("./utils");
 
 const { LOCATION_TYPES } = require("../../models/Location");
 
+// address/coordinates will always be returned by geo service
 const locationSchema = strictSchema()
-  .prop("address", S.string())
+  .prop("address", S.string().required())
   .prop("city", S.string())
-  .prop("coordinates",S.array().items(S.number()).minItems(2).maxItems(2))
+  .prop(
+    "coordinates",
+    S.array().items(S.number()).minItems(2).maxItems(2).required()
+  )
   .prop("country", S.string())
   .prop("neighborhood", S.string())
   .prop("state", S.string())
