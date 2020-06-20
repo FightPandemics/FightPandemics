618d47c3b70a724be11177c5e6868f9bf3970a56
joshmorel <morel.josh@hotmail.com>
2020-06-20 13:30:33 -0400

    fix authLoading logic for mobile nav

--- client/src/templates/layouts/NavigationLayout.js
+++ client/src/templates/layouts/NavigationLayout.js
@@ -237,7 +237,7 @@ const NavigationLayout = (props) => {
     <MenuContainer>
       {drawerOpened && <CloseNav onClick={toggleDrawer} />}
       <NavList>
-        {authLoading && isAuthenticated ? <AuthenticatedMenu /> : <UnAuthenticatedMenu />}
+        {!authLoading && isAuthenticated ? <AuthenticatedMenu /> : <UnAuthenticatedMenu />}
       </NavList>
     </MenuContainer>
   );
