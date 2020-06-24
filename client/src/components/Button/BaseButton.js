8709956b17c9873c6f7c4b552fb699036956551c
Israel Adetunji <adisen230@gmail.com>
2020-06-24 21:59:55 +0100

    Change Button import from antd-mobile to antd to allow button to be set to type=submit

--- client/src/components/Button/BaseButton.js
+++ client/src/components/Button/BaseButton.js
@@ -1,5 +1,5 @@
 import styled, { css } from "styled-components";
-import { Button } from "antd-mobile";
+import { Button } from "antd";
 import { theme } from "constants/theme";
 const { button } = theme;
 
