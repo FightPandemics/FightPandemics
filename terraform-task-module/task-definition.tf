fa6e6d30d0ac36bc154b513aa6956cca155fd3fb
Manny Karyampudi <mannykary@users.noreply.github.com>
2020-06-27 18:36:13 -0700

    Refactor task-definition.tf Terraform
    - Define container definitions as Terraform maps
    - This allows us to reuse definitions when we need to conditionally
    add the Datadog container (future implementation)

--- terraform-task-module/task-definition.tf
+++ terraform-task-module/task-definition.tf
@@ -1,3 +1,51 @@
+locals {
+  backend_container_definition = {
+    cpu               = 512
+    name              = "backend"
+    essential         = true
+    image             = "fightpandemics/backend:${var.image_tag}"
+    memory            = 4096
+    memoryReservation = 1024
+    portMappings = [
+      {
+        containerPort = var.backend_port
+        hostPort      = var.backend_port
+      }
+    ]
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-region        = var.aws_region
+        awslogs-group         = "/ecs/${var.subdomain}-backend"
+        awslogs-stream-prefix = var.fp_context
+      }
+    }
+    environment = var.backend_env_variables
+  }
+  client_container_definition = {
+    cpu               = 256
+    name              = "client"
+    essential         = true
+    image             = "fightpandemics/client:${var.image_tag}"
+    memory            = 4096
+    memoryReservation = 1024
+    portMappings = [
+      {
+        containerPort = var.client_port
+        hostPort      = var.client_port
+      }
+    ]
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-region        = var.aws_region
+        awslogs-group         = "/ecs/${var.subdomain}-client"
+        awslogs-stream-prefix = var.fp_context
+      }
+    }
+  }
+}
+
 resource "aws_ecs_task_definition" "app" {
   family                   = var.subdomain
   network_mode             = "awsvpc"
@@ -6,53 +54,8 @@ resource "aws_ecs_task_definition" "app" {
   memory                   = 4096
   execution_role_arn       = data.aws_iam_role.ecs_execution_role.arn
   task_role_arn            = data.aws_iam_role.ecs_execution_role.arn
-  container_definitions    = <<DEFINITION
-  [
-    {
-      "cpu": 512,
-      "name": "backend",
-      "essential": true,
-      "image": "fightpandemics/backend:${var.image_tag}",
-      "memory": 4096,
-      "memoryReservation": 1024,
-      "portMappings": [
-        {
-          "containerPort": ${var.backend_port},
-          "hostPort": ${var.backend_port}
-        }
-      ],
-      "logConfiguration": {
-        "logDriver": "awslogs",
-        "options": {
-          "awslogs-region": "${var.aws_region}",
-          "awslogs-group": "/ecs/${var.subdomain}-backend",
-          "awslogs-stream-prefix": "${var.fp_context}"
-        }
-      },
-      "environment": ${jsonencode(var.backend_env_variables)}
-    },
-    {
-      "cpu": 256,
-      "name": "client",
-      "essential": true,
-      "image": "fightpandemics/client:${var.image_tag}",
-      "memory": 4096,
-      "memoryReservation": 1024,
-      "portMappings": [
-        {
-          "containerPort": ${var.client_port},
-          "hostPort": ${var.client_port}
-        }
-      ],
-      "logConfiguration": {
-        "logDriver": "awslogs",
-        "options": {
-          "awslogs-region": "${var.aws_region}",
-          "awslogs-group": "/ecs/${var.subdomain}-client",
-          "awslogs-stream-prefix": "${var.fp_context}"
-        }
-      }
-    }
-  ]
-DEFINITION
+  container_definitions    = jsonencode([
+    local.backend_container_definition,
+    local.client_container_definition
+  ])
 }
