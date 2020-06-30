16b860dbe22819c619ee772b1eaff62c25855767
Josue Villanueva <idjevm@gmail.com>
2020-06-30 12:44:55 -0400

    updated schema for sendgrid

--- backend/lib/endpoints/schema/sendgrid.js
+++ backend/lib/endpoints/schema/sendgrid.js
@@ -6,7 +6,8 @@ const sendgridContactSchema = {
     .prop("email", S.string().format("email").required())
     .prop("location", S.object().required())
     .prop("helpType", S.string())
-    .prop("postType", S.string()),
+    .prop("postType", S.string())
+    .prop("providers", S.array()),
 };
 
 module.exports = { sendgridContactSchema };
