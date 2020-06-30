07562d367c5f4b8f6fe7e24f6cae998a8c98e9d5
Chen(Cees) Wang <33577774+CeesWang@users.noreply.github.com>
2020-06-30 06:18:14 -0400

    Feature/integrate onboarding answers (#896)
    * need help component now correctly goes to feed page when pressed on submit
    
    * need help passes correct props on submit and skip
    
    * offer help page now give feed the right props when pressed on submit or skip
    
    * onboarding questions in feed in progress
    
    * removed transitions in use effect hooks
    
    * cleaned up some console logs
    
    * updated cees branch
    
    * updated offer help for cees
    
    * onboarding filter options for requesting help show up
    
    * now shows correct endpoint for offer help
    
    * onboarding now shows up correctly when directed to feed Page
    
    Co-authored-by: Josue Villanueva <idjevm@gmail.com>

--- client/src/pages/OfferHelp.js
+++ client/src/pages/OfferHelp.js
@@ -26,7 +26,10 @@ import {
 } from "components/StepWizard";
 
 const INITIAL_STATE = {
-  answers: [],
+  postType: "Offering help",
+  providers: [],
+  location: "",
+  email: "",
 };
 
 const STEP_1_ANSWERS = [
@@ -47,7 +50,7 @@ const Step1 = (props) => {
     const updatedAnswers = { ...answers, [answer]: !answers[answer] };
     const checkedAnswers = getCheckedAnswers(updatedAnswers);
     updateState({ ...state, answers: updatedAnswers });
-    props.update("helpTypeOffered", checkedAnswers);
+    props.update("providers", checkedAnswers);
   };
 
   return (
@@ -125,7 +128,7 @@ const Step3 = (props) => {
   };
 
   const onSubmit = () => {
-    console.log("submit");
+    props.update("email", email);
   };
 
   return (
@@ -161,10 +164,7 @@ const Step3 = (props) => {
           Submit
         </WizardSubmit>
         <SkipLink>
-          <Link to="/feed">
-            {/* By clicking on “skip”, users can skip the landing questions to see the information directly */}
-            Skip
-          </Link>
+          <span onClick={onSubmit}>Skip</span>
         </SkipLink>
       </WizardFormWrapper>
     </WizardStep>
@@ -178,14 +178,15 @@ const OfferHelp = withRouter((props) => {
   useEffect(() => {
     setTransition(!transition);
   }, []);
+
   const updateAnswers = (key, value) => {
-    const { answers } = state;
-    const updatedAnswers = { ...answers, [key]: value };
-    setState({ ...state, updatedAnswers });
+    const updatedAnswers = { ...state, [key]: value };
+    setState({ ...updatedAnswers });
     if (key === "email") {
       localStorage.setItem("offerHelpAnswers", JSON.stringify(updatedAnswers));
       props.history.push({
         pathname: "/feed",
+        state: updatedAnswers,
       });
     }
   };
@@ -196,7 +197,7 @@ const OfferHelp = withRouter((props) => {
           <StyledWizard isHashEnabled status={status} nav={<WizardNav />}>
             <Step1 hashKey={"Step1"} update={updateAnswers} />
             <Step2 hashKey={"Step2"} update={updateAnswers} />
-            <Step3 hashKey={"Step3"} update={updateAnswers} />
+            <Step3 hashKey={"Step3"} update={updateAnswers} {...props} />
           </StyledWizard>
         )}
       </Transition>
