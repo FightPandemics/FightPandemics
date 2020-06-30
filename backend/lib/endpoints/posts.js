ea4a2b4d517fced2fa9979050a0debf513f22a1d
Neda Rohani <sonia@Nedas-MacBook-Pro.local>
2020-06-30 13:12:20 -0700

    remove space

--- backend/lib/endpoints/posts.js
+++ backend/lib/endpoints/posts.js
@@ -366,7 +366,7 @@ async function routes(app) {
       schema: deletePostSchema,
     },
     async (req) => {
-      const { userId } = req;      
+      const { userId } = req;    
       const { postId } = req.params;
       
       const [findErr, post] = await app.to(Post.findById(postId));
