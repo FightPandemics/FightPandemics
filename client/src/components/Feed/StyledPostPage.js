a2be437deaee0f7ed75341961ae8e2ed1d108c4a
briceida <BriceidaMariscal@gmail.com>
2020-07-03 16:58:27 -0700

    fixed styling

--- client/src/components/Feed/StyledPostPage.js
+++ client/src/components/Feed/StyledPostPage.js
@@ -2,20 +2,17 @@ import { mq,theme } from "constants/theme";
 import styled from "styled-components";
 import PostCard from "./PostCard";
 
-const { darkGray, lightGray, royalBlue, white } = theme.colors;
-
 export const StyledPostPage = styled.div`
-    width: 50%;
-    @media screen and (max-width: ${mq.phone.wide.maxWidth}) {
-        width: 100%;
-        position: relative;
-        overflow-wrap: break-word;
-
-    }      
+  width: 50%;
+  @media screen and (max-width: ${mq.phone.wide.maxWidth}) {
+    width: 100%;
+    position: relative;
+    overflow-wrap: break-word;
+  }      
 `;
 
 export const StyledPostPagePostCard = styled(PostCard)`
-    maxWidth: 80rem;
-    marginTop: 1rem;
-    overflowWrap: break-word;
+  maxWidth: 80rem;
+  marginTop: 1rem;
+  overflowWrap: break-word;
 `;
