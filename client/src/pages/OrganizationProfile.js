ea770320b4126cfba97009f53e53280f9476d44c
omarzoghayyer <ozoghayyer11@gmail.com>
2020-07-03 18:50:40 -0700

    replaced index 0 with 1

--- client/src/pages/OrganizationProfile.js
+++ client/src/pages/OrganizationProfile.js
@@ -195,7 +195,7 @@ const OrganizationProfile = () => {
       const nameArr = name.split(" ");
       if (nameArr.length < 2) {
         firstName = nameArr[0];
-        lastName = firstName.split("")[0]
+        lastName = firstName.split("")[1]
       } else {
         firstName = nameArr[0];
         lastName = nameArr[1];
