c112b8c685022294860b5f3f062dee4b1814325a
Josue V <idjevm@gmail.com>
2020-07-04 13:54:31 -0400

    reformat activity.js

--- client/src/components/Profile/Activity.js
+++ client/src/components/Profile/Activity.js
@@ -1,28 +1,28 @@
 import React from "react";
 import Post from "../Feed/Post";
 
-const Activity = ({ 
+const Activity = ({
   filteredPosts,
   updateComments,
   user,
   handlePostDelete,
   handleEditPost,
-  }) => {
+}) => {
   const posts = Object.entries(filteredPosts);
   return (
     <div className="activity">
       {!posts.length
         ? "No activity found"
         : posts.map(([key, post]) => (
-          <Post
-            currentPost={post}
-            updateComments={updateComments}
-            handlePostDelete={handlePostDelete}
-            onSelect={handleEditPost}
-            key={key}
-            user={user}
-          />
-        ))}
+            <Post
+              currentPost={post}
+              updateComments={updateComments}
+              handlePostDelete={handlePostDelete}
+              onSelect={handleEditPost}
+              key={key}
+              user={user}
+            />
+          ))}
     </div>
   );
 };
