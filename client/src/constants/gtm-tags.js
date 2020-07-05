892b0de453ff0c6c0f104cfa6d319822fdd13089
John <john.dev.work.01@gmail.com>
2020-07-05 13:26:13 -0700

    Add tags to GTM constants

---
+++ client/src/constants/gtm-tags.js
@@ -0,0 +1,15 @@
+module.exports = {
+  navBar: {
+    prefix: "NAV",
+    login: "_SI",
+    aboutUs: "_AU",
+    addOrg: "_ADO",
+    feed: "_HB",
+  },
+  homePage: {
+    prefix: "HP",
+    requestHelp: "_RE",
+    offerHelp: "_OF",
+    viewCommPost: "_VHB",
+  },
+};
