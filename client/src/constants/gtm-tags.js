1351f5b7be6687b483d7b44e1507e9903ed286f8
John <johnlamyc@gmail.com>
2020-07-06 08:03:57 -0700

    Add ID tags for Homepage and Navbar (#1010)
    * Add id tags to homepage elements
    
    * Add is tags to navigation elements
    
    * Refactor tags to pull from constants file
    
    * Add tags to GTM constants
    
    * Add tags to header
    
    * Rename navBar to nav
    
    Co-authored-by: John <john.dev.work.01@gmail.com>
    Co-authored-by: Josue V <idjevm@gmail.com>

--- client/src/constants/gtm-tags.js
+++ client/src/constants/gtm-tags.js
@@ -1,4 +1,19 @@
 module.exports = {
+  nav: {
+    prefix: "NAV",
+    login: "_SI",
+    register: "_JN",
+    aboutUs: "_AU",
+    addOrg: "_ADO",
+    feed: "_HB",
+    feedBack: "_FDB",
+  },
+  homePage: {
+    prefix: "HP",
+    requestHelp: "_RE",
+    offerHelp: "_OF",
+    viewCommPost: "_VHB",
+  },
   wizardNav: {
     next: "_NE",
     back: "_BA",
