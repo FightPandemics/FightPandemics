d36fd99ef6a5710c6af8523eca5046e2428831e3
omarzoghayyer <ozoghayyer11@gmail.com>
2020-07-11 19:56:32 -0700

    removed validation method and increased font size

--- client/src/components/CreatePost/Form/FirstSection.js
+++ client/src/components/CreatePost/Form/FirstSection.js
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Input } from "antd";
 import styled from "styled-components";
 import Head from "./Head";
 import { Section } from "../StyledModal";
 import { theme } from "constants/theme";
-import InputError from "components/Input/InputError";
-import { validateEmail } from "utils/validators";
 const { black } = theme.colors;
 
 const TitleInput = styled(Input)`
@@ -16,6 +14,7 @@ const TitleInput = styled(Input)`
 const { TextArea } = Input;
 const TextInput = styled(TextArea)`
   border-top: none;
+  font-size: 1.2rem !important;
   resize: none;
   overflow-y: scroll;
   font-weight: 450 !important;
@@ -27,14 +26,6 @@ const First = ({
   renderError,
   formData,
 }) => {
-  const [valid, setValid] = useState(false);
-  useEffect(() => {
-    const validated = !formData.title || validateEmail(formData.title);
-    setValid(validated);
-  }, [formData.title]);
-  const onChange = (event) => {
-    setValid(event.target.value);
-  };
   return (
     <Section>
       <Head number={1} title="Write your post here" />
@@ -42,12 +33,9 @@ const First = ({
         onChange={onChangeTitle}
         value={formData.title}
         placeholder="Title"
-        maxLength={20}
-        className={!valid && "has-error"}
+        maxLength={35}
         required
       />
-      {!valid && <InputError>Please fill out missing fields</InputError>}
-
       <TextInput
         onChange={onChangeDescription}
         value={formData.description}
