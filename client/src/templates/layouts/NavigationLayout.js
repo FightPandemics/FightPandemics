172ee64872fae9ebd4962aeb3b5afca689b78bae
omarzoghayyer <ozoghayyer11@gmail.com>
2020-07-11 21:00:12 -0700

    changed the thank you messege

--- client/src/templates/layouts/NavigationLayout.js
+++ client/src/templates/layouts/NavigationLayout.js
@@ -320,9 +320,10 @@ const NavigationLayout = (props) => {
     >
       <h2 className="title">Thank you!</h2>
       <p>
-        Your input means a lot and helps us help you and others during and after
-        the COVID-19 pandemic.
+        Your input means a lot and helps us improve our services during and
+        after the COVID-19 pandemic.
       </p>
+
       <Logo src={logo} alt="FightPandemics logo" />
     </ThanksModal>
   );
