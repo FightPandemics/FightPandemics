45c597a8f95f94bb36568f8f89a47e04914bfb2e
John <john.dev.work.01@gmail.com>
2020-07-15 15:37:59 -0700

    Use base url for Orgs, allow _ - / in validation

--- client/src/constants/urls.js
+++ client/src/constants/urls.js
@@ -1,6 +1,6 @@
 export const FACEBOOK_URL = "https://www.facebook.com/";
-export const LINKEDIN_INDIVIDUAL_URL = "https://www.linkedin.com/in/";
-export const LINKEDIN_ORGANISATION_URL = "https://www.linkedin.com/company/";
+export const LINKEDIN_URL = "https://www.linkedin.com/";
+export const LINKEDIN_INDIVIDUAL_URL = `${LINKEDIN_URL}in/`;
 export const TWITTER_URL = "https://twitter.com/";
 export const GITHUB_URL = "https://github.com/";
 export const PLAYSTORE_URL = "https://play.google.com/";
