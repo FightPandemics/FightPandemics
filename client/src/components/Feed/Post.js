9437e4842e2bee88f528b71eae1004b7b2933553
John <john.dev.work.01@gmail.com>
2020-07-16 12:14:17 -0700

    Fix kebab case

--- client/src/components/Feed/Post.js
+++ client/src/components/Feed/Post.js
@@ -340,7 +340,7 @@ const Post = ({
   const renderHeader = (
     <Card.Header
       title={
-        <div className="title-Wrapper">
+        <div className="title-wrapper">
           <div className="author">{post?.author?.name}</div>
           <div className="location-status">
             <SvgIcon src={statusIndicator} className="status-icon" />
