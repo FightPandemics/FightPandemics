834744c5a74dcffb958c287a8fe1f183af75c84e
Josue Villanueva <idjevm@gmail.com>
2020-07-17 20:44:23 -0400

    refactored initialization of GTM module (was being initialized on the wrong place)

--- client/src/index.js
+++ client/src/index.js
@@ -12,6 +12,13 @@ import App from "./App";
 import GlobalStyles from "./GlobalStyles";
 import * as serviceWorker from "./serviceWorker";
 import rootReducer from "./reducers";
+import TagManager from "react-gtm-module";
+
+const tagManagerArgs = {
+  gtmId: process.env.REACT_APP_GTM_ID,
+};
+
+TagManager.initialize(tagManagerArgs);
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
