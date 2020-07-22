630d180d8667e66eb696f0cbef602dec0e5c6483
Serhazor <sergej.dikun@gmail.com>
2020-07-22 21:55:26 +0100

    changed message to The acquisition of the geolocation failed because at least one internal source of position returned an internal error.

--- client/src/utils/geolocation.js
+++ client/src/utils/geolocation.js
@@ -7,7 +7,7 @@ export const asyncGetGeoLocation = () => {
       resolve({ lat: latitude, lng: longitude });
     };
     const onGeoError = () => {
-      reject(new Error("failed getting location"));
+      reject(new Error("The acquisition of the geolocation failed because at least one internal source of position returned an internal error."));
     };
     navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
   });
