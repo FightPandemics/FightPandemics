d5c148b5c9cad8dfe2187758d4c52bc0a37e80dc
Joeumar Souza <joeumar@gmail.com>
2020-07-25 11:45:08 -0300

    Fix for issue #1267

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,7 +7,7 @@ import styled from "styled-components";
 // ICONS
 import SvgIcon from "./Icon/SvgIcon";
 import MenuIcon from "assets/icons/menu.svg";
-import envelope from "assets/icons/envelope.svg";
+import feedback from "assets/icons/feedback.svg";
 import logo from "assets/logo.svg";
 import Logo from "./Logo";
 
@@ -147,9 +147,12 @@ export default ({
           ))
         : null}
       {user?.organisations?.length > 0 && <Menu.Divider />}
-      <Menu.Item 
-      id={GTM.nav.prefix + GTM.nav.feedback}
-      onClick={onFeedbackIconClick}>Feedback</Menu.Item>
+      <Menu.Item
+        id={GTM.nav.prefix + GTM.nav.feedback}
+        onClick={onFeedbackIconClick}
+      >
+        Feedback
+      </Menu.Item>
       <Menu.Divider />
       <Menu.Item>
         <Link to="/auth/logout">Sign Out</Link>
@@ -215,7 +218,7 @@ export default ({
               id={GTM.nav.prefix + GTM.nav.feedback}
               onClick={onFeedbackIconClick}
             >
-              <SvgIcon src={envelope} />
+              <SvgIcon src={feedback} />
             </button>
           </>
         )}
