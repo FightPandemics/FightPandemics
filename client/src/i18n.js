cfcd3d3989afa0374317d04dffd7b12e7976af67
tr1503 <hanlinye6@gmail.com>
2020-08-06 14:24:45 -0400

    Change the tab format

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -5,34 +5,34 @@ import Backend from 'i18next-http-backend';
 import resources from './locales/resources';
 
 export const Languages = {
-    en: 'en',
-    en_US: 'en',
-    ar: 'ar',
-    id: 'id'
+  en: 'en',
+  en_US: 'en',
+  ar: 'ar',
+  id: 'id'
 }
 
 export function getLang() {
-    const systemLang = navigator.language;
-    const primaryLang = Object.values(Languages).includes(systemLang) ? systemLang : Languages.en;
-    let res = Languages.en;
+  const systemLang = navigator.language;
+  const primaryLang = Object.values(Languages).includes(systemLang) ? systemLang : Languages.en;
+  let res = Languages.en;
 
-    const language = primaryLang;
-    if (resources[language]) 
-        res = language;
-    // window.localStorage.setItem('locale', res);
-    return res;
+  const language = primaryLang;
+  if (resources[language])
+    res = language;
+  // window.localStorage.setItem('locale', res);
+  return res;
 }
 
 i18n.use(Backend)
-    .use(languageDetector)
-    .use(initReactI18next)
-    .init({
-        resources,
-        lng: getLang(),
-        fallbackLng: Languages.en,
-        interpolation: {
-            escapeValue: false,
-        },
-    });
+  .use(languageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: getLang(),
+    fallbackLng: Languages.en,
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
 export default i18n;
