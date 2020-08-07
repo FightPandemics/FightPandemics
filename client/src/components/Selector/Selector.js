821c232ba7cac183e60bc36002c25c153eb3029f
John <john.dev.work.01@gmail.com>
2020-08-07 12:26:05 -0700

    Clean up Selector

--- client/src/components/Selector/Selector.js
+++ client/src/components/Selector/Selector.js
@@ -2,31 +2,26 @@ import React from "react";
 import StyledSelector from "./StyledSelector";
 const { Option } = StyledSelector;
 
-const BaseSelector = (props) => {
-  const {
-    options,
-    key = "text",
-    optionProps,
-    onChange,
-    suffixIcon,
-    filterOptions,
-    defaultValue,
-  } = props;
-
-  return (
-    <StyledSelector
-      suffixIcon={suffixIcon}
-      defaultValue={defaultValue}
-      filterOptions={filterOptions}
-      onChange={onChange}
-    >
-      {options.map((item) => (
-        <Option {...optionProps} key={item.value} value={item.value}>
-          {item[key]}
-        </Option>
-      ))}
-    </StyledSelector>
-  );
-};
-
+const BaseSelector = ({
+  options,
+  key = "text",
+  optionProps,
+  onChange,
+  suffixIcon,
+  filterOptions,
+  defaultValue,
+}) => (
+  <StyledSelector
+    suffixIcon={suffixIcon}
+    defaultValue={defaultValue}
+    filterOptions={filterOptions}
+    onChange={onChange}
+  >
+    {options.map((item) => (
+      <Option {...optionProps} key={item.value} value={item.value}>
+        {item[key]}
+      </Option>
+    ))}
+  </StyledSelector>
+);
 export default BaseSelector;
