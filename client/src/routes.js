cf833886fc968b477f61ef868b2ad7448a0647af
Nadine Juraschek <nadine.jura@online.de>
2020-08-10 23:49:41 +0200

    Error Page created (#1333)
    * error page styling finished
    
    * Button sends user to Help Board
    
    * component name changed to ErrorPage
    
    * updated name to NotFoundPage (more descriptive)
    
    Co-authored-by: Josue Villanueva <idjevm@gmail.com>

--- client/src/routes.js
+++ client/src/routes.js
@@ -14,6 +14,7 @@ import CookiesPolicy from "./pages/CookiesPolicy";
 import Profile from "./pages/Profile";
 import EditProfile from "./pages/EditProfile";
 import EditAccount from "./pages/EditAccount";
+import NotFoundPage from "./pages/NotFoundPage";
 import Feed from "./containers/FeedContainer";
 import Login from "./pages/Login";
 import VerifyEmail from "./pages/VerifyEmail";
@@ -211,6 +212,11 @@ const routes = [
   {
     path: "/faq",
     component: Faq,
+  },
+  {
+    path: "*",
+    component: NotFoundPage,
+    exact: true,
   },
 ];
 
