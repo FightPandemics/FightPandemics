9bfb0eda8eeeeafb330c37759af235fcceb8f0ea
Le Bao Phuc <lbphuc1108@gmail.com>
2020-08-12 14:16:44 +0700

    fix the shivering elements

--- client/src/components/CreatePost/StyledModal.js
+++ client/src/components/CreatePost/StyledModal.js
@@ -36,6 +36,27 @@ export const Section = styled.section`
   .ant-input::placeholder {
     color: ${colors.darkGray};
   }
+
+  .visibility-post--selector {
+    margin-right: -0.5rem;
+    margin-left: -0.5rem;
+  }
+
+  @media screen and (min-width: ${mq.phone.wide.minWidth}) {
+    .visibility-post {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+    .visibility-post--info {
+      margin-bottom: 0;
+    }
+    .visibility-post--selector {
+      display: flex;
+      justify-content: flex-start;
+      align-items: cener;
+    }
+  }
 `;
 
 export const HeadWrapper = styled.div`
@@ -141,17 +162,8 @@ export const ModalWrapper = styled(Modal)`
 `;
 
 export const SubTitle = styled.p`
-  font-family: ${typography.font.family.display};
-  font-style: normal;
-  font-weight: normal;
   font-size: 1.1rem;
   line-height: 140%;
-  margin: 0 3rem 2rem 2.8rem;
-
-  @media screen and (min-width: ${mq.phone.wide.minWidth}) {
-    margin-bottom: 0;
-    display: inline-block;
-  }
 `;
 
 export const Selector = styled(BaseSelector)``;
