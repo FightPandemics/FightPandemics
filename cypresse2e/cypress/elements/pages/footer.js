82cab275474baac79dc7c5db64ac48eee1a36ee4
jessica-lau <jessica.lau2@gmail.com>
2020-08-14 21:39:30 -0400

    add tests for footer section and sign in page

---
+++ cypresse2e/cypress/elements/pages/footer.js
@@ -0,0 +1,46 @@
+class Footer {
+
+    copyright = 'footer div:nth-child(1)';
+    aboutUs = 'footer a:nth-child(1)';
+    faq = 'footer a:nth-child(2)';
+    blog = 'footer a:nth-child(3)';
+    termsAndConditions = 'footer a:nth-child(4)';
+    privacyPolicy = 'footer a:nth-child(5)';
+    cookiesPolicy = 'footer a:nth-child(6)';
+
+    constructor() { }
+
+    visit() {
+        cy.visit('');
+    }
+
+    getCopyright() {
+        return cy.get(this.copyright);
+    }
+
+    getAboutUs() {
+        return cy.get(this.aboutUs);
+    }
+
+    getFaq() {
+        return cy.get(this.faq);
+    }
+
+    getBlog() {
+        return cy.get(this.blog);
+    }
+
+    getTermsAndConditions() {
+        return cy.get(this.termsAndConditions);
+    }
+
+    getPrivacyPolicy() {
+        return cy.get(this.privacyPolicy);
+    }
+
+    getCookiesPolicy() {
+        return cy.get(this.cookiesPolicy);
+    }
+}
+
+export default Footer;
---
+++ cypresse2e/cypress/integration/e2eTests/signIn/signIn.spec.js
@@ -0,0 +1,162 @@
+import SignIn from '../../../elements/pages/signIn';
+
+describe('FightPandemics Sign In Page', () => {
+
+    const signIn = new SignIn();
+
+    context('User signs into account', () => {
+        beforeEach(() => {
+            signIn.visit();
+        });
+
+        it('FP logo is visible and clickable', () => {
+            var fpLogo = signIn.getFpLogo();
+            fpLogo.should('be.visible').and('have.attr', 'alt', 'Fight Pandemics logo').click();
+
+        });
+
+        it('Sign in page contains heading and image', () => {
+            var h4Heading = "Sign In";
+            signIn.getH4Heading().should('be.visible').contains(h4Heading);
+
+            signIn.getImage().should('be.visible');
+
+        });
+
+        it('Email address field is visible and can be populated', () => {
+            var emailField = signIn.getEmailField();
+            emailField.should('be.visible').and('have.attr', 'name', 'email');
+            emailField.type('qa.test@gmail.com');
+
+        });
+
+        it('Leaving email field blank triggers error', () => {
+            var emailField = signIn.getEmailField();
+            emailField.should('be.visible').and('have.attr', 'name', 'email').focus().blur();
+            var emailRequired = signIn.getEmailRequired();
+            emailRequired.should('be.visible');
+            emailRequired.contains('Email is required.');
+
+        });
+
+        it('Entering invalid email triggers error', () => {
+            var emailField = signIn.getEmailField();
+            emailField.should('be.visible').and('have.attr', 'name', 'email');
+            emailField.type('qa.test@').focus().blur();
+            var validEmailRequired = signIn.getValidEmailRequired();
+            validEmailRequired.should('be.visible');
+            validEmailRequired.contains('Invalid email');
+
+        });
+
+        it('Password field is visible and can be populated', () => {
+            var passwordField = signIn.getPasswordField();
+            passwordField.should('be.visible').and('have.attr', 'name', 'password');
+            passwordField.type('Testing!');
+
+        });
+
+        it('Clicking the password eye displays the password entered', () => {
+            var passwordField = signIn.getPasswordField();
+            passwordField.should('be.visible').and('have.attr', 'name', 'password');
+            passwordField.type('Testing!');
+            var passwordEye = signIn.getPasswordEye();
+            passwordEye.should('be.visible').and('have.attr', 'alt', 'Icon').click();
+
+        });
+
+        it('Leaving password field blank triggers error', () => {
+            var passwordField = signIn.getPasswordField();
+            passwordField.should('be.visible').and('have.attr', 'name', 'password').focus().blur();
+            var passwordRequired = signIn.getPasswordRequired();
+            passwordRequired.should('be.visible');
+            passwordRequired.contains('Password is required.');
+
+        });
+
+        it('Password entered not meeting 8 characters triggers error', () => {
+            var passwordField = signIn.getPasswordField();
+            passwordField.should('be.visible');
+            passwordField.type('test');
+            var passwordLengthRequired = signIn.getPasswordLengthRequired();
+            passwordLengthRequired.should('be.visible');
+            passwordLengthRequired.contains('Password must be at least 8 characters');
+
+        });
+
+        it('Password entered not meeting special characters triggers error', () => {
+            var passwordField = signIn.getPasswordField();
+            passwordField.should('be.visible');
+            passwordField.type('testtest');
+            var passwordCharacterRequired = signIn.getPasswordCharacterRequired();
+            passwordCharacterRequired.should('be.visible');
+            passwordCharacterRequired.contains('Password must contain at least 3 of these: a lower-case letter, an upper-case letter, a number, a special character (such as !@#$%^&*).');
+
+        });
+
+        it('Sign in button is disabled when required details are not entered', () => {
+            var signInButton = signIn.getSignInButton();
+            signInButton.should('be.visible').and('have.attr', 'aria-disabled', 'true');
+
+        });
+
+        it('Sign in button is enabled when required details are entered', () => {
+            var emailField = signIn.getEmailField();
+            emailField.should('be.visible').and('have.attr', 'name', 'email');
+            emailField.type('qa.test@gmail.com');
+            var passwordField = signIn.getPasswordField();
+            passwordField.should('be.visible').and('have.attr', 'name', 'password');
+            passwordField.type('Testing!');
+            var signInButton = signIn.getSignInButton();
+            signInButton.should('be.visible').and('have.attr', 'aria-disabled', 'false');
+
+        });
+
+        it('Login fail alert appears when incorrect email & password are entered and submitted by user', () => {
+            var emailField = signIn.getEmailField();
+            emailField.should('be.visible').and('have.attr', 'name', 'email');
+            emailField.type('qa.test@gmail.com');
+            var passwordField = signIn.getPasswordField();
+            passwordField.should('be.visible').and('have.attr', 'name', 'password');
+            passwordField.type('WrongPW!wpw3');
+            var signInButton = signIn.getSignInButton();
+            signInButton.should('be.visible').and('have.attr', 'aria-disabled', 'false').click();
+
+        });
+
+        it('Forgot password link is visible and clickable', () => {
+            var forgotPasswordLink = signIn.getForgotPasswordLink();
+            forgotPasswordLink.should('be.visible');
+            forgotPasswordLink.contains('a', 'Forgot password?').click();
+
+        });
+
+        it('Join now link is visible and clickable', () => {
+            var joinNowLink = signIn.getJoinNowLink();
+            joinNowLink.should('be.visible');
+            joinNowLink.contains('u', 'Join Now').click();
+
+        });
+
+        it('Facebook button for sign in is visible', () => {
+            var signInByFbButton = signIn.getSignInFbButton();
+            signInByFbButton.should('be.visible');
+            signInByFbButton.contains('span', 'Facebook');
+
+        });
+
+        it('Google button for sign in is visible', () => {
+            var signInByGoogleButton = signIn.getSignInGoogleButton();
+            signInByGoogleButton.should('be.visible');
+            signInByGoogleButton.contains('span', 'Google');
+
+        });
+
+        it('LinkedIn button for sign in is visible', () => {
+            var signInByLinkedinButton = signIn.getSignInLinkedinButton();
+            signInByLinkedinButton.should('be.visible');
+            signInByLinkedinButton.contains('span', 'Linkedin');
+
+        });
+    });
+});
