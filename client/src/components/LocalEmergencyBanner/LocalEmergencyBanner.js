4b83e112225e39554f7145b41d7356ed0c6cce0f
joshmorel <morel.josh@hotmail.com>
2020-08-23 17:10:48 -0400

    fix formatting + remove render fnc
    better to return null if displayBanner false

--- client/src/components/LocalEmergencyBanner/LocalEmergencyBanner.js
+++ client/src/components/LocalEmergencyBanner/LocalEmergencyBanner.js
@@ -1,29 +1,28 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
 import { Icon } from "antd-mobile";
 import { theme } from "../../constants/theme";
-// import localEmergencyData from './localEmergencyData';
-import localEmergencyData from './emergency-numbers-by-code'
-import axios from "axios"
+import localEmergencyData from "./emergency-numbers-by-code";
 
 const { typography, colors } = theme;
 
 const LocalEmergencyBannerStyle = styled.div`
   display: flex;
   justify-content: center;
-  background-color: #D1222C;
+  background-color: #d1222c;
   color: ${colors.white};
-  textAlign: ${typography.paragraph};
+  textalign: ${typography.paragraph};
   & .close {
-      align-self: flex-start;
-      & button {
-        position: relative;
-        padding: 0;
-        border: none;
-        background: transparent;
-        cursor: pointer;
-      }
+    align-self: flex-start;
+    & button {
+      position: relative;
+      padding: 0;
+      border: none;
+      background: transparent;
+      cursor: pointer;
     }
+  }
   .localEmergencyNumber {
     flex: 1 0 auto;
     font-size: ${typography.size};
@@ -31,7 +30,7 @@ const LocalEmergencyBannerStyle = styled.div`
     align-self: center;
     padding: 10px;
   }
-  .emergencyNumberText{
+  .emergencyNumberText {
     font-size: 22px;
   }
   .emergencyNumber {
@@ -42,54 +41,45 @@ const LocalEmergencyBannerStyle = styled.div`
 
 const LocalEmergencyBanner = () => {
   const [displayBanner, setDisplayBanner] = useState(true);
-  const [localEmergencyNumber, setLocalEmergencyNumber] = useState("")
+  const [localEmergencyNumber, setLocalEmergencyNumber] = useState("");
 
   // Shows banner until client closes
-  const bannerDisplay = () => {
+  if (!displayBanner) return null;
 
-    if (!displayBanner){
-      return <div></div>;
-    } else {
-        axios.get('https://extreme-ip-lookup.com/json/')
-          .then(ipInfo => {
-            let countryCode = localEmergencyData[ipInfo.data.countryCode];
-            if(countryCode.Ambulance === 0){
-              return '000'
-            } else if(typeof countryCode.Ambulance === "string"){
-              let updatedAmbulance = countryCode.Ambulance.replace(/[/]/g, " or ")
-              return updatedAmbulance
-            } else {
-              return countryCode.Ambulance
-            }
-          })
-          .then(emergencyNumber => {
-            setLocalEmergencyNumber(emergencyNumber);
-          })
-          .catch(() => {
-            setLocalEmergencyNumber('location unavailable')
-          });
+  axios
+    .get("https://extreme-ip-lookup.com/json/")
+    .then((ipInfo) => {
+      let countryCode = localEmergencyData[ipInfo.data.countryCode];
+      if (countryCode.Ambulance === 0) {
+        return "000";
+      } else if (typeof countryCode.Ambulance === "string") {
+        let updatedAmbulance = countryCode.Ambulance.replace(/[/]/g, " or ");
+        return updatedAmbulance;
+      } else {
+        return countryCode.Ambulance;
+      }
+    })
+    .then((emergencyNumber) => {
+      setLocalEmergencyNumber(emergencyNumber);
+    })
+    .catch(() => {
+      setLocalEmergencyNumber("location unavailable");
+    });
 
-      return (
-          <LocalEmergencyBannerStyle>
-            <div className="localEmergencyNumber">
-              <span className="emergencyNumberText">Local Emergency Number</span>
-              <br />
-              <span className="emergencyNumber">{localEmergencyNumber}</span>
-            </div>
-            
-            <div className="close">
-              <button onClick={() => setDisplayBanner(!displayBanner)}>
-                <Icon type="cross" size="lg" />
-              </button>
-            </div>
-          </LocalEmergencyBannerStyle>
-      );
-    };
-  };
   return (
-    <div>
-      {bannerDisplay()}
-    </div>
+    <LocalEmergencyBannerStyle>
+      <div className="localEmergencyNumber">
+        <span className="emergencyNumberText">Local Emergency Number</span>
+        <br />
+        <span className="emergencyNumber">{localEmergencyNumber}</span>
+      </div>
+
+      <div className="close">
+        <button onClick={() => setDisplayBanner(!displayBanner)}>
+          <Icon type="cross" size="lg" />
+        </button>
+      </div>
+    </LocalEmergencyBannerStyle>
   );
 };
 
