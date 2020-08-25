0cb133cbd1dd8d6d8d61168431b0ee6750a47900
John <john.dev.work.01@gmail.com>
2020-08-25 15:04:19 -0700

    Add useSuspense: false to i18next config

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -1,29 +1,31 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import languageDetector from 'i18next-browser-languagedetector';
-import Backend from 'i18next-http-backend';
-import resources from './locales/resources';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import languageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
+import resources from "./locales/resources";
 
 export const Languages = {
-  en: 'en',
-  en_US: 'en',
-  ar: 'ar',
-  id: 'id'
-}
+  en: "en",
+  en_US: "en",
+  ar: "ar",
+  id: "id",
+};
 
 export function getLang() {
   const systemLang = navigator.language;
-  const primaryLang = Object.values(Languages).includes(systemLang) ? systemLang : Languages.en;
+  const primaryLang = Object.values(Languages).includes(systemLang)
+    ? systemLang
+    : Languages.en;
   let res = Languages.en;
 
   const language = primaryLang;
-  if (resources[language])
-    res = language;
+  if (resources[language]) res = language;
   // window.localStorage.setItem('locale', res);
   return res;
 }
 
-i18n.use(Backend)
+i18n
+  .use(Backend)
   .use(languageDetector)
   .use(initReactI18next)
   .init({
@@ -32,6 +34,9 @@ i18n.use(Backend)
     fallbackLng: Languages.en,
     interpolation: {
       escapeValue: false,
+    },
+    react: {
+      useSuspense: false,
     },
   });
 
