1aa4519dd51a49037153f0162d34b1961aa41e17
tr1503 <hanlinye6@gmail.com>
2020-08-26 15:31:59 -0400

    Add language switch, needs to modify the style later

--- client/src/components/Footnote.js
+++ client/src/components/Footnote.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { Trans, useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
+import { Menu, Dropdown } from "antd";
 import { theme, mq } from "constants/theme";
 
 import styled from "styled-components";
+
+import i18n from "../i18n";
 
 const { colors } = theme;
 
@@ -63,6 +66,36 @@ const FooterALink = styled.a`
 export default () => {
   const currentYear = new Date().getFullYear();
   const { t } = useTranslation();
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
+
+  const langs = {
+    "ar": {value: "العربية", text: "Arabic (العربية)"},
+    "id": {value: "Bahasa Indonesia", text: "Bahasa Indonesia (Bahasa Indonesia)"},
+    "zh-TW": {value: "繁體中文", text: "Chinese Traditional (繁體中文)"},
+    "nl": {value: "Nederlands", text: "Dutch (Nederlands)"},
+    "en": {value: "English", text: "English (English)"},
+    "fr": {value: "Français", text: "French (Français)"},
+    "de": {value: "Deutsch", text: "German (Deutsch)"},
+    "he": {value: "עברית", text: "Hebrew (עברית)"},
+    "it": {value: "Italiano", text: "Italian (Italiano)"},
+    "pt": {value: "Português", text: "Portuguese (Português)"},
+    "ru": {value: "русский", text: "Russian (русский)"},
+    "es": {value: "Español", text: "Spain (Español)"},
+    "tr": {value: "Türkçe", text: "Turkish (Türkçe)"},
+    "uk": {value: "Українська", text: "Ukrainian (Українська)"}
+  }
+
+  const menu = (
+    <Menu>
+      {Object.entries(langs).map(([key, label]) => (
+        <Menu.Item>
+          <a onClick={() => changeLanguage(key)}>{label.text}</a>
+        </Menu.Item>
+      ))}
+    </Menu>
+  );
 
   return (
     <StyledFooter>
@@ -83,7 +116,10 @@ export default () => {
         </FooterALink>
   |     <FooterLink to={"/terms-conditions"}>{t("footer.termsConditions")}</FooterLink> |{" "}
         <FooterLink to={"/privacy-policy"}>{t("footer.privacyPolicy")}</FooterLink> |{" "}
-        <FooterLink to={"/cookies-policy"}>{t("footer.cookiesPolicy")}</FooterLink>
+        <FooterLink to={"/cookies-policy"}>{t("footer.cookiesPolicy")}</FooterLink> |{" "}
+        <Dropdown overlay={menu} placement="topLeft">
+          <FooterLink>{langs[i18n.language].value}</FooterLink>
+        </Dropdown>
       </Policies>
     </StyledFooter>
   );
