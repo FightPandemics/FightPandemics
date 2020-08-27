ed8361b89834fdc4e3738c8f3795f5318c44bdec
John <john.dev.work.01@gmail.com>
2020-08-26 17:27:36 -0700

    Move localization and languages objects to constants

--- client/src/components/Footnote.js
+++ client/src/components/Footnote.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Trans, useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import { Menu, Dropdown } from "antd";
-import { theme, mq } from "constants/theme";
-
 import styled from "styled-components";
 
 import i18n from "../i18n";
+
+import { theme, mq } from "constants/theme";
+import { localization, languages } from "constants/languages";
 import SvgIcon from "./Icon/SvgIcon";
 import globe from "assets/icons/globe.svg";
 
@@ -66,46 +67,9 @@ export default () => {
     i18n.changeLanguage(lng);
   };
 
-  const localization = {
-    en: "en-US",
-    "en-US": "en-US",
-    "en-GB": "en-GB",
-    ar: "ar",
-    id: "id",
-    "zh-TW": "zh-TW",
-    nl: "nl",
-    fr: "fr",
-    de: "de",
-    he: "he",
-    it: "it",
-    pt: "pt",
-    ru: "ru",
-    es: "es",
-    tr: "tr",
-    uk: "uk",
-  };
-
-  const langs = {
-    ar: { value: "العربية", text: "Arabic (العربية)" },
-    id: { value: "Bahasa Indonesia", text: "Indonesian (Bahasa Indonesia)" },
-    "zh-TW": { value: "繁體中文", text: "Chinese Traditional (繁體中文)" },
-    nl: { value: "Nederlands", text: "Dutch (Nederlands)" },
-    "en-US": { value: "English", text: "English (American English)" },
-    "en-GB": { value: "English", text: "English (British English)" },
-    fr: { value: "Français", text: "French (Français)" },
-    de: { value: "Deutsch", text: "German (Deutsch)" },
-    he: { value: "עברית", text: "Hebrew (עברית)" },
-    it: { value: "Italiano", text: "Italian (Italiano)" },
-    pt: { value: "Português", text: "Portuguese (Português)" },
-    ru: { value: "русский", text: "Russian (русский)" },
-    es: { value: "Español", text: "Spain (Español)" },
-    tr: { value: "Türkçe", text: "Turkish (Türkçe)" },
-    uk: { value: "Українська", text: "Ukrainian (Українська)" },
-  };
-
   const menu = (
     <Menu>
-      {Object.entries(langs).map(([key, label]) => (
+      {Object.entries(languages).map(([key, label]) => (
         <Menu.Item>
           <a onClick={() => changeLanguage(key)}>{label.text}</a>
         </Menu.Item>
@@ -154,7 +118,7 @@ export default () => {
         <StyledSpan>
           <SvgIcon src={globe} className="globe-icon-svg"></SvgIcon>
           <Dropdown overlay={menu} placement="topLeft">
-            <Link>{langs[localization[i18n.language]].value}</Link>
+            <Link>{languages[localization[i18n.language]].value}</Link>
           </Dropdown>
         </StyledSpan>
       </StyledDiv>
