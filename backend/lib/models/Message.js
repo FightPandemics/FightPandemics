72a22c84a210acb346b2a224ef3c4b418cd8bf79
Nícolas de Araujo <nicolas.oreques@gmail.com>
2020-09-07 23:21:27 -0300

    Add Message model

---
+++ backend/lib/models/Message.js
@@ -0,0 +1,75 @@
+// -- Imports
+const { Schema, model, ObjectId } = require("mongoose");
+const { schema: authorSchema } = require("./Author");
+const { OBJECTIVES: POST_OBJECTIVES } = require("./Post");
+
+const MESSAGE_STATUS_OPTIONS = ["deleted", "edited", "sent"]
+
+// -- Schema
+const postRefSchema = new Schema(
+  {
+    author: Object,
+    content: {
+      required: true,
+      type: String,
+    },
+    id: {
+      ref: "Post",
+      required: true,
+      type: ObjectId,
+    },
+    objective: {
+      enum: OBJECTIVES,
+      required: true,
+      trim: true,
+      type: String,
+    },
+    title: {
+      required: true,
+      type: String,
+    }
+  }
+)
+
+const messageSchema = new Schema(
+  {
+    author: Object,
+    content: {
+      required: true,
+      trim: true,
+      type: String,
+    },
+    postRef: postRefSchema,
+    status: {
+      default: "sent",
+      enum: MESSAGE_STATUS_OPTIONS,
+      lowercase: true,
+      required: true,
+      trim: true,
+      type: String,
+    },
+    threadId: {
+      ref: "Thread",
+      type: ObjectId,
+    },
+  },
+  { collection: "messages", timestamps: true },
+);
+
+// -- Indexes
+/* eslint-disable */
+// Index to get messages of a conversation ordered by creation date (_id)uses
+// creation timestamp)
+messageSchema.index({
+  "threadId": 1,
+  "status": 1,
+  "_id": -1
+});
+/* eslint-enable */
+
+const Message = model("Message", messageSchema);
+
+module.exports = {
+  model: Message,
+  schema: messageSchema,
+};
