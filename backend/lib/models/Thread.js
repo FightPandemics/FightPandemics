b70525ea96eb157a9093acd56da2c7326c685eda
Nícolas de Araujo <nicolas.oreques@gmail.com>
2020-09-07 23:21:20 -0300

    Add Thread model

---
+++ backend/lib/models/Thread.js
@@ -0,0 +1,70 @@
+// -- Imports
+const { Schema, model, ObjectId } = require("mongoose");
+
+const CONVERSATION_STATUS_OPTIONS = [
+  "accepted", "archived", "blocked", "pending"
+];
+
+// -- Schema
+const participantSchema = new Schema(
+  {
+    id: {
+      ref: "User",
+      required: true,
+      type: ObjectId,
+    },
+    location: locationSchema,
+    name: {
+      required: true,
+      type: String,
+    },
+    newMessages: {
+      default: false,
+      required: true,
+      type: Boolean
+    },
+    photo: String,
+    type: {
+      enum: USER_TYPES,
+      required: true,
+      trim: true,
+      type: String,
+    },
+  }
+)
+
+const threadSchema = new Schema(
+  {
+    participants: {
+      type: [participantSchema]
+    },
+    status: {
+      default: "accepted",
+      enum: CONVERSATION_STATUS_OPTIONS,
+      lowercase: true,
+      required: true,
+      trim: true,
+      type: String,
+    },
+  },
+  { collection: "threads", timestamps: true },
+);
+
+// -- Indexes
+/* eslint-disable */
+// Index to get inbox sorted by most recent
+threadSchema.index({
+  "participants.id": 1,
+  "status": 1,
+  "updatedAt": -1
+});
+/* eslint-enable */
+
+// -- Model
+const Thread = model("Thread", threadSchema);
+
+module.exports = {
+  CONVERSATION_STATUS_OPTIONS,
+  model: Thread,
+  schema: threadSchema,
+};
