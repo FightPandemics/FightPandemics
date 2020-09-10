fb610530ed698f274cefed09119063a4be1709fd
John <john.dev.work.01@gmail.com>
2020-09-10 00:44:02 -0700

    Refactor getOptionText call

--- client/src/components/Feed/FiltersList.js
+++ client/src/components/Feed/FiltersList.js
@@ -5,21 +5,9 @@ import { useTranslation } from "react-i18next";
 // Local
 import { FeedContext } from "pages/Feed.js";
 import ButtonTag from "../Tag/ButtonTag.js";
+import { getOptionText } from "components/Feed/utils";
 
 const LOCATION_DISPLAY_LENGTH_MAX = 30;
-
-const getOptionText = (filterOptions, filterLabel, option) => {
-  let optionValue;
-  if (typeof option === "string") {
-    optionValue = option;
-  } else if (typeof option === "object") {
-    optionValue = option.value;
-  }
-
-  return filterOptions
-    .filter(({ label }) => label === filterLabel)[0]
-    .options.filter(({ value }) => value === optionValue)[0].text;
-};
 
 const FiltersList = () => {
   const { t } = useTranslation();
@@ -51,7 +39,13 @@ const FiltersList = () => {
             onClick={handleOption(filter, option)}
             className="tag-closable"
           >
-            {t(getOptionText(filters, filter, option))}
+            {t(
+              getOptionText(
+                filters,
+                filter,
+                option.value ? option.value : option,
+              ),
+            )}
           </ButtonTag>
         )),
       )}
