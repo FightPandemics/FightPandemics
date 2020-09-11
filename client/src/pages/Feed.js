2ecaff617915658bb13ceed80119d2f47ab620b9
John <john.dev.work.01@gmail.com>
2020-09-11 16:31:32 -0700

    Clean up

--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -717,7 +717,7 @@ const Feed = (props) => {
                       onClick={handleCreatePost}
                     />,
                   ]}
-                ></Trans>
+                />
               </NoPosts>
             ) : (
               <CreatePostIcon
