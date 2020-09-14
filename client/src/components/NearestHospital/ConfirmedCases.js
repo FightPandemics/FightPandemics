887baef761cde1df1ece97675c5046358c2695b9
Chris Ohlmacher <55671489+cjohlmacher@users.noreply.github.com>
2020-09-14 14:12:34 -0700

    initial draft feature 1412

--- client/src/components/NearestHospital/ConfirmedCases.js
+++ client/src/components/NearestHospital/ConfirmedCases.js
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import styled from "styled-components";
 
 import SearchInput from "../Input/SearchInput";
+import { getLocalStorageJson } from "utils/local-storage";
 
 import { theme, mq } from "../../constants/theme";
 const { typography, colors } = theme;
@@ -64,6 +66,61 @@ const SearchBarContainer = styled.div`
 `;
 
 const NearestHealthFacilities = (props) => {
+  //TO-DO: Pull location data.
+  const country = "US";
+  const local = "Miami";
+  const latitude = "25.00";
+  const longitude = "-80.00";
+
+  const findBestLocationMatch = (locationList) => {
+    return locationList.map((location) => {
+      return {
+        ...location,
+        distance: Math.abs(location?.latitude - latitude) + Math.abs(location?.longitude - longitude),
+    };
+  }).reduce((acc, cur) => {
+      if (acc.distance < cur.distance) {
+        return acc;
+      } else {
+        return cur;
+      };
+    });
+  };
+  
+  const [caseCountGlobal, setCaseCountGlobal] = useState(null);
+  const [caseCountCountry, setCaseCountCountry] = useState(null);
+  const [caseCountLocal, setCaseCountLocal] = useState(null);
+
+  useEffect(() => {
+    const casesUrl = `https://api.covid19api.com/summary`;
+    axios
+      .get(casesUrl)
+      .then((res) => {
+        const globalCount = res?.data?.Global?.TotalConfirmed;
+        const countryList = res?.data?.Countries;
+        const countryData = countryList.find((countryObject) => {
+          return countryObject.CountryCode === country;
+        });
+        const countryCount = countryData.TotalConfirmed;
+        setCaseCountGlobal(globalCount);
+        setCaseCountCountry(countryCount);
+      })
+      .catch((err) => console.error(err));
+  },[]);
+
+  useEffect(() => {
+    const casesUrl = `https://www.trackcorona.live/api/cities/${local}`;
+    axios
+      .get(casesUrl)
+      .then((res) => {
+        const localList = res?.data?.data;
+        const localData = findBestLocationMatch(localList);
+        const localCount = localData.confirmed;
+        setCaseCountLocal(localCount);
+      })
+      .catch((err) => console.error(err));
+  },[]);
+  
   const [searchValue, setSearchValue] = useState("");
 
   const onSearchInputChange = (value) => {
@@ -85,15 +142,15 @@ const NearestHealthFacilities = (props) => {
       </SearchBarContainer>
       <ConfirmedCasesContent>
         <div>
-          <h2>420</h2>
-          <p>Confirmed cases in Miami</p>
+          <h2>{caseCountLocal === null ? 'No data' : caseCountLocal.toLocaleString('en')}</h2>
+          <p>Confirmed cases in {local}</p>
         </div>
         <div>
-          <h2>2,051</h2>
-          <p>Confirmed cases in USA</p>
+          <h2>{caseCountCountry === null ? 'No data' : caseCountCountry.toLocaleString('en')}</h2>
+          <p>Confirmed cases in {country}</p>
         </div>
         <div>
-          <h2>56,462</h2>
+          <h2>{caseCountGlobal === null ? 'No data' : caseCountGlobal.toLocaleString('en')}</h2>
           <p>Confirmed cases in World</p>
         </div>
       </ConfirmedCasesContent>
