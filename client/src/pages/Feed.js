6e1c4d5e58a7ac711654a6421585e905e34ee81e
Zaydme <zaydme8@gmail.com>
2020-09-25 17:10:48 +0100

    eslint-disable-line

--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -410,7 +410,7 @@ const Feed = (props) => {
         handleChangeType({ key: "ALL" });
       dispatchAction(SET_VALUE, "searchCategory", selectedValueId);
       dispatchAction(SET_VALUE, "searchKeyword", inputValue);
-      refetchPosts(); // eslint-disable-line react-hooks/exhaustive-deps
+      refetchPosts();
     },
   );
 
@@ -418,7 +418,7 @@ const Feed = (props) => {
     if (!searchKeywords || !searchKeywords.length) return handleSearchClear();
     dispatchAction(SET_VALUE, "searchKeyword", searchKeywords);
     handleSearchSubmit(searchCategory);
-  }, [handleSearchClear, handleSearchSubmit, searchCategory, searchKeywords]);
+  }, [searchKeywords]); // eslint-disable-line react-hooks/exhaustive-deps
 
   const handleLocation = (value) => {
     if (applyFilters) {
