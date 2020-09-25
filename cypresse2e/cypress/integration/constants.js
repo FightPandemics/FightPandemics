34b3bb97f1165c24f599158f4aeb35053f5eead1
galeza <galeza.agnieszka@gmail.com>
2020-09-25 12:06:51 +0200

    Frontend e2e tests update for email error messages - RecoverPassword, Login, JoinIn page (#1550)
    * Update error messages for email
    
    * Update error messages, add constants.js for joinNow spec
    
    * Update error messages, add constants.js for signIn spec

--- cypresse2e/cypress/integration/constants.js
+++ cypresse2e/cypress/integration/constants.js
@@ -1,5 +1,5 @@
 export const DUMMY_SAMPLE_EMAIL = 'qa.test@testfpfp.com';
 export const VALID_SAMPLE_EMAIL = 'samplemail.qa@gmail.com';
-export const INVALID_EMAIL_ERROR_MESSAGE = 'Invalid email';
-export const REQUIRED_EMAIL_ERROR_MESSAGE = 'Email is required.';
+export const INVALID_EMAIL_ERROR_MESSAGE = 'Email address is invalid';
+export const REQUIRED_EMAIL_ERROR_MESSAGE = 'Email address is required';
 export const LOCATION = 'New York';
