a80d9a93adf12d4155c246ddc57c82086dd914ff
galeza <galeza.agnieszka@gmail.com>
2020-09-28 14:17:25 +0200

    Add testData.js file

--- mocha/test/login.test.js
+++ mocha/test/login.test.js
@@ -1,98 +1,64 @@
 const httpStatus = require('http-status');
 const APP_URL = process.env.MOCHA_URL 
-const randomStringGenerator = require("../utils/randomStringGenerator");
 const apiHelper = require("../utils/apiHelper");
 const apiEndPointHelper = require('../utils/apiEndpoints');
 const validator = require('../utils/validators');
-
+const testData = require('../utils/testData')
 let apiEndPoint = apiEndPointHelper.loginApiEndpoint;
+let userCredentialsWithRandomEmailAndRandomPassword = testData.userCredentialsWithRandomEmailAndRandomPassword;
+let userCredentialsWithRandomEmail = testData.userCredentialsWithRandomEmail;
+let userCredentialsWithEmptyEmail = testData.userCredentialsWithEmptyEmail;
+let userCredentialsWithInvalidEmailNoDomainSpecified = testData.userCredentialsWithInvalidEmailNoDomainSpecified;
+let userCredentialsWithEmailDomainExeeding64Characters = testData.userCredentialsWithEmailDomainExeeding64Characters;
+let userCredentialsWithEmailExeeding254Characters = testData.userCredentialsWithEmailExeeding254Characters;
 
-describe('POST Login unauthorized and maximum sign in attempts', function () {
-    let userCredentials;
 
-    beforeEach(async function () {
-        userCredentials = {
-            email : generateRandomEmail(),
-            password :generateRandomPassword(),
-        }
+describe('POST Login endpoint tests for user that is NOT signed in', function () {
+
+    describe('401  - unauthorized', function () {
+
+        it('Unauthorized error - when trying to log in without signed in', async function () {
+            let response = await apiHelper.sendPOSTRequest(APP_URL, apiEndPoint, userCredentialsWithRandomEmailAndRandomPassword);
+            validator.validateStatusCodeErrorAndMessage(response, httpStatus.UNAUTHORIZED, 'Unauthorized', 'Wrong email or password.');
+        });
+    
+    });
+
+    describe('429 - maximum sign in attempts', function () {
+
+        it('Too Many Requests error - when maximum sign in attempts are exceeded', async function () {
+            let response ;
+            for (var i = 0; i < 11; i++) {
+                response = await apiHelper.sendPOSTRequest(APP_URL, apiEndPoint, userCredentialsWithRandomEmail);
+        };
+            validator.validateStatusCodeErrorAndMessage(response, httpStatus.TOO_MANY_REQUESTS, 'Too Many Requests', 'Maximum number of sign in attempts exceeded.');
+        });   
+    });
+
+    describe('Incorrect email data', function () {
+
+
+        it('Empty email triggers Bad Request error', async function () {
+            let response = await apiHelper.sendPOSTRequest(APP_URL, apiEndPoint, userCredentialsWithEmptyEmail);
+            validator.validateStatusCodeErrorAndMessage(response, httpStatus.BAD_REQUEST, 'Bad Request', 'body.email should match format "email"');
+        }); 
+        
+        it('Invalid email syntax triggers Bad Request error', async function () {
+            let response = await apiHelper.sendPOSTRequest(APP_URL, apiEndPoint, userCredentialsWithInvalidEmailNoDomainSpecified);
+            validator.validateStatusCodeErrorAndMessage(response, httpStatus.BAD_REQUEST, 'Bad Request', 'body.email should match format "email"');
+        });
+
+        it('Email local part bigger then 64 characters triggers Bad request error', async function () {
+            let response = await apiHelper.sendPOSTRequest(APP_URL, apiEndPoint, userCredentialsWithEmailDomainExeeding64Characters);
+            validator.validateStatusCodeErrorAndMessage(response, httpStatus.UNAUTHORIZED, 'Unauthorized', 'Wrong email or password.');
+        });
+
+        //following the rules that Auth0 are using the email needs to have 64max for the local part and an overall max of 254 chars
+        it('Email domain part bigger then 189 characters. Total bigger then 254 characters triggers Bad request error', async function () {
+            let response = await apiHelper.sendPOSTRequest(APP_URL, apiEndPoint, userCredentialsWithEmailExeeding254Characters);
+            validator.validateStatusCodeErrorAndMessage(response, httpStatus.BAD_REQUEST, 'Bad Request', 'body.email should match format "email"');
+        });
 
     });
 
-
-    it('Unauthorized error - when trying to log in with the wrong credentials', async function () {
-        let response = await apiHelper.sendPOSTRequest(APP_URL, apiEndPoint, userCredentials);
-        validator.validateStatusCodeErrorAndMessage(response, httpStatus.UNAUTHORIZED, 'Unauthorized', 'Wrong email or password.');
-    });
-
-    it('Too Many Requests error - when maximum sign in attempts are exceeded', async function () {
-        let response ;
-        for (var i = 0; i < 11; i++) {
-            response = await apiHelper.sendPOSTRequest(APP_URL, apiEndPoint, userCredentials);
-    };
-        validator.validateStatusCodeErrorAndMessage(response, httpStatus.TOO_MANY_REQUESTS, 'Too Many Requests', 'Maximum number of sign in attempts exceeded.');
-    });   
 });
-
-describe('POST Login with empty email', function () {
-    let userCredentials;
-
-    before(async function () {
-        userCredentials = {
-            email : '',
-            password :generateRandomPassword(),
-        }
-
-    });
-
-    it('Empty email triggers Bad Request error', async function () {
-        let response = await apiHelper.sendPOSTRequest(APP_URL, apiEndPoint, userCredentials);
-        validator.validateStatusCodeErrorAndMessage(response, httpStatus.BAD_REQUEST, 'Bad Request', 'body.email should match format "email"');
-    });  
-});
-
-describe('POST Login with empty password', function () {
-
-    let userCredentials;
-
-    before(async function () {
-        userCredentials = {
-            email : generateRandomEmail(),
-            password : '',
-        }
-
-    });
-
-    it('Empty password triggers Internal Server Error error', async function () {
-        let response = await apiHelper.sendPOSTRequest(APP_URL, apiEndPoint, userCredentials);
-        validator.validateStatusCodeErrorAndMessage(response, httpStatus.INTERNAL_SERVER_ERROR, 'Internal Server Error', 'Internal Server Error');
-    });  
-});
-
-describe('POST Login with invalid email', function () {
- 
-    let userCredentials;
-
-    before(async function () {
-        userCredentials = {
-            email : generateInvalidRandomEmail(),
-            password : generateRandomPassword(),
-        }
-
-    });
-
-    it('Invalid email triggers Internal Server Error error', async function () {
-        let response = await apiHelper.sendPOSTRequest(APP_URL, apiEndPoint, userCredentials);
-        validator.validateStatusCodeErrorAndMessage(response, httpStatus.BAD_REQUEST, 'Bad Request', 'body.email should match format "email"');
-    });
-});
-
-function generateRandomEmail(){
-    return randomStringGenerator.randomString(8) + '.' + randomStringGenerator.randomString(8) + '@'  + randomStringGenerator.randomString(5) + '.com';
-}
-
-function generateInvalidRandomEmail(){
-    return randomStringGenerator.randomString(8) + '.' + randomStringGenerator.randomString(8) + '@';
-}
-function generateRandomPassword(){
-    return randomStringGenerator.randomString(8) + '.;';
-}
