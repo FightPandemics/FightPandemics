1322ecb80ff7d6c5a319a19c9a0814a9ef1ca7ea
galeza <galeza.agnieszka@gmail.com>
2020-10-01 14:07:06 +0200

    Add validateBody method

--- mocha/utils/validators.js
+++ mocha/utils/validators.js
@@ -5,3 +5,8 @@ exports.validateStatusCodeErrorAndMessage = function (response, statusCode, erro
     expect(response.body).to.have.a.property('error').to.be.equal(error);
     expect(response.body).to.have.a.property('message').to.be.equal(message);
 };
+
+exports.validateBody = function (response, bodyProperty, body, messageProperty, message) {
+    expect(response.body).to.have.a.property('' + bodyProperty + '').to.be.equal(body);
+    expect(response.body).to.have.a.property('' + messageProperty + '').to.be.equal(message);
+};
