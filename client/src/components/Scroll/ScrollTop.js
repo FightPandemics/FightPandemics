d417a6ac44e2f013a2e4061e01db08b39785b09b
sujatha <jhanavi.bhushan@gmail.com>
2020-10-02 12:45:52 -0400

    Fixed issues with :- 1) Refresh the current page 2) Switch to the any new page (e.g. All feeds, Request, Offer, About us, FAQ, Privacy policy, etc.)

---
+++ client/src/components/Scroll/ScrollTop.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+import { withRouter, useLocation } from "react-router-dom";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  const [flag, setFlag] = useState(true);
+
+  window.onbeforeunload = function () {
+    setFlag(false);
+  };
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname, !flag]);
+
+  return null;
+}
+
+export default withRouter(ScrollToTop);
