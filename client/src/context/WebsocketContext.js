2ad612a562c0b2c1396f88a65b00ff74d61f8702
Zaydme <zaydme8@gmail.com>
2020-10-04 17:33:36 +0100

    change socket url to work in review env

--- client/src/context/WebsocketContext.js
+++ client/src/context/WebsocketContext.js
@@ -16,7 +16,7 @@ import {
 
 const WebSocketContext = createContext();
 
-export { WebSocketContext }
+export { WebSocketContext };
 
 export default class SocketManager extends React.Component {
   socket = null;
@@ -24,63 +24,61 @@ export default class SocketManager extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        user: null
-    }
+      user: null,
+    };
     props.store.subscribe(() => {
-        const newState = props.store.getState()
-        if (this.state.user != newState.session.user){
-            this.state.user = newState.session.user
-            if (newState.session.isAuthenticated) this.identify(this.state.user);
-            else this.socket.disconnect();
-        }
+      const newState = props.store.getState();
+      if (this.state.user != newState.session.user) {
+        this.state.user = newState.session.user;
+        if (newState.session.isAuthenticated) this.identify(this.state.user);
+        else this.socket.disconnect();
+      }
     });
-    this.socket = io.connect('localhost:8000');
+    // for local testing, io.connect('localhost:8000')
+    this.socket = io.connect();
     this.socket.on("connect", () => {
-        this.socket.connected = true;
-        console.log(`[WS]: Connected`);
-        if (this.state.user) this.identify(this.state.user)
+      this.socket.connected = true;
+      console.log(`[WS]: Connected`);
+      if (this.state.user) this.identify(this.state.user);
     });
 
     this.socket.on("disconnect", () => {
-        this.socket.connected = false;
-        console.log(`[WS]: Disconnected`);
-        console.log(`[WS}]: Trying to reconnect!`);
-        let recon = setInterval(() => {
-            if (this.socket.connected) clearInterval(recon);
-            this.socket.connect();
-        }, 500);
+      this.socket.connected = false;
+      console.log(`[WS]: Disconnected`);
+      console.log(`[WS}]: Trying to reconnect!`);
+      let recon = setInterval(() => {
+        if (this.socket.connected) clearInterval(recon);
+        this.socket.connect();
+      }, 500);
     });
 
     this.socket.on("NEW_MESSAGE", (messageData) => {
-        this.props.store.dispatch(receivedMessage(messageData));
+      this.props.store.dispatch(receivedMessage(messageData));
     });
 
     this.socket.on("USER_STATUS_UPDATE", (data) => {
-      if (!this.state.user || data.id == this.state.user.id) return
+      if (!this.state.user || data.id == this.state.user.id) return;
       this.props.store.dispatch(userStatusUpdate(data));
-    })
-
+    });
   }
 
   identify = (userData) => {
-    if (!this.socket || !this.socket) return
+    if (!this.socket || !this.socket) return;
     this.socket.emit("IDENTIFY", userData, (response) => {
-        if (response.code == 200) {
-            console.log(`[WS]: ${userData.id} Identified`);
-            this.props.store.dispatch(identifySuccess());
-        }
-        else this.props.store.dispatch(identifyError(response));
+      if (response.code == 200) {
+        console.log(`[WS]: ${userData.id} Identified`);
+        this.props.store.dispatch(identifySuccess());
+      } else this.props.store.dispatch(identifyError(response));
     });
   };
 
   sendMessage = async (messageData) => {
     return new Promise((resolve) => {
       this.socket.emit("SEND_MESSAGE", messageData, (response) => {
-        if (response.code == 200) return resolve(true)
-        resolve(false)
+        if (response.code == 200) return resolve(true);
+        resolve(false);
       });
-    })
-
+    });
   };
 
   joinRoom = (data) => {
@@ -88,28 +86,29 @@ export default class SocketManager extends React.Component {
       this.socket.emit("JOIN_ROOM", data, (response) => {
         if (response.code == 200) {
           this.props.store.dispatch(joinRoomSuccess(response.data));
-          return resolve(response.data)
-        }
-        else this.props.store.dispatch(joinRoomError());
-        resolve(false)
+          return resolve(response.data);
+        } else this.props.store.dispatch(joinRoomError());
+        resolve(false);
       });
-    })
+    });
   };
 
   leaveAllRooms = () => {
-    this.joinRoom({})
-  }
+    this.joinRoom({});
+  };
 
   getUserRooms = () => {
     this.socket.emit("GET_USER_THREADS", null, (response) => {
-      if (response.code == 200) this.props.store.dispatch(getRoomsSuccess(response.data));
+      if (response.code == 200)
+        this.props.store.dispatch(getRoomsSuccess(response.data));
       else this.props.store.dispatch(getRoomsError());
     });
   };
 
   getChatLog = (data) => {
     this.socket.emit("GET_CHAT_LOG", data, (response) => {
-      if (response.code == 200) this.props.store.dispatch(getChatLogSuccess(response.data));
+      if (response.code == 200)
+        this.props.store.dispatch(getChatLogSuccess(response.data));
       else this.props.store.dispatch(getChatLogError());
     });
   };
@@ -119,26 +118,25 @@ export default class SocketManager extends React.Component {
       this.socket.emit("GET_CHAT_LOG_MORE", data, (response) => {
         if (response.code == 200) {
           this.props.store.dispatch(loadMoreSuccess(response.data));
-          return resolve(true)
+          return resolve(true);
         }
-        resolve(false)
+        resolve(false);
       });
-    })
-
+    });
   };
 
   getUserStatus = (userId) => {
     return new Promise((resolve) => {
       this.socket.emit("GET_USER_STATUS", userId, (response) => {
-        if (response.code == 200) return resolve(response.data) // online/offline
-        resolve(false)
+        if (response.code == 200) return resolve(response.data); // online/offline
+        resolve(false);
       });
-    })
+    });
   };
 
   messageSeen = () => {
     // later
-  }
+  };
 
   componentWillUnmount() {
     try {
@@ -150,15 +148,15 @@ export default class SocketManager extends React.Component {
 
   render() {
     const value = {
-        identify: this.identify,
-        sendMessage: this.sendMessage,
-        joinRoom: this.joinRoom,
-        getChatLog: this.getChatLog,
-        loadMore: this.loadMore,
-        getUserRooms: this.getUserRooms,
-        getUserStatus: this.getUserStatus,
-        leaveAllRooms: this.leaveAllRooms,
-    }
+      identify: this.identify,
+      sendMessage: this.sendMessage,
+      joinRoom: this.joinRoom,
+      getChatLog: this.getChatLog,
+      loadMore: this.loadMore,
+      getUserRooms: this.getUserRooms,
+      getUserStatus: this.getUserStatus,
+      leaveAllRooms: this.leaveAllRooms,
+    };
     return (
       <WebSocketContext.Provider value={value}>
         {this.props.children}
@@ -166,4 +164,3 @@ export default class SocketManager extends React.Component {
     );
   }
 }
-
