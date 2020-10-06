0d3f111c393b8b0f760ec9a98a72a14400df343f
galeza <galeza.agnieszka@gmail.com>
2020-10-06 15:12:23 +0200

    Add test for empty help board

--- cypresse2e/cypress/integration/e2eTests/helpBoard/helpBoard.spec.js
+++ cypresse2e/cypress/integration/e2eTests/helpBoard/helpBoard.spec.js
@@ -4,6 +4,7 @@ describe('FightPandemics Help Board Page for unauthorized user', () => {
 
     const helpBoard = new HelpBoard();
     var h1Heading = "Help Board";
+    var noPostsAvailableText = "Sorry, there are currently no relevant posts available. Please try using a different filter search or";
 
     context('User opens Help Board', () => {
         beforeEach(() => {
@@ -28,5 +29,21 @@ describe('FightPandemics Help Board Page for unauthorized user', () => {
               })
         });
 
+        it('Unauthorized user sees a message that posts are not available', () => {     
+            cy.get("body").then($body => {
+                if ($body.find(helpBoard.addPostLink).length > 0) {   
+                    helpBoard.getNoPostsAvailableTextElement().contains(noPostsAvailableText);
+                    var addPostLink = helpBoard.getaddPostLinkElement();
+                    addPostLink.click({ force: true });
+                    cy.on("url:changed", (newUrl) => {
+                        expect(newUrl).to.contain("auth/login")
+                      })   
+                }
+            });     
+           
+           
+          });
+
     });
+
 });
