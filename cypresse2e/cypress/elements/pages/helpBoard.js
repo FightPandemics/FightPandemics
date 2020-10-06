3a7ffb93ad50002cccb044e2dbab0a13fd052a2b
galeza <galeza.agnieszka@gmail.com>
2020-10-06 15:11:56 +0200

    Add elements to helpBoard POM

--- cypresse2e/cypress/elements/pages/helpBoard.js
+++ cypresse2e/cypress/elements/pages/helpBoard.js
@@ -3,7 +3,8 @@ class HelpBoard{
     fpLogo = '.am-navbar.am-navbar-light a img';
     helpBoardpageTitle = 'h1';
     addPostButton = 'button > img#HB_CP'
-
+    noPostsAvailableTextElement = '//*/a[@id="HB_CP"]//parent::div';
+    addPostLink = 'a#HB_CP';
 
 
     constructor() { }
@@ -24,6 +25,21 @@ class HelpBoard{
         return cy.get(this.addPostButton);
     }
 
+    getNoPostsAvailableDiv(){
+        return this.noPostsAvailableTextElement;
+    }
+
+    getaddPostLink(){
+        return this.addPostLink;
+    }
+
+    getaddPostLinkElement(){
+        return cy.get(this.addPostLink);
+    }
+    
+    getNoPostsAvailableTextElement(){
+        return cy.xpath(this.noPostsAvailableTextElement);
+    }
 }
 
 export default HelpBoard;
