80a8b9d5b0d53b5f0318b969b7117c11b1bf236f
Zaydme <zaydme8@gmail.com>
2020-10-07 12:41:45 +0100

    fixed mistake

--- client/src/components/Inbox/ChatList.js
+++ client/src/components/Inbox/ChatList.js
@@ -79,7 +79,7 @@ export const ChatList = ({
           <header>
             <UserName>{getReceiver(_room.participants).name}</UserName>
             <h5>{_room.lastMessage? getRelativeTime(_room.lastMessage.createdAt) : getRelativeTime(_room.createdAt)}</h5>
-            {getSender(_room.participants).newMessages && <span className="unread-indicator">{getSender(_room.participants).newMessages}</span>}
+            {getSender(_room.participants).newMessages != 0 && <span className="unread-indicator">{getSender(_room.participants).newMessages}</span>}
           </header>
           <div className="content">
             {<div className="title">Topic....</div>}
