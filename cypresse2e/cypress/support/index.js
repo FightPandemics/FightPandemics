0d45ec2167bac4bc8afaec5e39530baf3e4174fd
jessica-lau <jessica.lau2@gmail.com>
2020-10-12 19:36:54 -0400

    revert changes in requestHelp spec and index.js

--- cypresse2e/cypress/support/index.js
+++ cypresse2e/cypress/support/index.js
@@ -21,7 +21,7 @@ import './commands'
 const resizeObserverLoopErrRe = /^ResizeObserver loop limit exceeded/
 Cypress.on('uncaught:exception', (err) => {
   if (resizeObserverLoopErrRe.test(err.message)) {
-    // returning false here prevents Cypress from	
+    // returning false here prevents Cypress from
     // failing the test
     return false
   }
