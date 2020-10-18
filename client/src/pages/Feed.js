a8ed02694be2c5b1862d34585b2cb44f62e61ce8
joshmorel <morel.josh@hotmail.com>
2020-10-18 11:27:36 -0400

    apply formatting standard

--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -542,7 +542,7 @@ const Feed = (props) => {
           type: ADD_OPTION,
           payload: { option: option.value, label },
         });
-        props.history.replace("/feed")
+        props.history.replace("/feed");
       };
 
       const {
