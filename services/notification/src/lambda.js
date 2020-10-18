778e78b071642e89cf4a92263ebf82f4f1857b55
Manny Karyampudi <mannykary@users.noreply.github.com>
2020-10-17 23:07:59 -0700

    Update Lambda handler and service

--- services/notification/src/lambda.js
+++ services/notification/src/lambda.js
@@ -1,12 +1,23 @@
 const { config } = require("./config");
 const { NotificationService } = require("./service");
 
+let cachedDb = null;
+
+function setCachedDb(db) {
+  if (cachedDb === null) {
+    cachedDb = db;
+  }
+}
+
 exports.handler = async (event, context) => {
   try {
+    context.callbackWaitsForEmptyEventLoop = false;
     // TODO if processing daily, weekly, bi-weekly digests, may need multiple cron jobs. Probably should pass in
     // frequency into the event payload
-    // const event = event.frequency;
+    // const frequency = event.frequency;
     // const service = new NotificationService(config);
+    // service.initializeDb(cachedDb);
+    // setCachedDb(service.dbHelper.db);
     // await service.process(frequency);
     return {
       body: JSON.stringify("Hello world!"),
