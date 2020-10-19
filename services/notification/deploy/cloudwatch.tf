79b9a484faa0730e753ec3df34e43b119a0ba017
Manny Karyampudi <mannykary@users.noreply.github.com>
2020-10-18 18:27:00 -0700

    Update Cloudwatch rule to pass in frequency

--- services/notification/deploy/cloudwatch.tf
+++ services/notification/deploy/cloudwatch.tf
@@ -1,19 +1,24 @@
-resource "aws_cloudwatch_event_rule" "cron" {
-  name                = "notification-service-rule"
-  description         = "notification-service rule"
+resource "aws_cloudwatch_event_rule" "instant" {
+  name                = "notification-service-instant-rule"
+  description         = "notification-service instant rule"
   schedule_expression = "rate(5 minutes)"
 }
 
-resource "aws_cloudwatch_event_target" "notification_lambda" {
-  rule      = aws_cloudwatch_event_rule.cron.name
-  target_id = "notificaiton-service-rule"
+resource "aws_cloudwatch_event_target" "instant_target" {
+  rule      = aws_cloudwatch_event_rule.instant.name
+  target_id = aws_cloudwatch_event_rule.instant.name
   arn       = aws_lambda_function.lambda.arn
+  input = jsonencode({
+    frequency = "instant"
+  })
 }
 
-resource "aws_lambda_permission" "lambda_invoke_permission" {
-  statement_id  = "AllowExecutionFromCloudWatch-notification-service-rule"
+resource "aws_lambda_permission" "instant" {
+  statement_id  = "AllowExecutionFromCloudWatch-notification-service-instant-rule"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.lambda.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.cron.arn
+  source_arn    = aws_cloudwatch_event_rule.instant.arn
 }
+
+// TODO Add daily, weekly, biweekly Cloudwatch rules
