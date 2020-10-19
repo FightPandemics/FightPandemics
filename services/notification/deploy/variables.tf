7e93c2de36bbbb5a51b21e9a19cf2cfce76621d2
Manny Karyampudi <mannykary@users.noreply.github.com>
2020-10-18 18:29:48 -0700

    Add more Lambda environment variables

--- services/notification/deploy/variables.tf
+++ services/notification/deploy/variables.tf
@@ -9,3 +9,15 @@ data "aws_ssm_parameter" "aws_ses_secret_access_key" {
 data "aws_ssm_parameter" "aws_ses_region" {
   name = "/fp/aws_ses/region"
 }
+
+data "aws_ssm_parameter" "database_host" {
+  name = "/fp/database/host"
+}
+
+data "aws_ssm_parameter" "database_password" {
+  name = "/fp/database/password"
+}
+
+data "aws_ssm_parameter" "database_user" {
+  name = "/fp/database/user"
+}
