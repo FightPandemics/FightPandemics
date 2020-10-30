ba062d0394071de7f4258475d8e09431e0776316
galeza <galeza.agnieszka@gmail.com>
2020-10-30 14:48:40 +0100

    Prettier - Format support files

--- cypresse2e/cypress/support/index.js
+++ cypresse2e/cypress/support/index.js
@@ -20,8 +20,8 @@ require('cypress-xpath')
 // require('./commands')
 const resizeObserverLoopErrRe = /^[^(ResizeObserver loop limit exceeded)]/
 Cypress.on('uncaught:exception', (err) => {
-    /* returning false here prevents Cypress from failing the test */
-    if (resizeObserverLoopErrRe.test(err.message)) {
-        return false
-    }
+  /* returning false here prevents Cypress from failing the test */
+  if (resizeObserverLoopErrRe.test(err.message)) {
+    return false
+  }
 })
