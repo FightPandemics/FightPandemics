9f7d12a636d222da757a489304bd9dd282c54aab
Manny Karyampudi <mannykary@users.noreply.github.com>
2020-11-01 19:08:05 -0800

    TemplateBuilder: build digest email templates

--- services/notification/src/helpers/template-builder.js
+++ services/notification/src/helpers/template-builder.js
@@ -1,3 +1,4 @@
+const { EmailFrequency } = require("../models/email-frequency");
 const fs = require("fs");
 const jwt = require("jsonwebtoken");
 const Mustache = require("mustache");
@@ -12,6 +13,16 @@ class TemplateBuilder {
       base: {
         html: this._loadTemplateFile("../templates/base.html"),
         text: this._loadTemplateFile("../templates/base.txt"),
+      },
+      biweekly: {
+        html: this._loadTemplateFile("../templates/digests/biweekly.html"),
+        subject: "Your FightPandemics activity over the past two weeks",
+        text: this._loadTemplateFile("../templates/digests/biweekly.txt"),
+      },
+      daily: {
+        html: this._loadTemplateFile("../templates/digests/daily.html"),
+        subject: "Your FightPandemics activity over the past day",
+        text: this._loadTemplateFile("../templates/digests/daily.txt"),
       },
       instant: {
         comment: {
@@ -31,24 +42,87 @@ class TemplateBuilder {
           text: this._loadTemplateFile("../templates/instant/share.txt"),
         },
       },
+      post: {
+        html: this._loadTemplateFile("../templates/digests/_post.html"),
+        text: this._loadTemplateFile("../templates/digests/_post.txt"),
+      },
+      weekly: {
+        html: this._loadTemplateFile("../templates/digests/weekly.html"),
+        subject: "Your FightPandemics activity over the past week",
+        text: this._loadTemplateFile("../templates/digests/weekly.txt"),
+      },
     };
   }
 
   build(frequency, notifications) {
-    // TODO need to come up with algorithm to go through user's notifications, and then come up with a summary email
-    // that groups the notifications
-    // For now just handle instant
-    return this._buildInstant(notifications);
+    if (frequency === EmailFrequency.INSTANT) {
+      return this._buildInstant(notifications);
+    }
+    return this._buildDigest(frequency, notifications);
+  }
+
+  _buildDigest(frequency, notifications) {
+    return notifications
+      .map((notification) => {
+        const notifyPrefs = notification.receiver.notifyPrefs;
+        const posts = notification.posts;
+        if (notifyPrefs && !notifyPrefs.digest[frequency]) {
+          return;
+        }
+        const token = this._generateToken(notification.receiver._id);
+        const htmlTemplate = Mustache.render(this.templates.base.html, {
+          baseUrl: this.baseUrl,
+          body: this.templates[frequency].html,
+          token,
+        });
+        const textTemplate = Mustache.render(this.templates.base.text, {
+          baseUrl: this.baseUrl,
+          body: this.templates[frequency].text,
+          token,
+        });
+        const subject = this.templates[frequency].subject;
+
+        const postHtmlTemplates = posts.map((post) =>
+          Mustache.render(this.templates.post.html, {
+            baseUrl: this.baseUrl,
+            commentText: post.latest ? post.latest.commentText : "",
+            post: post.post,
+            actionCounts: this._generatePostCountsCopy(post.counts),
+          }),
+        );
+        const postTextTemplates = posts.map((post) =>
+          Mustache.render(this.templates.post.text, {
+            baseUrl: this.baseUrl,
+            commentText: post.latest ? post.latest.commentText : "",
+            post: post.post,
+            actionCounts: this._generatePostCountsCopy(post.counts),
+          }),
+        );
+
+        const htmlView = {
+          posts: postHtmlTemplates.join(" "),
+        };
+        const textView = {
+          posts: postTextTemplates.join("<br>"),
+        };
+
+        return {
+          htmlBody: Mustache.render(htmlTemplate, htmlView),
+          notificationId: null,
+          subject,
+          textBody: Mustache.render(textTemplate, textView),
+          toEmailAddress: notification.receiver.email,
+        };
+      })
+      .filter((email) => !!email);
   }
 
   _buildInstant(notifications) {
     return notifications
       .map((notification) => {
         const action = notification.action;
-        if (
-          notification.receiver.notifyPrefs &&
-          !notification.receiver.notifyPrefs[action].instant
-        ) {
+        const notifyPrefs = notification.receiver.notifyPrefs;
+        if (notifyPrefs && !notifyPrefs.instant[action]) {
           return;
         }
         const token = this._generateToken(notification.receiver._id);
@@ -99,6 +173,23 @@ class TemplateBuilder {
   _generateToken(userId) {
     const token = jwt.sign({ userId }, this.tokenKey, { expiresIn: "30d" });
     return token;
+  }
+
+  // TODO localize
+  _generatePostCountsCopy(counts) {
+    const countCopies = [];
+    // We show the first comment preview, so we only need to display
+    // the comment count if there are additional comments on the post.
+    if (counts.comment > 1) {
+      countCopies.push(`${counts.comment - 1} more comment(s)`);
+    }
+    if (counts.like > 0) {
+      countCopies.push(`${counts.like} like(s)`);
+    }
+    if (counts.share > 0) {
+      countCopies.push(`${counts.share} share(s)`);
+    }
+    return countCopies.join(", ");
   }
 }
 
