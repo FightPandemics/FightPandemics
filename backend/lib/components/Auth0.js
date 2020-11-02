50167bc8abfaf7cd82832704466fd2703b9b56b2
ddveloper <44758313+ddveloper@users.noreply.github.com>
2020-11-02 10:44:48 -0800

    we don't need to merge metadata, nor unlink

--- backend/lib/components/Auth0.js
+++ backend/lib/components/Auth0.js
@@ -106,31 +106,7 @@ const changePassword = async (token, email) => {
   }
 };
 
-const _getUserDetails = async (token, userId) => {
-  try {
-    const res = await axios.request(
-      `${AUTH_DOMAIN}/api/v2/users/${userId}`,
-      getAuthHeaders(token),
-    );
-    return res.data;
-  } catch (err) {
-    return wrapError(err);
-  }
-};
-
-const _updateUser = async (token, userId, payload) => {
-  try {
-    const res = await axios.request(`${AUTH_DOMAIN}/api/v2/users/${userId}`, {
-      json: payload,
-      ...getAuthHeaders(token),
-    });
-    return res.data;
-  } catch (err) {
-    return wrapError(err);
-  }
-};
-
-const _linkAccounts = async (token, rootUserId, targetUserId) => {
+const linkAccounts = async (token, rootUserId, targetUserId) => {
   const parts = targetUserId.split("|"); // "provider|user_id"
   const payload = {
     provider: parts[0],
@@ -140,56 +116,6 @@ const _linkAccounts = async (token, rootUserId, targetUserId) => {
     const res = await axios.post(
       `${AUTH_DOMAIN}/api/v2/users/${rootUserId}/identities`,
       payload,
-      getAuthHeaders(token),
-    );
-    return res.data;
-  } catch (err) {
-    return wrapError(err);
-  }
-};
-
-const _mergeMetadata = async (token, primaryUserId, secondaryUserId) => {
-  // load both users with metedata.
-  const primaryUser = await _getUserDetails(token, primaryUserId);
-  const secondaryUser = await _getUserDetails(token, secondaryUserId);
-
-  const customizerCallback = function (objectValue, sourceValue) {
-    if (_.isArray(objectValue)) {
-      return sourceValue.concat(objectValue);
-    }
-  };
-  const mergedUserMetadata = _.merge(
-    {},
-    secondaryUser.user_metadata,
-    primaryUser.user_metadata,
-    customizerCallback,
-  );
-  const mergedAppMetadata = _.merge(
-    {},
-    secondaryUser.app_metadata,
-    primaryUser.app_metadata,
-    customizerCallback,
-  );
-  await _updateUser(token, primaryUserId, {
-    user_metadata: mergedUserMetadata,
-    app_metadata: mergedAppMetadata,
-  });
-};
-
-const linkAccounts = async (token, primaryUserId, secondaryUserId) => {
-  try {
-    await _mergeMetadata(token, primaryUserId, secondaryUserId);
-    await _linkAccounts(token, primaryUserId, secondaryUserId);
-  } catch (err) {
-    return wrapError(err);
-  }
-};
-
-const unlinkAccounts = async (token, primaryUserId, secondaryUserId) => {
-  const parts = secondaryUserId.split("|"); // "provider|user_id"
-  try {
-    const res = await axios.delete(
-      `${AUTH_DOMAIN}/api/v2/users/${primaryUserId}/identities/${parts[0]}/${parts[1]}`,
       getAuthHeaders(token),
     );
     return res.data;
@@ -221,5 +147,4 @@ module.exports = {
   getUser,
   getAccountsWithSameEmail,
   linkAccounts,
-  unlinkAccounts,
 };
