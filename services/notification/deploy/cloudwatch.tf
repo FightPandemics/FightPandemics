39f7001ebc332b36373a197b89f5773681d34517
Manny Karyampudi <mannykary@users.noreply.github.com>
2020-11-07 16:21:04 -0800

    Add daily/weekly/biweekly cron jobs

--- services/notification/deploy/cloudwatch.tf
+++ services/notification/deploy/cloudwatch.tf
@@ -1,24 +1,37 @@
-resource "aws_cloudwatch_event_rule" "instant" {
-  name                = "notification-service-instant-rule"
-  description         = "notification-service instant rule"
-  schedule_expression = "rate(5 minutes)"
+locals {
+  rules = {
+    instant  = "2/5 * ? * * *" # Every 5th minute of the hour, starting from minute 2
+    daily    = "0 2 ? * * *"   # Every day at 2am UTC
+    weekly   = "0 3 ? * MON *" # Every week on Monday at 3am UTC
+    biweekly = "rate(14 days)" # Every two weeks
+  }
 }
 
-resource "aws_cloudwatch_event_target" "instant_target" {
-  rule      = aws_cloudwatch_event_rule.instant.name
-  target_id = aws_cloudwatch_event_rule.instant.name
+resource "aws_cloudwatch_event_rule" "notification" {
+  for_each = local.rules
+
+  name                = "notification-service-${each.key}-rule"
+  description         = "notification-service ${each.key} rule"
+  schedule_expression = each.value
+}
+
+resource "aws_cloudwatch_event_target" "target" {
+  for_each = local.rules
+
+  rule      = aws_cloudwatch_event_rule.notification[each.key].name
+  target_id = aws_cloudwatch_event_rule.notification[each.key].name
   arn       = aws_lambda_function.lambda.arn
   input = jsonencode({
-    frequency = "instant"
+    frequency = each.key
   })
 }
 
-resource "aws_lambda_permission" "instant" {
-  statement_id  = "AllowExecutionFromCloudWatch-notification-service-instant-rule"
+resource "aws_lambda_permission" "permission" {
+  for_each = local.rules
+
+  statement_id  = "AllowExecutionFromCloudWatch-notification-service-${each.key}-rule"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.lambda.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.instant.arn
+  source_arn    = aws_cloudwatch_event_rule.notification[each.key].arn
 }
-
-// TODO Add daily, weekly, biweekly Cloudwatch rules
