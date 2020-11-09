be28c7425e6c44663fc733a8116f56311a7fd0ee
Manny Karyampudi <mannykary@users.noreply.github.com>
2020-11-08 19:19:13 -0800

    DatabaseHelper: find unread direct messages

--- services/notification/src/helpers/database-helper.js
+++ services/notification/src/helpers/database-helper.js
@@ -1,5 +1,6 @@
 const { DateHelper } = require("./date-helper");
 const { EmailFrequency } = require("../models/email-frequency");
+const { MessageThreadStatus } = require("../models/message-thread-status");
 const { MongoClient } = require("mongodb");
 const { NotificationAction } = require("../models/notification-action");
 
@@ -25,6 +26,103 @@ class DatabaseHelper {
       return this._findInstantNotifications();
     }
     return this._findDigestNotifications(frequency);
+  }
+
+  async findUnreadDirectMessages() {
+    const threads = await this.db
+      .collection("threads")
+      .aggregate([
+        {
+          $match: {
+            "participants.newMessages": { $gt: 0 },
+            "participants.lastAccess": {
+              $lt: DateHelper.subtractMinutes(
+                new Date(),
+                this.instantUnreadLookbackInterval,
+              ),
+              // Don't send out instant notifications for anything older than 30 minutes
+              // TODO uncomment after done dev testing
+              // $gt: DateHelper.subtractMinutes(new Date(), 30),
+            },
+            "participants.status": {
+              $in: [MessageThreadStatus.ACCEPTED, MessageThreadStatus.PENDING],
+            },
+          },
+        },
+        {
+          $lookup: {
+            from: "users",
+            localField: "participants.id",
+            foreignField: "_id",
+            as: "users",
+          },
+        },
+      ])
+      .toArray();
+
+    const threadIds = threads.map((thread) => thread._id);
+
+    // Convert threads array to object keyed by thread ID for O(1) lookup
+    // See https://www.olioapps.com/blog/map-reduce/ for indexing an array of objects
+    const threadsObject = threads.reduce(
+      (accumulator, thread) => ({ ...accumulator, [thread._id]: thread }),
+      {},
+    );
+
+    const messagesCursor = this.db.collection("messages").aggregate([
+      {
+        $match: {
+          threadId: { $in: threadIds },
+          // emailSentAt: null, // TODO need to add this field to messages collection
+        },
+      },
+      { $sort: { createdAt: 1 } },
+      {
+        $group: {
+          _id: "$threadId",
+          latestMessage: {
+            $last: "$$ROOT",
+          },
+        },
+      },
+    ]);
+
+    const messages = [];
+
+    while (await messagesCursor.hasNext()) {
+      const message = await messagesCursor.next();
+      const thread = threadsObject[message._id];
+      /*
+        The thread.participants array will always have only two elements in it
+        (at least for direct messages). One of these participants is a sender
+        while the other is a receiver. The newMessages count for participants
+        is mutually exclusive in that only one of the participants will have
+        this count greater than 0, while the other will have this count equal
+        to 0. Therefore we can assume that the sender is the participant with
+        newMessages count equal to 0, while the receiver is the participant
+        with newMessages greater than 0.
+      */
+      const sender = thread.participants.find((p) => p.newMessages === 0);
+      const receiver = thread.participants.find((p) => p.newMessages > 0);
+      if (sender === undefined || receiver === undefined) {
+        continue;
+      }
+      const receiverUser = thread.users.find(
+        (user) => user._id.toString() === receiver.id.toString(),
+      );
+      const { notifyPrefs, email } = receiverUser;
+      if (notifyPrefs && !notifyPrefs.instant.message) {
+        // Receiver has disabled instant notifications for direct messages
+        continue;
+      }
+      messages.push({
+        sender: { ...sender },
+        receiver: { ...receiver, email },
+        message: { ...message.latestMessage },
+      });
+    }
+
+    return messages;
   }
 
   async setEmailSentAt(notificationIds, frequency) {
