f558ba87afd06fb9e0fc6f1788b7da7ebf539934
ddveloper <44758313+ddveloper@users.noreply.github.com>
2020-11-11 14:04:18 -0800

    lost Notification link by merge, add it back

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -532,7 +532,7 @@ const Profile = ({
             closable={false}
             onClose={onToggleDrawer}
             visible={drawer}
-            height="150px"
+            height="200px"
             key="bottom"
           >
             <DrawerHeader>
@@ -541,6 +541,11 @@ const Profile = ({
             <DrawerHeader>
               <Link to="/edit-profile">
                 {t("profile.individual.editProfile")}{" "}
+              </Link>
+            </DrawerHeader>
+            <DrawerHeader>
+              <Link to="/edit-notifications">
+                {t("profile.individual.editNotification")}{" "}
               </Link>
             </DrawerHeader>
           </CustomDrawer>
