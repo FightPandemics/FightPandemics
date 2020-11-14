0bfa2bc06f9f822663972d36024331f65fffee2e
Zayd <zaydme8@gmail.com>
2020-11-14 19:50:10 +0100

    re: fixed companies LinkedIn URL

--- client/src/constants/urls.js
+++ client/src/constants/urls.js
@@ -1,6 +1,6 @@
 export const FACEBOOK_URL = "https://www.facebook.com/";
 export const INSTAGRAM_URL = "https://www.instagram.com/";
-export const LINKEDIN_URL = "https://www.linkedin.com/in/company/";
+export const LINKEDIN_URL = "https://www.linkedin.com/company/";
 export const LINKEDIN_INDIVIDUAL_URL = `${LINKEDIN_URL}in/`;
 export const TWITTER_URL = "https://twitter.com/";
 export const GITHUB_URL = "https://github.com/";
