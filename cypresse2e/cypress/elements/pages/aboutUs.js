21938c62f549e14006071006fa51b13f1409cc02
TimeaKovacs86 <kovacs.timea.86@gmail.com>
2020-11-16 11:43:54 +0000

    Applied .prettierrc

--- cypresse2e/cypress/elements/pages/aboutUs.js
+++ cypresse2e/cypress/elements/pages/aboutUs.js
@@ -1,30 +1,30 @@
 class AboutUs {
-  linkedInButton = '#AU_LN'
-  facebookButton = '#AU_FB'
-  instagramButton = '#AU_IG'
-  twitterButton = '#AU_TW'
-  linkedInLink = 'https://www.linkedin.com/company/fightpandemics/'
-  facebookLink = 'https://www.facebook.com/FightPandemics/'
-  instagramLink = 'https://www.instagram.com/fightpandemics/'
-  twitterLink = 'https://twitter.com/FightPandemics'
-  getInvolvedCloseButton = '.ant-modal-close'
-  getInvolvedButton = '#AU_GI'
-  joinAsVolunteerButton = '#AU_GI_JV'
-  joinAsVolunteerLink = 'https://apply.workable.com/fightpandemics'
-  joinAsAmbassadorButton = '#AU_GI_JA'
-  joinAsAmbassadorLink = 'https://apply.workable.com/fightpandemics/j/46D6EF3B44/'
-  joinStudentProgramButton = '#AU_GI_JSP'
-  joinStudentProgramLink = 'https://apply.workable.com/fightpandemics/j/58B157AAB2/'
-  getToHelpBoardButton = '#AU_VHB'
-  goToHelpBoardLink = '/feed'
-  partnershipEmail = 'partnerships@fightpandemics.com'
-  contactEmail = 'contact@fightpandemics.com'
-  partnershipEmailHref = 'mailto:partnerships@fightpandemics.com'
-  contactEmailHref = 'mailto:admin@fightpandemics.com'
+  linkedInButton = "#AU_LN";
+  facebookButton = "#AU_FB";
+  instagramButton = "#AU_IG";
+  twitterButton = "#AU_TW";
+  linkedInLink = "https://www.linkedin.com/company/fightpandemics/";
+  facebookLink = "https://www.facebook.com/FightPandemics/";
+  instagramLink = "https://www.instagram.com/fightpandemics/";
+  twitterLink = "https://twitter.com/FightPandemics";
+  getInvolvedCloseButton = ".ant-modal-close";
+  getInvolvedButton = "#AU_GI";
+  joinAsVolunteerButton = "#AU_GI_JV";
+  joinAsVolunteerLink = "https://apply.workable.com/fightpandemics";
+  joinAsAmbassadorButton = "#AU_GI_JA";
+  joinAsAmbassadorLink = "https://apply.workable.com/fightpandemics/j/46D6EF3B44/";
+  joinStudentProgramButton = "#AU_GI_JSP";
+  joinStudentProgramLink = "https://apply.workable.com/fightpandemics/j/58B157AAB2/";
+  getToHelpBoardButton = "#AU_VHB";
+  goToHelpBoardLink = "/feed";
+  partnershipEmail = "partnerships@fightpandemics.com";
+  contactEmail = "contact@fightpandemics.com";
+  partnershipEmailHref = "mailto:partnerships@fightpandemics.com";
+  contactEmailHref = "mailto:admin@fightpandemics.com";
 
   constructor() {
   }
 
 }
 
-export default AboutUs
+export default AboutUs;
--- cypresse2e/cypress/integration/e2eTests/aboutUs/aboutUs.spec.js
+++ cypresse2e/cypress/integration/e2eTests/aboutUs/aboutUs.spec.js
@@ -1,94 +1,94 @@
-import AboutUs from '../../../elements/pages/aboutUs'
+import AboutUs from "../../../elements/pages/aboutUs";
 
 
-describe('FightPandemics About Us page', () => {
-  const aboutUs = new AboutUs()
+describe("FightPandemics About Us page", () => {
+  const aboutUs = new AboutUs();
 
-  context('User opens About Us Page', () => {
+  context("User opens About Us Page", () => {
     beforeEach(() => {
-      cy.visit('/about-us')
-    })
+      cy.visit("/about-us");
+    });
 
-    it('Check if the social media buttons are visible and get us to the appropriate link', () => {
-      linkedInButtonIsVisibleAndPointToAppropriateLink()
-      facebookButtonIsVisibleAndPointToAppropriateLink()
-      instagramButtonIsVisibleAndPointToAppropriateLink()
-      twitterButtonIsVisibleAndPointToAppropriateLink()
-    })
+    it("Check if the social media buttons are visible and get us to the appropriate link", () => {
+      linkedInButtonIsVisibleAndPointToAppropriateLink();
+      facebookButtonIsVisibleAndPointToAppropriateLink();
+      instagramButtonIsVisibleAndPointToAppropriateLink();
+      twitterButtonIsVisibleAndPointToAppropriateLink();
+    });
 
-    it('Check if close (x) button works on the modal window and check if Get Involved modal windows sub-buttons are visible and get us to the appropriate link', () => {
-      clickOnGetInvolvedButton()
-      closeGetInvolvedModalWindow()
-      clickOnGetInvolvedButton()
-      joinAsVolunteerIsVisibleAndPointToAppropriateLink()
-      joinAsAmbassadorIsVisibleAndPointToAppropriateLink()
-      joinStudentProgramIsVisibleAndPointToAppropriateLink()
-    })
+    it("Check if close (x) button works on the modal window and check if Get Involved modal windows sub-buttons are visible and get us to the appropriate link", () => {
+      clickOnGetInvolvedButton();
+      closeGetInvolvedModalWindow();
+      clickOnGetInvolvedButton();
+      joinAsVolunteerIsVisibleAndPointToAppropriateLink();
+      joinAsAmbassadorIsVisibleAndPointToAppropriateLink();
+      joinStudentProgramIsVisibleAndPointToAppropriateLink();
+    });
 
-    it('Check if Go to Help Board button visible and get us to the appropriate link', () => {
-      goToHelpBoardButtonIsVisibleAndPointToAppropriateLink()
-    })
+    it("Check if Go to Help Board button visible and get us to the appropriate link", () => {
+      goToHelpBoardButtonIsVisibleAndPointToAppropriateLink();
+    });
 
-    it('Check if FightPandemics email links are working', () => {
-      partnershipEmailLinkIsVisibleAndPointToAppropriateLink()
-      contactEmailLinkIsVisibleAndPointToAppropriateLink()
-    })
-  })
+    it("Check if FightPandemics email links are working", () => {
+      partnershipEmailLinkIsVisibleAndPointToAppropriateLink();
+      contactEmailLinkIsVisibleAndPointToAppropriateLink();
+    });
+  });
 
   //the following functions are for make the code more readable and maintainable, and to avoid code duplications
 
   function linkedInButtonIsVisibleAndPointToAppropriateLink() {
-    cy.checkAnyKindOfLinks(aboutUs.linkedInButton, aboutUs.linkedInLink)
+    cy.checkAnyKindOfLinks(aboutUs.linkedInButton, aboutUs.linkedInLink);
   }
 
   function facebookButtonIsVisibleAndPointToAppropriateLink() {
-    cy.checkAnyKindOfLinks(aboutUs.facebookButton, aboutUs.facebookLink)
+    cy.checkAnyKindOfLinks(aboutUs.facebookButton, aboutUs.facebookLink);
   }
 
   function instagramButtonIsVisibleAndPointToAppropriateLink() {
-    cy.checkAnyKindOfLinks(aboutUs.instagramButton, aboutUs.instagramLink)
+    cy.checkAnyKindOfLinks(aboutUs.instagramButton, aboutUs.instagramLink);
   }
 
   function twitterButtonIsVisibleAndPointToAppropriateLink() {
-    cy.checkAnyKindOfLinks(aboutUs.twitterButton, aboutUs.twitterLink)
+    cy.checkAnyKindOfLinks(aboutUs.twitterButton, aboutUs.twitterLink);
   }
 
   function clickOnGetInvolvedButton() {
-    cy.get(aboutUs.getInvolvedButton).click()
+    cy.get(aboutUs.getInvolvedButton).click();
   }
 
   function joinAsVolunteerIsVisibleAndPointToAppropriateLink() {
-    cy.checkAnyKindOfLinks(aboutUs.joinAsVolunteerButton, aboutUs.joinAsVolunteerLink)
+    cy.checkAnyKindOfLinks(aboutUs.joinAsVolunteerButton, aboutUs.joinAsVolunteerLink);
   }
 
   function joinAsAmbassadorIsVisibleAndPointToAppropriateLink() {
-    cy.checkAnyKindOfLinks(aboutUs.joinAsAmbassadorButton, aboutUs.joinAsAmbassadorLink)
+    cy.checkAnyKindOfLinks(aboutUs.joinAsAmbassadorButton, aboutUs.joinAsAmbassadorLink);
   }
 
   function joinStudentProgramIsVisibleAndPointToAppropriateLink() {
-    cy.checkAnyKindOfLinks(aboutUs.joinStudentProgramButton, aboutUs.joinStudentProgramLink)
+    cy.checkAnyKindOfLinks(aboutUs.joinStudentProgramButton, aboutUs.joinStudentProgramLink);
   }
 
   function goToHelpBoardButtonIsVisibleAndPointToAppropriateLink() {
-    cy.checkAnyKindOfLinks(aboutUs.getToHelpBoardButton, aboutUs.goToHelpBoardLink)
+    cy.checkAnyKindOfLinks(aboutUs.getToHelpBoardButton, aboutUs.goToHelpBoardLink);
   }
 
   function closeGetInvolvedModalWindow() {
-    cy.get(aboutUs.getInvolvedCloseButton).click()
+    cy.get(aboutUs.getInvolvedCloseButton).click();
   }
 
   function partnershipEmailLinkIsVisibleAndPointToAppropriateLink() {
-    checkEmailWorks(aboutUs.partnershipEmail, aboutUs.partnershipEmailHref)
+    checkEmailWorks(aboutUs.partnershipEmail, aboutUs.partnershipEmailHref);
   }
 
   function contactEmailLinkIsVisibleAndPointToAppropriateLink() {
-    checkEmailWorks(aboutUs.contactEmail, aboutUs.contactEmailHref)
+    checkEmailWorks(aboutUs.contactEmail, aboutUs.contactEmailHref);
   }
 
 
   function checkEmailWorks(email, link) {
     cy.contains(email)
-      .should('be.visible')
-      .and('have.attr', 'href', link)
+      .should("be.visible")
+      .and("have.attr", "href", link);
   }
-})
+});
