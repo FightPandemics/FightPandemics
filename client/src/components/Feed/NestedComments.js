6796fc31636bc96f00c72763b7d37757f9111e42
Dawei Zhang <dz4@pdx.edu>
2020-11-19 13:29:48 -0800

    #1860 - in comments: show user initials when no picture uploaded (#1861)
    * show user initials when no picture uploaded
    
    * should use name of author

--- client/src/components/Feed/NestedComments.js
+++ client/src/components/Feed/NestedComments.js
@@ -1,7 +1,9 @@
 // Core
 import React, { useState } from "react";
 import axios from "axios";
-import { Avatar, Input, Tooltip, Space } from "antd";
+import { Input, Tooltip, Space } from "antd";
+import { Avatar } from "components/Avatar";
+import { getInitialsFromFullName } from "utils/userInfo";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
@@ -51,14 +53,14 @@ const NestedComments = ({ comment, dispatchPostAction, deleteComment }) => {
   const actorId = useSelector(selectActorId);
 
   const renderAvatar = (
-    <Avatar
-      src={
-        comment.author.photo
-          ? comment.author.photo
-          : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTGhWTUkY0xGbbdHyReD6227iz53ADtRmcn1PTN4GUS3clC6MCT&usqp=CAU"
-      }
-      alt={`${comment.author.name}`}
-    />
+    <Avatar src={comment.author.photo} alt={`${comment.author.name}`}>
+      {getInitialsFromFullName(
+        `${
+          comment.author.name ||
+          `${comment.author.firstName} ${comment.author.lastName}`
+        }`,
+      )}
+    </Avatar>
   );
 
   //TODO: Add comment replies, like button and number of likes.
