b17e782afa6397c2209a988b22ca7139c9ca4858
adorapham <70918430+adorapham@users.noreply.github.com>
2020-11-21 23:05:32 -0800

    Feature/privacy policy-Add validation tests for privacy policy page (#1810)
    * Add validation tests for privacy policy page
    1-FP logo
    2-Privacy Policy Header
    3-Existing each paragraph
    4-Link of Email address
    
    * Below are the updated:
    -class name change to 'PrivacyPolicy' in privacePolicy.js and privacyPolicy.spec.js
    -reduce the sentence to check on each paragraph
    -date of document is defined as variable
    -remove email information from Json file
    
    * remove console print
    
    * Clean up the format
    
    * Fix the spelling check checkEmailEachParagraph
    Make the function name more clear and use the passing parameter
    
    * fixing the single quote to double  quote

---
+++ cypresse2e/cypress/elements/pages/privacyPolicy.js
@@ -0,0 +1,107 @@
+class PrivacyPolicy {
+  privacyPolicyH2 = "//*[@id='root']/div/div/div[3]/main/div/div/h2";
+  policyContent = ".am-drawer-content main div div";
+  firstParaLoc = "//*[@id='root']/div/div/div[3]/main/div/div/div[1]";
+  secondParaLoc = "//*[@id='root']/div/div/div[3]/main/div/div/div[2]";
+  thirdParaLoc = "//*[@id='root']/div/div/div[3]/main/div/div/div[3]";
+  fourthParaLoc = "//*[@id='root']/div/div/div[3]/main/div/div/div[4]";
+  dateDocumented = "//*[@id='root']/div/div/div[3]/main/div/div/div[5]";
+  firstParaLocWithOrdList =
+    "//*[@id='root']/div/div/div[3]/main/div/div/ol/li[1]";
+  secondParaLocWithOrdList =
+    "//*[@id='root']/div/div/div[3]/main/div/div/ol/li[2]";
+  thirdParaLocWithOrdList =
+    "//*[@id='root']/div/div/div[3]/main/div/div/ol/li[3]";
+  fourthParaLocWithOrdList =
+    "//*[@id='root']/div/div/div[3]/main/div/div/ol/li[4]";
+  fifthParaLocWithOrdList =
+    "//*[@id='root']/div/div/div[3]/main/div/div/ol/li[5]";
+  sixthParaLocWithOrdList =
+    "//*[@id='root']/div/div/div[3]/main/div/div/ol/li[6]";
+  seventhParaLocWithOrdList =
+    "//*[@id='root']/div/div/div[3]/main/div/div/ol/li[7]";
+  eightParaLocWithOrdList =
+    "//*[@id='root']/div/div/div[3]/main/div/div/ol/li[8]";
+  ninethParaLocWithOrdList =
+    "//*[@id='root']/div/div/div[3]/main/div/div/ol/li[9]";
+  tenthParaLocWithOrdList =
+    "//*[@id='root']/div/div/div[3]/main/div/div/ol/li[10]";
+  eleventhParaLocWithOrdList =
+    "//*[@id='root']/div/div/div[3]/main/div/div/ol/li[11]";
+  twelveParaLocWithOrdList =
+    "//*[@id='root']/div/div/div[3]/main/div/div/ol/li[12]";
+  thirteenParaLocWithOrdList =
+    "//*[@id='root']/div/div/div[3]/main/div/div/ol/li[13]";
+  fourthteenParaLocWithOrdList =
+    "//*[@id='root']/div/div/div[3]/main/div/div/ol/li[14]";
+  fifthteenParaLocWithOrdList =
+    "//*[@id='root']/div/div/div[3]/main/div/div/ol/li[15]";
+
+  legalEmail = "legal@fightpandemics.com";
+  legalEmailRef = "mailto:legal@fightpandemics.com";
+
+  constructor() {}
+
+  visit() {
+    cy.visit("privacy-policy");
+  }
+
+  getPrivacyPolicyH2() {
+    return cy.xpath(this.privacyPolicyH2);
+  }
+
+  getPolicyContent() {
+    return cy.get(this.policyContent);
+  }
+
+  getLocationPara(ParaNumber) {
+    switch (ParaNumber) {
+      case 1:
+        return cy.xpath(this.firstParaLoc);
+      case 2:
+        return cy.xpath(this.secondParaLoc);
+      case 3:
+        return cy.xpath(this.thirdParaLoc);
+      case 4:
+        return cy.xpath(this.fourthParaLoc);
+      case 11:
+        return cy.xpath(this.firstParaLocWithOrdList);
+      case 12:
+        return cy.xpath(this.secondParaLocWithOrdList);
+      case 13:
+        return cy.xpath(this.thirdParaLocWithOrdList);
+      case 14:
+        return cy.xpath(this.fourthParaLocWithOrdList);
+      case 15:
+        return cy.xpath(this.fifthParaLocWithOrdList);
+      case 16:
+        return cy.xpath(this.sixthParaLocWithOrdList);
+      case 17:
+        return cy.xpath(this.seventhParaLocWithOrdList);
+      case 18:
+        return cy.xpath(this.eightParaLocWithOrdList);
+      case 19:
+        return cy.xpath(this.ninethParaLocWithOrdList);
+      case 110:
+        return cy.xpath(this.tenthParaLocWithOrdList);
+      case 111:
+        return cy.xpath(this.eleventhParaLocWithOrdList);
+      case 112:
+        return cy.xpath(this.twelveParaLocWithOrdList);
+      case 113:
+        return cy.xpath(this.thirteenParaLocWithOrdList);
+      case 114:
+        return cy.xpath(this.fourthteenParaLocWithOrdList);
+      case 115:
+        return cy.xpath(this.fifthteenParaLocWithOrdList);
+      default:
+        break;
+    }
+    return cy.xpath(this.firstParaLoc);
+  }
+
+  getDateDocumented() {
+    return cy.xpath(this.dateDocumented);
+  }
+}
+export default PrivacyPolicy;
