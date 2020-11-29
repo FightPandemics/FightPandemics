b7eeaa2066cc4787dd3fd866507ea61f3c26a916
Zayd <zaydme8@gmail.com>
2020-11-29 19:18:23 +0100

    change font size

--- client/src/components/Tag/PostTag.js
+++ client/src/components/Tag/PostTag.js
@@ -1,14 +1,13 @@
 import styled from "styled-components";
 import { theme, mq } from "constants/theme";
 
-const { medium } = theme.typography.size;
 const { royalBlue, selago } = theme.colors;
 
 export const PostTag = styled.span`
   line-height: 2.5rem;
   margin: 0.5rem 0.3rem;
   padding: 0.7rem 1.5rem;
-  font-size: ${medium};
+  font-size: 1.3rem;
   display: inline-block;
   white-space: nowrap;
   color: ${royalBlue};
@@ -19,7 +18,6 @@ export const PostTag = styled.span`
     line-height: 1.9rem;
     margin: 0.5rem 0.3rem;
     padding: 0.7rem 1rem;
-    font-size: 1.3rem;
   }
 `;
 
