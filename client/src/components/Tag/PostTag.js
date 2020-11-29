a4cc0cfd570d3dbfdc55ae281f0db3bb6db1ed36
Zayd <zaydme8@gmail.com>
2020-11-29 19:01:43 +0100

    smaller sized tags for mobile

--- client/src/components/Tag/PostTag.js
+++ client/src/components/Tag/PostTag.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { theme } from "constants/theme";
+import { theme, mq } from "constants/theme";
 
 const { medium } = theme.typography.size;
 const { royalBlue, selago } = theme.colors;
@@ -14,6 +14,13 @@ export const PostTag = styled.span`
   color: ${royalBlue};
   background: ${selago};
   border-radius: 4px;
+
+  @media screen and (max-width: ${mq.phone.wide.maxWidth}) {
+    line-height: 1.9rem;
+    margin: 0.5rem 0.3rem;
+    padding: 0.7rem 1rem;
+    font-size: 1.3rem;
+  }
 `;
 
 export default PostTag;
