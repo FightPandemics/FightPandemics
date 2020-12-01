eb059b7354ce302560f7c30b66bc3bd21fc692fd
Med.Dev <46847212+Zaydme@users.noreply.github.com>
2020-12-01 18:06:04 +0000

    N/A - Fix black text on home page, for some languages (#2016)
    * Fix black text on home page, for some languages
    
    * left aligned the text

--- client/src/components/Typography/TextLabel.js
+++ client/src/components/Typography/TextLabel.js
@@ -6,7 +6,7 @@ const { Text } = Typography;
 
 const TextLabel = styled(Text)`
   &.ant-typography {
-    ${theme.typography.heading.font};
+    font-family: ${theme.typography.heading.font};
     color: ${(props) => props.color || "black"};
     font-size: ${(props) => props.size || theme.typography.size.medium};
     font-weight: ${(props) => props.weight || "normal"};
