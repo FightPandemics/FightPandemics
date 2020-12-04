a5b8abdb362d073269c786955f37a2de4c39b350
Roy Zhang <royqizhang@gmail.com>
2020-12-04 18:22:00 -0500

    darker text color for characters left

--- client/src/components/Input/InputInfo.js
+++ client/src/components/Input/InputInfo.js
@@ -5,7 +5,7 @@ import InputError from "./InputError";
 import { theme } from "constants/theme";
 
 const InputInfo = styled(InputError)`
-    color: ${props => props.error ? theme.colors.red : theme.colors.lightGray}
+    color: ${props => props.error ? theme.colors.red : theme.colors.darkerGray}
 `;
 
 export default InputInfo;
