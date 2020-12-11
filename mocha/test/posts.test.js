39bf56fbfed3a115fb7e857b2601536f9810598a
jessica-lau <jessica.lau2@gmail.com>
2020-12-10 21:29:08 -0500

    add test for checking a list of posts exist in endpoint

--- mocha/test/posts.test.js
+++ mocha/test/posts.test.js
@@ -4,4 +4,16 @@ const apiEndpoints = require("../utils/apiEndpoints");
 const apiHelper = require("../utils/apiHelper");
 const validator = require("../utils/validators");
 
-let apiEndpoint = apiEndpoints.postsEndpoint;
+let postsApiEndpoint = apiEndpoints.postsEndpoint;
+
+describe("GET posts endpoint test", () => {
+  it("send GET request to check that a list of posts exist in endpoint", async () => {
+    let response = await apiHelper.sendGETRequest(APP_URL, postsApiEndpoint);
+    validator.validateStatusCodeResponseBody(
+      response,
+      httpStatus.OK,
+      "_id",
+      "types",
+    );
+  });
+});
