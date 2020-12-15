131184bb19b490a8781b4bae69133dfa44a5fad7
jessica-lau <jessica.lau2@gmail.com>
2020-12-15 12:54:43 -0500

    added errorSchema, description, response to createFeedbackSchema and createPostReportSchema

--- backend/lib/endpoints/schema/feedback.js
+++ backend/lib/endpoints/schema/feedback.js
@@ -1,19 +1,32 @@
 const S = require("fluent-schema");
 const { strictSchema } = require("./utils");
 
+const feedbackSchema = strictSchema()
+  .prop("age", S.integer().minimum(1).maximum(150))
+  .prop("covidImpact", S.string())
+  .prop("generalFeedback", S.string())
+  .prop("mostValuableFeature", S.string())
+  .prop("rating", S.integer().minimum(1).maximum(5))
+  .prop("userId", S.string())
+  .prop("whatWouldChange", S.string());
+
+const errorSchema = S.object()
+  .prop("statusCode", S.integer())
+  .prop("error", S.string())
+  .prop("message", S.string());
+
 const createFeedbackSchema = {
-  body: strictSchema()
-    .prop("age", S.integer().minimum(1).maximum(150))
-    .prop("covidImpact", S.string())
-    .prop("generalFeedback", S.string())
-    .prop("mostValuableFeature", S.string())
-    .prop("rating", S.integer().minimum(1).maximum(5).required())
-    .prop("userId", S.string())
-    .prop("whatWouldChange", S.string()),
+  body: feedbackSchema.required(["rating"]),
   connection: strictSchema().prop(
     "remoteAddress",
     S.string().format("ipv4").format("ipv6").required(),
   ),
+  description: "For creating a new feedback record in the database.",
+  response: {
+    201: S.object().prop("success", S.boolean()),
+    400: errorSchema,
+    500: errorSchema,
+  },
 };
 
 module.exports = {
--- backend/lib/endpoints/schema/reports.js
+++ backend/lib/endpoints/schema/reports.js
@@ -1,8 +1,21 @@
 const S = require("fluent-schema");
 const { strictSchema } = require("./utils");
 
+const postReportSchema = strictSchema().prop("reason", S.string());
+
+const errorSchema = S.object()
+  .prop("statusCode", S.integer())
+  .prop("error", S.string())
+  .prop("message", S.string());
+
 const createPostReportSchema = {
-  body: strictSchema().prop("reason", S.string().required()),
+  body: postReportSchema.required(["reason"]),
+  description: "For flagging and reporting a post by its post id.",
+  response: {
+    201: S.object().prop("success", S.boolean()),
+    401: errorSchema,
+    500: errorSchema,
+  },
 };
 
 const getPostReportsSchema = {
