4b8b8259365a8e49e9a9ce6d932d627bb58a524a
galeza <galeza.agnieszka@gmail.com>
2020-12-15 12:21:24 +0100

    Correct validators

--- mocha/utils/validators.js
+++ mocha/utils/validators.js
@@ -38,16 +38,12 @@ exports.validateStatusCodeResponseBody = function (
       expect(object).to.have.a.property(key2);
     }),
   );
+};
 
-  exports.validateStatusCodeAndMessage = function (
-    response,
-    statusCode,
-    status,
-  ) {
-    expect(response).to.have.a.property("statusCode").to.be.equal(statusCode);
-    expect(response).to.have.a.property("status").to.be.equal(status);
-    expect(response).to.have.a.property("ok").to.be.equal(true);
-  };
+exports.validateStatusCodeAndMessage = function (response, statusCode, status) {
+  expect(response).to.have.a.property("statusCode").to.be.equal(statusCode);
+  expect(response).to.have.a.property("status").to.be.equal(status);
+  expect(response).to.have.a.property("ok").to.be.equal(true);
 };
 
 exports.validateInitialAndResponseObject = function (
