38b86322a104a8253313859fb8993d78cf4b0e37
galeza <galeza.agnieszka@gmail.com>
2020-12-17 12:24:30 +0100

    Remove unused var

--- mocha/utils/apiEndpoints.js
+++ mocha/utils/apiEndpoints.js
@@ -3,6 +3,3 @@ exports.feedbackEndpoint = "/api/feedback";
 exports.signUpApiEndpoint = "/api/auth/signup";
 exports.organisationsEndpoint = "/api/organisations";
 exports.postsEndpoint = "/api/posts";
-exports.postApiEndpoint = "/api/posts/{postId}";
-exports.getPostApiEndpoint = "/api/posts/";
-exports.getPostApiEndpoint = "/api/posts";
