17febeecc14844ca6c21251dcbae03463368ab7b
Selbinyyaz <altyyevaselbi@gmail.com>
2021-01-01 21:30:53 -0600

    some changes

--- mocha/utils/apiHelper.js
+++ mocha/utils/apiHelper.js
@@ -1,7 +1,6 @@
 const supertest = require("supertest");
 
 exports.sendPOSTRequest = async (baseUrl, apiEndPoint, requestBody) => {
-
   try {
     let res = await supertest(baseUrl)
       .post(apiEndPoint)
@@ -28,19 +27,3 @@ exports.sendGETRequest = async (baseUrl, apiEndPoint) => {
     console.log("Error in sending GET Request: ", err);
   }
 };
-
-    try {
-        let res = await supertest(baseUrl).post(apiEndPoint).retry(2)
-            .set('Accept', 'application/json')
-            .set('Content-Type', 'application/json')
-            .send(requestBody);
-        return res;
-    } catch (err) {
-        console.log('Error in sending POST Request: ', err);
-    }
-};
-
-
-
-
-
