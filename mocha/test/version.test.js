7ac83c463dcfe0132d2d6d34b3aa13820a787257
Adora Pham <adora.pham@gmail.com>
2021-01-04 20:27:38 -0800

    Add API testing for getting version of FP

--- mocha/test/version.test.js
+++ mocha/test/version.test.js
@@ -1,2 +1,25 @@
-const httpStatus= require("http-status");
+const httpStatus = require("http-status");
 const APP_URL = process.env.MOCHA_URL;
+const apiEndPoints = require("../utils/apiEndpoints");
+const apiHelper = require("../utils/apiHelper");
+const validator = require("../utils/validators");
+
+/* commit: "unknown",
+servivceName: "fightpandemics", 
+version: "1.0.0",  */
+let apiEndPoint = apiEndPoints.versionApiEndpoint;
+
+describe("GET Version endpoint test", () => {
+  it("sending GET request with endpoint, status : 200 OK with response body", async () => {
+    let response = await apiHelper.sendGETRequest(APP_URL, apiEndPoint);
+    validator.validateResponse(response, {
+      statusCode: httpStatus.OK,
+    });
+
+    validator.validateResponse(response.body, {
+      commit: "unknown",
+      serviceName: "fightpandemics",
+      version: "1.0.0",
+    });
+  });
+});
