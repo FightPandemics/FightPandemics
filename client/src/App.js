42ba070bd0b7f7bbc2c921d353d6076e3b72c87c
Ozkan <ozkanabdullahoglu@gmail.com>
2021-01-14 19:05:40 +0300

    feat: data layers * systemLang as default browser language * user preferable lang as language

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
+import TagManager from "react-gtm-module";
 
 import { initAuth } from "./actions/authActions";
 import routes from "./routes";
@@ -8,11 +9,22 @@ import AppMetaContainer from "components/Meta/AppMetaContainer";
 import RouteWithSubRoutes from "./templates/RouteWithSubRoutes";
 import history from "./utils/history";
 import ScrollToTop from "components/Scroll/ScrollTop";
+import { getLang } from "i18n";
 
 const App = (props) => {
   useEffect(() => {
     props.initAuth();
   }, [props]);
+
+  useEffect(() => {
+    const { language, systemLang } = getLang();
+    TagManager.dataLayer({
+      dataLayer: {
+        systemLang,
+        language,
+      },
+    });
+  }, []);
 
   return (
     <Router history={history}>
