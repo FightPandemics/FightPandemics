630b4a00492847276fe1f955c3b95d53ffa22e4c
Kamaren <43706940+kamarenmccord@users.noreply.github.com>
2021-01-26 11:36:13 -0600

    made zendesk widget dynamic and added react-zendesk npm package (#2149)
    * made zendesk widget dynamic and added react-zendesk npm package
    
    * forgot to save the package
    
    * Fixed widget dissapering on pages where it is turned off
    
    * added removed lines from package-lock
    
    * removed all  package-lock adjustments
    
    * Delete package-lock.json
    
    try this change if it can restore the review build
    
    * didnt mean to delete this json file
    
    Co-authored-by: Dawei Zhang <dz4@pdx.edu>
    Co-authored-by: ddveloper <44758313+ddveloper@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import Zendesk, { ZendeskAPI } from "react-zendesk";
 
 import { initAuth } from "./actions/authActions";
 import routes from "./routes";
@@ -10,14 +12,21 @@ import history from "./utils/history";
 import ScrollToTop from "components/Scroll/ScrollTop";
 
 const App = (props) => {
+  const { i18n } = useTranslation();
+
   useEffect(() => {
     props.initAuth();
   }, [props]);
+
+  useEffect(() => {
+    ZendeskAPI("webWidget", "setLocale", `${i18n.language}`);
+  }, [i18n.language]);
 
   return (
     <Router history={history}>
       <AppMetaContainer />
       <ScrollToTop />
+      <Zendesk zendeskKey={"95ac876b-8717-438b-ac57-7a2239714c9b"} />
       <Switch>
         {routes.map((route, i) => (
           <RouteWithSubRoutes key={i} {...route} />
