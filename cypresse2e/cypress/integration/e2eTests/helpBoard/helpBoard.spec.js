bd355bbe0b3d234a504a5969d82d4712c52c6028
JayashreeSamantasinghar <68357352+JayashreeSamantasinghar@users.noreply.github.com>
2021-01-30 03:42:28 -0800

    added tests for search in helpboard

--- cypresse2e/cypress/integration/e2eTests/helpBoard/helpBoard.spec.js
+++ cypresse2e/cypress/integration/e2eTests/helpBoard/helpBoard.spec.js
@@ -1,11 +1,14 @@
 import HelpBoard from "../../../elements/pages/helpBoard";
 import Logo from "../../../elements/pages/fpLogo";
+import inputValues from "../../../fixtures/inputValues.json";
+import errorMessages from "../../../fixtures/errorMessages.json";
 
 describe("FightPandemics Help Board Page for unauthorized user", () => {
   const helpBoard = new HelpBoard();
   const logo = new Logo();
-  var noPostsAvailableText =
-    "Sorry, there are currently no relevant posts available. Please try using a different filter search or";
+  var authorTitleText = "Sourced by";
+  // var noPostsAvailableText =
+  //"Sorry, there are currently no relevant posts available. Please try using a different filter search or";
 
   context("User opens Help Board", () => {
     beforeEach(() => {
@@ -21,12 +24,63 @@ describe("FightPandemics Help Board Page for unauthorized user", () => {
       cy.validateCorrectScreenIsOpen("auth/login");
     });
 
-    it.skip("Help Board is empty - Unauthorized user is redirected to SignIn page when clicking Add Post link", () => {
+    it("keyword search container is visible and clickable", () => {
+      helpBoard.getSearchContainerDiv().should("be.visible").click();
+    });
+    it(" clicking on search container, searchbox for typing opens", () => {
+      searchInputForPosts();
+    });
+
+    it("cancel button 'x' in search is visible and clickable", () => {
+      searchInputForPosts().type(inputValues.searchByRandomText + "{enter}");
+      helpBoard
+        .getSearchCancelButton()
+        .should("be.visible")
+        .click({ force: true });
+    });
+    it("Unauthorized user can search posts by typing name of an user", () => {
+      searchInputForPosts().type(inputValues.searchByName + "{enter}");
+      helpBoard.getAuthorTitle().should("be.visible").contains(authorTitleText);
+    });
+
+    it("Unauthorized user if type single character for search", () => {
+      searchInputForPosts().type(
+        inputValues.searchBySingleCharacter + "{enter}",
+      );
+      helpBoard
+        .getErrorMessage()
+        .should("be.visible")
+        .contains("small", errorMessages.errorMessageTextInSearch);
+    });
+    it("No post appear if unauthorized user search posts by typing misspelled name", () => {
+      searchInputForPosts().type(
+        inputValues.searchByMisspelledText + "{enter}",
+      );
+      helpBoard.getAddPostLinkElement();
+      helpBoard
+        .getNoPostsAvailableTextElement()
+        .contains(errorMessages.noPostsAvailableText);
+    });
+
+    it("Help Board is empty - Unauthorized user is redirected to SignIn page when clicking Add Post link", () => {
       //there is no post on the Help Board
-      helpBoard.getNoPostsAvailableTextElement().contains(noPostsAvailableText);
+      searchInputForPosts().type(
+        inputValues.searchByMisspelledText + "{enter}",
+      );
+      helpBoard
+        .getNoPostsAvailableTextElement()
+        .contains(errorMessages.noPostsAvailableText);
+      helpBoard.getAddPostLinkElement();
       var addPostLink = helpBoard.getAddPostLinkElement();
       addPostLink.click({ force: true });
       cy.validateCorrectScreenIsOpen("auth/login");
     });
   });
+
+  function searchInputForPosts() {
+    helpBoard.getSearchContainerDiv().click();
+    var searchInput = helpBoard.getKeywordSearchInput();
+    searchInput.should("be.visible").and("have.attr", "type", "text");
+    return searchInput;
+  }
 });
