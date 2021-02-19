59f59c76c0ac62edcb12269600af9a55c6600ce0
Beth Maskow <beth_maskow@yahoo.com>
2021-02-19 16:31:56 -0400

    bmpd iphones a little more again

--- client/src/components/NearestHospital/HealthFacilities.js
+++ client/src/components/NearestHospital/HealthFacilities.js
@@ -36,7 +36,7 @@ const HealthFacilitiesContainer = styled.div`
   align-self: flex-start;
   padding-top: 2rem;
   @media screen and (max-width: ${mq.phone.wide.maxWidth}) {
-    margin: ${(props) => (props.isIPhone ? "28rem 0 0 0" : "20rem 0 0 0")};
+    margin: ${(props) => (props.isIPhone ? "32rem 0 0 0" : "20rem 0 0 0")};
   }
   h2 {
     font-weight: bold;
