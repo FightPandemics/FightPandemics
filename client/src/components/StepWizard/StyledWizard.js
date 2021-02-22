1d427e94a7a99546a6df305f3c491a16db1290f2
David Merenyi <peedee4@gmail.com>
2021-02-22 23:11:22 +0100

    [1363] Show navigation under the steps in StyledWizard

--- client/src/components/StepWizard/StyledWizard.js
+++ client/src/components/StepWizard/StyledWizard.js
@@ -8,21 +8,20 @@ const StyledWizard = styled(StepWizard)`
   display: flex;
   flex-direction: column-reverse;
   align-self: flex-start;
-  transition: 0.5s; 
+  transition: 0.5s;
   opacity: ${({ status }) => {
     switch (status) {
       case "entering":
-        return '0'
+        return "0";
       case "entered":
-        return '1'
+        return "1";
       case "exiting":
-        return '1'
+        return "1";
       case "exited":
-        return '0'
+        return "0";
     }
   }};
   width: 100%;
-  height: 100%;
   margin: 0 auto;
   justify-content: space-between;
   @media screen and (min-width: ${tablet.narrow.minWidth}) {
