6f07f9220d5cbf93edecd28467016b74739a04e1
James <50638563+xJamesScott@users.noreply.github.com>
2021-03-26 16:59:12 -0400

    apply page updates

--- client/src/pages/Apply.js
+++ client/src/pages/Apply.js
@@ -21,7 +21,7 @@ import {
     fetchUserSuccess
 } from "hooks/actions/userActions";
 import React, {
-    useContext, useEffect, useRef
+    useContext, useEffect, useRef, useState
 } from "react";
 import { useTranslation } from "react-i18next";
 import { getInitialsFromFullName } from "utils/userInfo";
@@ -35,8 +35,10 @@ import {
     UserInfoDesktop
 } from "../components/Profile/ProfileComponents";
 import { useHistory } from "react-router-dom";
+import ExitModal from "components/Positions/ExitModal";
 
 const Apply = () => {
+
     const history = useHistory();
     const [visible, setVisible] = useState(false);
     const handleBackRequest = async (e) => {
@@ -53,34 +55,16 @@ const Apply = () => {
     const handleCancel = async (e) => {
         setVisible(false);
         window.history.pushState({}, '',);
-        // window.addEventListener('popstate', (e) => {
-        //     e.preventDefault();
-        //     alert("TEST2!")
-
-        // }
-        // )
-
-
-        // const history = useHistory();
-
-        // useEffect(() => {
-        //     const { state, pathname } = history.location;
-        //     history.push(pathname, {
-        //         ...state,
-        //         keepScroll: true,
-        //     });
-        //     window.addEventListener("popstate", onBrowserBack);
-        //     return () => {
-        //         window.removeEventListener("popstate", onBrowserBack);
-        //     };
-        // }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-        // const onBrowserBack = (e) => {
-        //     alert("TEST!")
-        //     e.preventDefault();
-        // };
-
     }
+
+    useEffect(() => {
+        window.history.pushState({}, '',);
+        window.onpopstate = function () {
+            initialBackRequest();
+        };
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+
     let url = window.location.pathname.split("/");
     const organisationId = url[url.length - 2];
     const { orgProfileState, orgProfileDispatch } = useContext(
@@ -107,66 +91,100 @@ const Apply = () => {
     }
 
     useEffect(() => {
-        window.history.pushState({}, '',);
-        window.onpopstate = function () {
-            initialBackRequest();
-        };
-    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+        (async function fetchOrgProfile() {
+            orgProfileDispatch(fetchOrganisation());
+            userProfileDispatch(fetchUser());
+            try {
+                const res = await axios.get(`/api/organisations/${organisationId}`);
+                orgProfileDispatch(fetchOrganisationSuccess(res.data));
+            } catch (err) {
+                const message = err.response?.data?.message || err.message;
+                const translatedErrorMessage = t([
+                    `error.${message}`,
+                    `error.http.${message}`,
+                ]);
+                orgProfileDispatch(
+                    fetchOrganisationError(
+                        `${t("error.failedLoadingProfile")} ${translatedErrorMessage}`,
+                    ),
+                );
+            }
+        })();
+        (async function fetchUserProfile() {
+            userProfileDispatch(fetchUser());
+            try {
+                const res = await axios.get("/api/users/current");
+                userProfileDispatch(fetchUserSuccess(res.data));
+            } catch (err) {
+                const message = err.response?.data?.message || err.message;
+                const translatedErrorMessage = t([
+                    `error.${message}`,
+                    `error.http.${message}`,
+                ]);
+                userProfileDispatch(
+                    fetchUserError(
+                        `${t("error.failedLoadingProfile")} ${translatedErrorMessage}`,
+                    ),
+                );
+            }
+        })();
+    }, [orgProfileDispatch, organisationId, userProfileDispatch]); // eslint-disable-line react-hooks/exhaustive-deps
 
+    if (error) {
+        return <ErrorAlert message={error} type="error" />;
+    }
+    if (loading) return <Loader />;
 
+    if (!organisation) {
+        return <Loader />;
+    }
+    else {
+        const { address } = location;
 
-    return (
-        // Header and class/component container for position info will be needed from new profile design to be consistent
-        <>
-            <ProfileBackgroup />
-            <ProfileLayout>
-                <PositionsContainer>
-                    <UserInfoContainer>
-                        <AvatarPhotoContainer>
-                            <ProfilePic
-                                user={organisation}
-                                initials={getInitialsFromFullName(name)}
-                            />
-                        </AvatarPhotoContainer>
-                        <UserInfoDesktop>
-                            <NameDiv>
-                                <div className="name-container">
-                                    <NamePara>
-                                        {name}
-                                    </NamePara>
-                                    {address && (
-                                        <div title={address} className="address-container">
-                                            <img src={locationIcon} alt={address} />
-                                            {address}
-                                        </div>
-                                    )}
-                                </div>
-                            </NameDiv>
-                            {about && <DescriptionDesktop> {about} </DescriptionDesktop>}
-                        </UserInfoDesktop>
-                    </UserInfoContainer>
-                    <div>test</div>
+        return (
+            // Header and class/component container for position info will be needed from new profile design to be consistent
+            <>
 
-                    <PositionApplicationForm
-                        orgName={name}
-                    >
-                        <ExitModal
-                            visible={visible}
-                            handleExit={handleExit}
-                            handleCancel={handleCancel}
-                        />
-                        <div>{window.history.state.idx}</div>
-                        <TestButton
-                            onClick={handleBackRequest}
-                        >
-                            CLICK TEST
-                        </TestButton>
-                    </PositionApplicationForm>
-                </PositionsContainer >
-            </ProfileLayout>
-        </>
-    );
+                <ProfileBackgroup />
+                <ProfileLayout>
+                    <PositionsContainer>
+                        <UserInfoContainer>
+                            <AvatarPhotoContainer>
+                                <ProfilePic
+                                    user={organisation}
+                                    initials={getInitialsFromFullName(name)}
+                                />
+                            </AvatarPhotoContainer>
+                            <UserInfoDesktop>
+                                <NameDiv>
+                                    <div className="name-container">
+                                        <NamePara>
+                                            {name}
+                                        </NamePara>
+                                        {address && (
+                                            <div title={address} className="address-container">
+                                                <img src={locationIcon} alt={address} />
+                                                {address}
+                                            </div>
+                                        )}
+                                    </div>
+                                </NameDiv>
+                                {about && <DescriptionDesktop> {about} </DescriptionDesktop>}
+                            </UserInfoDesktop>
+                        </UserInfoContainer>
+                        <PositionApplicationForm
+                            orgName={name}
+                        ></PositionApplicationForm>
+                    </PositionsContainer >
+                    <ExitModal
+                        visible={visible}
+                        handleExit={handleExit}
+                        handleCancel={handleCancel}
+                    />
+                </ProfileLayout>
+            </>
+        );
+    }
 }
-
 
 export default withUserContext(withOrganisationContext(Apply));
