83aa2b9f7a451e7493edff959e7ede826e65772b
linadinh2602 <lina.dinh262@gmail.com>
2021-04-10 15:05:07 -0700

    Add react testing to home component

---
+++ client/src/utils/constants.js
@@ -0,0 +1,8 @@
+const DataTestIds = {
+  HOME_HEAD_LINE: "home-head-line",
+  HOME_NEED_HELP_BUTTON: "home-need-help-button",
+  HOME_GIVE_HELP_BUTTON: "home-give-help-button",
+  HOME_VIEW_FEED_LINK: "home-view-feed-link",
+};
+
+export default DataTestIds;
