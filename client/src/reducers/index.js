6191548dc965435f63f0a086425d78ebd44f925a
xJamesScott <50638563+xJamesScott@users.noreply.github.com>
2021-04-21 10:37:30 -0400

    reducers index add members

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,12 +3,14 @@ import session from "./session";
 import posts from "./posts";
 import webSocket from "./wsReducers";
 import applicants from "./applicants"
+import members from "./members"
 
 const rootReducer = combineReducers({
   session,
   posts,
   webSocket,
-  applicants
+  applicants,
+  members
 });
 
 export default rootReducer;
