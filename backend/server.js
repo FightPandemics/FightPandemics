6b339d156430117873af8d1a75985ad88430695a
xJamesScott <50638563+xJamesScott@users.noreply.github.com>
2021-05-02 11:27:33 -0400

    server config

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,24 @@ require("dotenv").config();
 const createServer = require("./lib");
 const { config } = require("./config");
 
-const server = createServer(config);
+const server = createServer(config)
+// const server = createServer(config, {
+//     cors: {
+//       origin: '0.0.0',
+//     }
+//   });
+
+// const http = require('http').Server(server);
+
+
+
+// const io = require('socket.io')(server, {
+//   cors: {
+//     origin: '*',
+//   }
+// });
+
+// io.set('origins', 'http://0.0.0.0:8000');
 
 server.ready(async () => {
   await server.oas();
