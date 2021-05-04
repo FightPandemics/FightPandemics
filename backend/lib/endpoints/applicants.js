2d5497379d6864a09cada3523ed88f3860244b07
xJamesScott <50638563+xJamesScott@users.noreply.github.com>
2021-05-04 19:04:59 -0400

    endpoint update

--- backend/lib/endpoints/applicants.js
+++ backend/lib/endpoints/applicants.js
@@ -95,7 +95,7 @@ async function routes(app) {
       schema: getApplicantsSchema
     },
     async (req) => {
-      const { limit, skip, organisationId, includeMeta, permissions, userId } = req.query;
+      const { limit, skip, organisationId, includeMeta, permissions, userId, status } = req.query;
       const [applicantsErr, applicants] = await app.to(
         Applicant.aggregate(
           organisationId
@@ -137,9 +137,10 @@ async function routes(app) {
       const totalResultsAggregationPipeline = await Applicant.aggregate(
         organisationId
           ? [
-            { $match: { organization: { id: mongoose.Types.ObjectId(organisationId) } } },
+            { $match: { organization: { id: mongoose.Types.ObjectId(organisationId) }} },
             { $group: { _id: null, count: { $sum: 1 } } },
           ]
+
           : [
             // { $match: { organization: { id: mongoose.Types.ObjectId(organisationId) } } },
             { $group: { _id: null, count: { $sum: 1 } } },
@@ -173,92 +174,180 @@ async function routes(app) {
     }
   );
 
-  app.post(
-    "/",
-    // TODO - change authenticateOptional to authenticate
-    {
-      preValidation: [app.authenticateOptional, app.setActor],
-      schema: createApplicantSchema,
-    },
-    async (req, reply) => {
-      const { actor, body: applicantProps } = req;
-
-      //Creates the embeded author document
-      applicantProps.applicant = {
-        id: mongoose.Types.ObjectId(actor.id),
-        location: actor.location,
-        name: actor.name,
-        photo: actor.photo,
-        type: actor.type,
-        verified: actor.verification && actor.verification.status === "approved",
-      };
-
-      const [err, applicant] = await app.to(new Applicant(applicantProps).save());
-
-      if (err) {
-        req.log.error(err, "Failed creating applicant.");
-        throw app.httpErrors.internalServerError();
-      }
-
-      reply.code(201);
-      return {
-        ...applicant.toObject(),
-      };
-
-    }
-  );
-  app.patch(
-    "/:applicantId",
-    {
+  // Applicants for orgasniation
+  app.get(
+    "/:organisationId/applicants",
       // TODO - change authenticateOptional to authenticate
-      preValidation: [app.authenticateOptional],
-      schema: updateApplicantStatusSchema
-    },
-    async (req) => {
-      const {
-        body,
-        params: { applicantId },
-        organizationId,
-      } = req;
-      // const [applicantErr, applicant] = await app.to(Applicant.findById(applicantId));
-      // // const [orgErr, org] = await app.to(Organization.findById(organizationId));
-      // console.log("BE TEST!")
-      // // console.log({ applicant: req })
-      // if (applicantErr) {
-      //   req.log.error(applicantErr, "Failed retrieving data for applicant");
-      //   throw app.httpErrors.internalServerError();
-      // } else if (applicant === null) {
-      //   throw app.httpErrors.notFound();
-      // } /* We need to check below wcenario if it can occur. If not then we can delete below check */
-      // else if (!applicant.organizationId.equals(organizationId)) {
-      //   req.log.error("Organization owner/admin not allowed to update the application status.");
-      //   throw app.httpErrors.forbidden();
-      // }
-
-      console.log({ applicantId: applicantId })
-
-      const [updateErr, updateApplicant] = await app.to(
-        // console.log("update test!")
-        Applicant.findOneAndUpdate(
-          { _id: applicantId },
-          { $set: { status: body.status } }
-        ),
-      );
-      if (updateErr) {
-        if (updateErr.name === "ValidationError" ||
-          updateErr.name === "MongoError"
-        ) {
-          throw app.httpErrors.conflict("Validation error.");
+      {
+        preValidation: [app.authenticateOptional],
+        schema: getApplicantsSchema
+      },
+      async (req) => {
+        const { limit, skip, organisationId, includeMeta, permissions, userId, status } = req.query;
+        const [applicantsErr, applicants] = await app.to(
+          Applicant.aggregate(
+            organisationId
+              ?
+              [
+                {
+                  $match: {
+                    organization: { id: mongoose.Types.ObjectId(organisationId) }
+                  }
+                }
+                ,
+                {
+                  $skip: parseInt(skip, 10) || 0,
+                },
+                {
+                  $limit: parseInt(limit, 10) || APPLICANT_PAGE_SIZE
+                },
+              ] :
+              [
+                {
+                  $skip: parseInt(skip, 10) || 0,
+                },
+                {
+                  $limit: parseInt(limit, 10) || APPLICANT_PAGE_SIZE
+                },
+              ]
+  
+          ).then((applicants) => {
+            applicants.forEach((applicant) => {
+              applicant.elapsedTimeText = setElapsedTimeText(
+                applicant.createdAt,
+                applicant.updatedAt
+              );
+            });
+            return applicants;
+          })
+        );
+  
+        const totalResultsAggregationPipeline = await Applicant.aggregate(
+          organisationId
+            ? [
+              { $match: { organization: { id: mongoose.Types.ObjectId(organisationId) } || ""} },
+              { $group: { _id: null, count: { $sum: 1 } } },
+            ]
+  
+            : [
+              // { $match: { organization: { id: mongoose.Types.ObjectId(organisationId) } } },
+              { $group: { _id: null, count: { $sum: 1 } } },
+            ],
+        );
+        const applicantsResponse = (response) => {
+          if (!includeMeta) {
+            return response;
+          }
+          return {
+            meta: {
+              total: totalResultsAggregationPipeline.length
+                ? totalResultsAggregationPipeline[0].count
+                : 0,
+            },
+            data: response,
+          };
+        };
+  
+        if (applicantsErr) {
+          req.log.error(applicantsErr, "Failed requesting applicants");
+          throw app.httpErrors.internalServerError();
         }
-        req.log.error(updateErr, "Failed updating applicant status.");
-        throw app.httpErrors.internalServerError();
+        else if (applicants === null) {
+          return applicantsResponse([]);
+        }
+        else {
+          return applicantsResponse(applicants);
+        }
+  
       }
+    );
+
+      app.post(
+        "/",
+        // TODO - change authenticateOptional to authenticate
+        {
+          preValidation: [app.authenticateOptional, app.setActor],
+          schema: createApplicantSchema,
+        },
+        async (req, reply) => {
+          const { actor, body: applicantProps } = req;
+
+          //Creates the embeded author document
+          applicantProps.applicant = {
+            id: mongoose.Types.ObjectId(actor.id),
+            location: actor.location,
+            name: actor.name,
+            photo: actor.photo,
+            type: actor.type,
+            verified: actor.verification && actor.verification.status === "approved",
+          };
+
+          const [err, applicant] = await app.to(new Applicant(applicantProps).save());
+
+          if (err) {
+            req.log.error(err, "Failed creating applicant.");
+            throw app.httpErrors.internalServerError();
+          }
+
+          reply.code(201);
+          return {
+            ...applicant.toObject(),
+          };
+
+        }
+      );
+      app.patch(
+        "/:applicantId",
+        {
+          // TODO - change authenticateOptional to authenticate
+          preValidation: [app.authenticateOptional],
+          schema: updateApplicantStatusSchema
+        },
+        async (req) => {
+          const {
+            body,
+            params: { applicantId },
+            organizationId,
+          } = req;
+          // const [applicantErr, applicant] = await app.to(Applicant.findById(applicantId));
+          // // const [orgErr, org] = await app.to(Organization.findById(organizationId));
+          // console.log("BE TEST!")
+          // // console.log({ applicant: req })
+          // if (applicantErr) {
+          //   req.log.error(applicantErr, "Failed retrieving data for applicant");
+          //   throw app.httpErrors.internalServerError();
+          // } else if (applicant === null) {
+          //   throw app.httpErrors.notFound();
+          // } /* We need to check below wcenario if it can occur. If not then we can delete below check */
+          // else if (!applicant.organizationId.equals(organizationId)) {
+          //   req.log.error("Organization owner/admin not allowed to update the application status.");
+          //   throw app.httpErrors.forbidden();
+          // }
+
+          console.log({ applicantId: applicantId })
+
+          const [updateErr, updateApplicant] = await app.to(
+            // console.log("update test!")
+            Applicant.findOneAndUpdate(
+              { _id: applicantId },
+              { $set: { status: body.status } }
+            ),
+          );
+          if (updateErr) {
+            if (updateErr.name === "ValidationError" ||
+              updateErr.name === "MongoError"
+            ) {
+              throw app.httpErrors.conflict("Validation error.");
+            }
+            req.log.error(updateErr, "Failed updating applicant status.");
+            throw app.httpErrors.internalServerError();
+          }
 
 
-      return updateApplicant;
-    }
-  );
+          return updateApplicant;
+        }
+      );
 
-};
+    };
 
-module.exports = routes;
+  module.exports = routes;
