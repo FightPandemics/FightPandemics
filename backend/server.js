383934496bc880fee1848f6ae597fce71e54f731
xJamesScott <50638563+xJamesScott@users.noreply.github.com>
2021-06-07 11:45:19 -0400

    temp server config

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,14 @@ require("dotenv").config();
 const createServer = require("./lib");
 const { config } = require("./config");
 
-const server = createServer(config);
+// const server = createServer(config);
+
+// TODO - REMOVE TEMP SERVER CONFIG BELOW
+const server = createServer(config, {
+  cors: {
+    origin: '0.0.0.0',
+  }
+});
 
 server.ready(async () => {
   await server.oas();
