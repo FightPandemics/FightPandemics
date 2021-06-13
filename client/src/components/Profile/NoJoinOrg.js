a22838d90b6d507d833a1b3f1b789907b92921c8
xJamesScott <50638563+xJamesScott@users.noreply.github.com>
2021-06-13 15:06:40 -0400

    edit no orgs message

--- client/src/components/Profile/NoJoinOrg.js
+++ client/src/components/Profile/NoJoinOrg.js
@@ -2,12 +2,12 @@ import React from "react";
 import styled from "styled-components";
 import { theme, mq } from "constants/theme";
 import noJoinOrgPic from "assets/no-org-animal.svg";
+import { useTranslation } from "react-i18next";
+
+
 
 const NoJoinOrg = ({ isSelf }) => {
-  const ownText =
-    "It looks like you haven't joined an organisation yet! To join an organisation, please go to the organisation's profile page and click to Join button under the description.";
-  const othersText =
-    "It looks like this person hasn’t joined an organisation yet! ";
+  const { t } = useTranslation();
 
   return (
     <Wrapper>
@@ -15,7 +15,7 @@ const NoJoinOrg = ({ isSelf }) => {
         <div className="no-join-org-pic">
           <img src={noJoinOrgPic} alt="no join org pic" />
         </div>
-        {isSelf ? <p>{ownText}</p> : <p>{othersText}</p>}
+        {isSelf ? <p>{t("profile.individual.noOrgs")}</p> : <p>{t("profile.individual.noOrgsOther")}</p>}
       </div>
     </Wrapper>
   );
@@ -24,19 +24,16 @@ const NoJoinOrg = ({ isSelf }) => {
 const Wrapper = styled.div`
   position: relative;
   width: 100%;
-  heigh: 100%;
   background: ${theme.colors.white};
   box-shadow: 0rem 0.4rem 2rem rgba(0, 0, 0, 0.08);
   border-radius: 1.2rem;
   padding: 2rem;
 
   .wrapper-content {
-    // position: absolute;
     margin: 4rem 12rem 10rem 12rem;
   }
 
   .no-join-org-pic {
-    // position: absolute;
     display: flex;
     justify-content: center;
     height: 20rem;
@@ -44,7 +41,6 @@ const Wrapper = styled.div`
     margin-bottom: 4rem;
   }
   p {
-    // position: absolute;
     display: block;
     font-family: Work Sans;
     font-style: normal;
