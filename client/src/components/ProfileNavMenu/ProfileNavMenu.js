a89bc036ea287165b509673b8fd1da104b846527
emanuelquerty <emanuelinacio1@hotmail.com>
2021-06-15 17:32:51 -0700

    Fixes svg attributes spelling mistakes

--- client/src/components/ProfileNavMenu/ProfileNavMenu.js
+++ client/src/components/ProfileNavMenu/ProfileNavMenu.js
@@ -10,8 +10,8 @@ const leftContainerWidth = "9.5rem";
 const Arrow = () => {
     return (
         <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path className="path1" stroke= {colors.royalBlue} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            <path className="path2" stroke= {colors.royalBlue} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path className="path1" stroke= {colors.royalBlue} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            <path className="path2" stroke= {colors.royalBlue} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
     );
 }
